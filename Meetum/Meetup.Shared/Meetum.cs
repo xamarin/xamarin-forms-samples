2f3c519857cd73970ae4e20f52f39bfa19bd1216
James Montemagno <james.montemagno@gmail.com>
2014-05-02 16:10:02 -0700

    Initial Windows Phone and Shared Code Transition
    Added icons for WP and read json file from individual projects.

---
+++ Meetum/Meetup.Shared/Meetum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Resources;
+using System.IO;
+using System.Collections.Generic;
+using Meetum.Models;
+
+namespace Meetum
+{
+    /// <summary>
+    /// Meetum placeholder.
+    /// </summary>
+    public static class Meetum
+    {
+        static Assembly _reflectionAssembly;
+
+        public static void Init(Assembly assembly)
+        {
+            System.Threading.Interlocked.CompareExchange(ref _reflectionAssembly, assembly, null);
+        }
+
+        public static Stream LoadResource(String name)
+        {
+            return _reflectionAssembly.GetManifestResourceStream(name);
+        }
+
+        public static List<POI> PointsOfInterest { get; set; }
+    }
+}
---
+++ Meetum/Meetup.Shared/Views/DarkTextCell.cs
@@ -0,0 +1,11 @@
+using System;
+using Xamarin.QuickUI;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Meetum.Views
+{
+    public class DarkTextCell : TextCell { }
+}
