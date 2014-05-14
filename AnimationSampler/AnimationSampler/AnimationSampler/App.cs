d091bbba475df223516796ca38793bc87a6c660a
cpetzold <charles.petzold@xamarin.com>
2014-05-14 17:06:26 -0400

    AnimationSampler Early Code

---
+++ AnimationSampler/AnimationSampler/AnimationSampler/App.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AnimationSampler
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            return new NavigationPage(new HomePage());
+        }
+    }
+}
