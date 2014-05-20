065b419578cba522b4309bb04943c807be842d43
Oleg Demchenko <olegoid@mars.dlink>
2014-05-20 23:38:35 +0400

    [EmployeeDirectory] Move IPhoneFeatureService.cs from EmployeeDirectory.csproj to EmployeeDirectoryCSharp.csproj

---
+++ EmployeeDirectory/EmployeeDirectoryCSharp/IPhoneFeatureService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeDirectoryCSharp
+{
+	public interface IPhoneFeatureService
+	{
+		bool Email (string emailAddress);
+		bool Browse (string websiteUrl);
+		bool Tweet (string twitterName);
+		bool Call (string phoneNumber);
+		bool Map (string address);
+	}
+}
+
