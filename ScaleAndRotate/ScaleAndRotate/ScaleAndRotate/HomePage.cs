5c399657ef0cc48d63b103bd665d9ce9eac2befe
cpetzold <charles.petzold@xamarin.com>
2014-05-20 11:56:56 -0400

    [ScaleAndRotate] First Code Upload
    Program demonstrates scaling and rotation with XAML data bindings.

---
+++ ScaleAndRotate/ScaleAndRotate/ScaleAndRotate/HomePage.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace ScaleAndRotate
+{
+    class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            Command<Type> navigateCommand = 
+                new Command<Type>((Type pageType) =>
+                {
+                    if (pageType == null)
+                    {
+                        this.DisplayAlert("ScaleAndRotate", 
+                                    "Page not yet implemented", "OK", null);
+                        return;
+                    }
+
+                    // Get all the constructors of the page type.
+                    IEnumerable<ConstructorInfo> constructors = 
+                            pageType.GetTypeInfo().DeclaredConstructors;
+
+                    foreach (ConstructorInfo constructor in constructors)
+                    {
+                        // Check if the constructor has no parameters.
+                        if (constructor.GetParameters().Length == 0)
+                        {
+                            // If so, instantiate it, and navigate to it.
+                            Page page = (Page)constructor.Invoke(null);
+                            this.Navigation.Push(page);
+                            break;
+                        }
+                    }
+                });
+
+            this.Content = new TableView
+                {
+                    Intent = TableIntent.Menu,
+                    Root = new TableRoot
+                    {
+                        new TableSection()
+                        {
+                            new TextCell
+                            {
+                                Text = "Scale",
+                                Command = navigateCommand,
+                                CommandParameter = typeof(ScaleDemoPage)
+                            },
+
+                            new TextCell
+                            {
+                                Text = "Rotate",
+                                Command = navigateCommand,
+                           //     CommandParameter = typeof(RotateDemoPage)
+                            },
+
+                            new TextCell
+                            {
+                                Text = "RotateX",
+                                Command = navigateCommand,
+                            //    CommandParameter = typeof(RotateXDemoPage)
+                            },
+
+                            new TextCell
+                            {
+                                Text = "RotateY",
+                                Command = navigateCommand,
+                          //      CommandParameter = typeof(RotateYDemoPage)
+                            }
+                        }
+                    }
+                };
+        }
+    }
+}
