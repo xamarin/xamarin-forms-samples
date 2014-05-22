4a1136094b634e201b63b7871bc2b51970994b58
cpetzold <charles.petzold@xamarin.com>
2014-05-22 17:43:56 -0400

    [XamlScaleAndRotate] XAML version of ScaleAndRotate

---
+++ XamlScaleAndRotate/XamlScaleAndRotate/XamlScaleAndRotate/HomePage.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace XamlScaleAndRotate
+{
+    class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            Command<Type> navigateCommand = 
+                new Command<Type>((Type pageType) =>
+                {
+                    if (pageType == null)
+                    {
+                        this.DisplayAlert("XamlScaleAndRotate", 
+                                    "Page not yet implemented", "OK", null);
+                        return;
+                    }
+
+                    // Get all the constructors of the page type.
+                    IEnumerable<ConstructorInfo> constructors = 
+                            pageType.GetTypeInfo().DeclaredConstructors;
+
+                    foreach (ConstructorInfo constructor in constructors)
+                    {
+                        // Check if the constructor has no parameters.
+                        if (constructor.GetParameters().Length == 0)
+                        {
+                            // If so, instantiate it, and navigate to it.
+                            Page page = (Page)constructor.Invoke(null);
+                            this.Navigation.Push(page);
+                            break;
+                        }
+                    }
+                });
+
+            this.Content = new TableView
+                {
+                    Intent = TableIntent.Menu,
+                    Root = new TableRoot
+                    {
+                        new TableSection()
+                        {
+                            new TextCell
+                            {
+                                Text = "Scale",
+                                Command = navigateCommand,
+                                CommandParameter = typeof(ScaleDemoPage)
+                            },
+
+                            new TextCell
+                            {
+                                Text = "Rotation",
+                                Command = navigateCommand,
+                                CommandParameter = typeof(RotationDemoPage)
+                            },
+
+                            new TextCell
+                            {
+                                Text = "RotationX",
+                                Command = navigateCommand,
+                            //    CommandParameter = typeof(RotateXDemoPage)
+                            },
+
+                            new TextCell
+                            {
+                                Text = "RotationY",
+                                Command = navigateCommand,
+                          //      CommandParameter = typeof(RotateYDemoPage)
+                            }
+                        }
+                    }
+                };
+        }
+    }
+}
---
+++ XamlScaleAndRotate/XamlScaleAndRotate/XamlScaleAndRotate/ScaleDemoPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlScaleAndRotate
+{
+    public partial class ScaleDemoPage
+    {
+        public ScaleDemoPage()
+        {
+            InitializeComponent();
+
+            // Set BindingContext properties "manually."
+            scaleSliderValue.BindingContext = scaleSlider;
+            scaleSlider.BindingContext = label;
+
+            anchorxStepperValue.BindingContext = anchorxStepper;
+            anchorxStepper.BindingContext = label;
+
+            anchoryStepperValue.BindingContext = anchoryStepper;
+            anchoryStepper.BindingContext = label;
+        }
+    }
+}
