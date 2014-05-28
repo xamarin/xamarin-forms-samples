d831f852d4e1e314ba997638222c1a750b0d47ee
Oleg Demchenko <olegoid@mars.dlink>
2014-05-28 06:43:02 +0400

    [EmployeeDirectory] Platform specific xaml layout for login screen

--- EmployeeDirectory/EmployeeDirectoryUI/ViewsXaml/LoginXaml.xaml.cs
+++ EmployeeDirectory/EmployeeDirectoryUI/ViewsXaml/LoginXaml.xaml.cs
@@ -19,7 +19,9 @@ namespace EmployeeDirectoryUI.Xaml
 		protected override void OnAppearing ()
 		{
 			base.OnAppearing ();
-
+			string platformName= Device.OS.ToString ();
+			Content.FindByName<Button> ("loginButton" + platformName).Clicked += OnLoginClicked;
+			Content.FindByName<Button> ("helpButton" + platformName).Clicked += OnHelpClicked;
 		}
 
 		private void OnLoginClicked (object sender, EventArgs e)
