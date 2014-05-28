cabcd05c69b49fe45f352094c97c1705e4cc1124
Olegoid <olegoid91@gmail.com>
2014-05-29 03:38:47 +0400

    [EmployeeDirectory] FileCache code formatting

--- EmployeeDirectory/EmployeeDirectory/Data/FileCache.cs
+++ EmployeeDirectory/EmployeeDirectory/Data/FileCache.cs
@@ -13,30 +13,30 @@ namespace EmployeeDirectory
 {
 	public static class FileCache
 	{
-		static FileCache()
-		{
-			init ();
-		}
 		static IFolder folder;
 		static JeffWilcox.Utilities.Silverlight.MD5 md5;
 
-		static async void init()
+		static FileCache ()
+		{
+			Init ();
+		}
+
+		static async void Init ()
 		{
 			IFolder rootFolder = FileSystem.Current.LocalStorage;
 			folder = await rootFolder.CreateFolderAsync ("Cache", CreationCollisionOption.OpenIfExists);
 			md5 = JeffWilcox.Utilities.Silverlight.MD5.Create ("MD5");
 		}
 
-		public static async Task<string> Download(string url)
+		public static async Task<string> Download (string url)
 		{
-			//var fileName = MD5.GetHashString(url);
 			var hash = md5.ComputeHash (Encoding.UTF8.GetBytes (url.Trim ()));
 			var fileName = string.Join ("", hash.Select (x => x.ToString ("x2")));
 
 			return await Download (url, fileName);
 		}
 
-		public static async Task<string> Download(Uri url, string email)
+		public static async Task<string> Download (Uri url, string email)
 		{
 			var hash = md5.ComputeHash (Encoding.UTF8.GetBytes (email.Trim ()));
 			var fileName = string.Join ("", hash.Select (x => x.ToString ("x2")));
@@ -45,27 +45,27 @@ namespace EmployeeDirectory
 		}
 
 		static object locker = new object ();
-		public static async Task<string> Download(string url, string fileName)
+
+		public static async Task<string> Download (string url, string fileName)
 		{
-			try{
+			try {
 				var path = Path.Combine (folder.Path, fileName);
-				var exists = await folder.CheckExistsAsync(fileName);
-				if (exists == ExistenceCheckResult.FileExists && !downloadTasks.ContainsKey(path))
-				{
+				var exists = await folder.CheckExistsAsync (fileName);
+				if (exists == ExistenceCheckResult.FileExists && !downloadTasks.ContainsKey (path)) {
 					return path;
 				}
 
-				var succes = await GetDownload(url,path);
-				return succes  ? path : "";
-			}
-			catch(Exception ex) {
+				var succes = await GetDownload (url, path);
+				return succes ? path : "";
+			} catch (Exception ex) {
 				Debug.WriteLine (ex);
 				return  "";
 			}
 		}
 
 		static Dictionary<string,Task<bool>> downloadTasks = new Dictionary<string, Task<bool>> ();
-		static Task<bool> GetDownload(string url, string fileName)
+
+		static Task<bool> GetDownload (string url, string fileName)
 		{
 			lock (locker) {
 				Task<bool> task;
@@ -74,38 +74,35 @@ namespace EmployeeDirectory
 
 				downloadTasks.Add (fileName, task = download (url, fileName));
 				return task;
-
 			}
 		}
-		static async Task<bool> download(string url, string fileName)
+
+		static async Task<bool> download (string url, string fileName)
 		{ 
 			IFile file = null;
-			try{
+			try {
 				var client = new HttpClient ();
 				var data = await client.GetByteArrayAsync (url);
 				file = await folder.CreateFileAsync (fileName,
 					CreationCollisionOption.ReplaceExisting);
-				using(var fileStream = await file.OpenAsync (FileAccess.ReadAndWrite)){
+				using (var fileStream = await file.OpenAsync (FileAccess.ReadAndWrite)) {
 					fileStream.Write (data, 0, data.Length);
 				}
 				return true;
-			}
-			catch(Exception ex) {
+			} catch (Exception ex) {
 				Debug.WriteLine (ex);
 			}
 			if (file != null)
 				await file.DeleteAsync ();
 			return false;
 		}
-		static void removeTask(string fileName)
+
+		static void removeTask (string fileName)
 		{
 			lock (locker) {
 				downloadTasks.Remove (fileName);
 			}
 		}
-
-
-
 	}
 }
 
