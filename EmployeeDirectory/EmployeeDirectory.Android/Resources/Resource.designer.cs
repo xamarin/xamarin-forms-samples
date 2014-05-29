30d46d4609ffa36dfc33404fc9371713e4bfba63
Olegoid <olegoid91@gmail.com>
2014-05-29 06:50:46 +0400

    [EmployeeDirectory] iOS and Android assets

--- EmployeeDirectory/EmployeeDirectory.Android/Resources/Resource.designer.cs
+++ EmployeeDirectory/EmployeeDirectory.Android/Resources/Resource.designer.cs
@@ -47,22 +47,31 @@ namespace EmployeeDirectory.Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int DetailsPlaceholder = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int login_box = 2130837505;
+			public const int DetailsPlaceholder_2x = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int logo = 2130837506;
+			public const int ic_search = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int logo_2x = 2130837507;
+			public const int Icon = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int monoandroidsplash = 2130837508;
+			public const int login_box = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int Search = 2130837509;
+			public const int logo = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int monoandroidsplash = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int Placeholder = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int Search = 2130837512;
 			
 			static Drawable()
 			{
