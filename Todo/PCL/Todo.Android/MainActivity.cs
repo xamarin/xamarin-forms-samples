f8af685ee28cba1ab1fc5ceeb7b6be6a061881f7
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-06-16 11:02:18 -0700

    [todo] update PCL to use DependencyService

---
+++ Todo/PCL/Todo.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using Android.Speech.Tts;
+
+namespace Todo
+{
+	[Activity (Label = "Todo", MainLauncher = true)]
+	public class Activity1 : Xamarin.Forms.Platform.Android.AndroidActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			Xamarin.Forms.Forms.Init (this, bundle);
+
+			SetPage (App.GetMainPage ());
+		}
+	}
+}
+
+
---
+++ Todo/PCL/Todo.iOS/SQLite_iOS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Todo;
+using Xamarin.Forms;
+using Todo.iOS;
+using System.IO;
+
+[assembly: Dependency (typeof (SQLite_iOS))]
+
+namespace Todo.iOS
+{
+	public class SQLite_iOS : ISQLite
+	{
+		public SQLite_iOS ()
+		{
+		}
+
+		#region ISQLite implementation
+		public SQLite.Net.SQLiteConnection GetConnection ()
+		{
+			var sqliteFilename = "TodoSQLite.db3";
+			string documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal); // Documents folder
+			string libraryPath = Path.Combine (documentsPath, "..", "Library"); // Library folder
+			var path = Path.Combine(libraryPath, sqliteFilename);
+
+			// This is where we copy in the prepopulated database
+			Console.WriteLine (path);
+			if (!File.Exists (path)) {
+				File.Copy (sqliteFilename, path);
+			}
+
+			var plat = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
+			var conn = new SQLite.Net.SQLiteConnection(plat, path);
+
+			// Return the database connection 
+			return conn;
+		}
+		#endregion
+	}
+}
