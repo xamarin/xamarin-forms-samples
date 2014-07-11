cc9cab79617941e5066ff6916b500751780b5c85
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-07-10 17:06:06 -0700

    [working-with] Gestures add WinPhone

---
+++ WorkingWithGestures/WinPhone/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace WorkingWithGestures.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = WorkingWithGestures.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
