7e584df2728cc029e2b06f64ddcde17c05a2c569
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-07-15 13:45:45 -0700

    [working-with] Fonts new attributes and xaml examples

---
+++ WorkingWithFonts/WorkingWithFonts/FontPageCs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+// forums question
+// http://forums.xamarin.com/discussion/17278/custom-font-in-xamarin-forms-font-awesome#latest
+
+// custom fonts in iOS
+// http://blog.xamarin.com/custom-fonts-in-ios/
+
+// font download
+// http://www.dafont.com/hollywood-hills.font
+using Xamarin.Forms;
+
+namespace WorkingWithFonts
+{
+	public class FontPageCs : ContentPage
+	{
+		public FontPageCs ()
+		{
+			var label = new Label {
+				Text = "Hello, Forms!",
+				Font = Device.OnPlatform (
+					Font.OfSize ("SF Hollywood Hills", 24),
+					Font.SystemFontOfSize (NamedSize.Medium),
+					Font.SystemFontOfSize (NamedSize.Large)
+				),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+
+			};
+
+			var myLabel = new MyLabel {
+				Text = "MyLabel for Android!",
+				Font = Device.OnPlatform (
+					Font.SystemFontOfSize (NamedSize.Small),
+					Font.SystemFontOfSize (NamedSize.Medium), // will get overridden in custom Renderer
+					Font.SystemFontOfSize (NamedSize.Large)
+				),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+
+			var labelBold = new Label {
+				Text = "Bold",
+				Font = Font.SystemFontOfSize (14, FontAttributes.Bold),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+			var labelItalic = new Label {
+				Text = "Italic",
+				Font = Font.SystemFontOfSize (14, FontAttributes.Italic),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+			var labelBoldItalic = new Label {
+				Text = "BoldItalic",
+				Font = Font.SystemFontOfSize (14, FontAttributes.Bold | FontAttributes.Italic),
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+
+			Content = new StackLayout { 
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				Children = {
+					label, myLabel, labelBold, labelItalic, labelBoldItalic
+				}
+			};
+		}
+	}
+}
+
