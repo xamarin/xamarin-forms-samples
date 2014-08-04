ea06b2e67ef433569a835dc32741c1b468c12263
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-08-04 09:40:01 -0700

    [solitaire] show tweet button for competition

--- SolitaireEncryption/Solitaire/Controls/TweetButton.cs
+++ SolitaireEncryption/Solitaire/Controls/TweetButton.cs
@@ -5,7 +5,7 @@ namespace Solitaire
 {
 	public class TweetButton : Button 
 	{
-		string tweetText = "Tell me more about Xaml #Petzold at #Xamarin Evolve {0}";
+		const string tweetText = "Tell me more about Xaml #Petzold at #Xamarin Evolve {0}";
 
 		public TweetButton () {
 			Code = "";
@@ -15,7 +15,7 @@ namespace Solitaire
 		public string Code { get; set; }
 
 		// url to blog post/competition
-		public string AttachedUrl = "http://blog.xamarin.com/";
+		public string AttachedUrl = "http://bit.ly/WWExLo";
 
 		// embed code in the tweet text
 		public string FormattedText {
