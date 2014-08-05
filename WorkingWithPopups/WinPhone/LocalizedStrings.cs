e66e1e2b5aba1b4ced0a72484f9277d6fe7de832
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-08-05 15:44:25 -0700

    [working-with] popups (add WinPhone)

---
+++ WorkingWithPopups/WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WorkingWithPopups.WinPhone.Resources;
+
+namespace WorkingWithPopups.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
