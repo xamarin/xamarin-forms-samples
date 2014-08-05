ee5a9d29726f896a98c2789717a5a45e8718a079
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-08-05 15:35:58 -0700

    [working-with] popups (simple sample)

---
+++ WorkingWithPopups/WorkingWithPopups/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WorkingWithPopups
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			var tabs = new TabbedPage ();
+			tabs.Children.Add(new AlertPage { Title = "Alerts", Icon="csharp.png"});
+			tabs.Children.Add(new ActionSheetPage {Title = "ActionSheets", Icon="csharp.png"});
+			return tabs;
+
+			return new ActionSheetPage();
+		}
+	}
+}
+
