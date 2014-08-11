250c329677b17df8861b664796beb8437725397e
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-08-11 15:40:16 -0700

    [forms2native] add winphone project (not implemented)

---
+++ Forms2Native/Forms2Native.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+using Forms2Native;
+
+namespace Forms2Native.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = Forms2Native.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
