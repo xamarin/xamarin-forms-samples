11dce7f74dbffc04090ebb76a4a1f073ecc9e85a
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-08-18 17:02:40 -0700

    [working-with] add debug output for Region info

--- WorkingWithMaps/WorkingWithMaps/MapPage.cs
+++ WorkingWithMaps/WorkingWithMaps/MapPage.cs
@@ -53,6 +53,14 @@ namespace WorkingWithMaps
 			stack.Children.Add (slider);
 			stack.Children.Add (segments);
 			Content = stack;
+
+
+			// for debugging output only
+			map.PropertyChanged += (sender, e) => {
+				Debug.WriteLine(e.PropertyName + " just changed!");
+				if (e.PropertyName == "VisibleRegion" && map.VisibleRegion != null)
+					CalculateBoundingCoordinates (map.VisibleRegion);
+			};
 		}
 
 		void HandleClicked (object sender, EventArgs e)
@@ -69,6 +77,36 @@ namespace WorkingWithMaps
 				map.MapType = MapType.Satellite;
 				break;
 			}
+		}
+
+
+
+		/// <summary>
+		/// In response to this forum question http://forums.xamarin.com/discussion/22493/maps-visibleregion-bounds
+		/// Useful if you need to send the bounds to a web service or otherwise calculate what
+		/// pins might need to be drawn inside the currently visible viewport.
+		/// </summary>
+		static void CalculateBoundingCoordinates (MapSpan region)
+		{
+			// WARNING: I haven't tested the correctness of this exhaustively!
+			var center = region.Center;
+			var halfheightDegrees = region.LatitudeDegrees / 2;
+			var halfwidthDegrees = region.LongitudeDegrees / 2;
+
+			var left = center.Longitude - halfwidthDegrees;
+			var right = center.Longitude + halfwidthDegrees;
+			var top = center.Latitude + halfheightDegrees;
+			var bottom = center.Latitude - halfheightDegrees;
+
+			// Adjust for Internation Date Line (+/- 180 degrees longitude)
+			if (left < -180) left = 180 + (180 + left);
+			if (right > 180) right = (right - 180) - 180;
+			// I don't wrap around north or south; I don't think the map control allows this anyway
+
+			Debug.WriteLine ("Bounding box:");
+			Debug.WriteLine ("                    " + top);
+			Debug.WriteLine ("  " + left + "                " + right);
+			Debug.WriteLine ("                    " + bottom);
 		}
 	}
 }
