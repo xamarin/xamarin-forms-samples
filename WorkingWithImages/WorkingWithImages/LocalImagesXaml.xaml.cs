bf7365236468b72d83e858ef578e962339d93522
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-11-03 15:59:57 -0800

    [working-with] images update Xamarin.Forms version (also iOS8)

--- WorkingWithImages/WorkingWithImages/LocalImagesXaml.xaml.cs
+++ WorkingWithImages/WorkingWithImages/LocalImagesXaml.xaml.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace WorkingWithImages
 {
-    public partial class LocalImagesXaml
+    public partial class LocalImagesXaml : ContentPage
     {
         public LocalImagesXaml()
         {
