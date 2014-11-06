c34b06eed2bafa9458d4e182cf9cd20b2276cacc
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-11-06 14:13:14 -0800

    [resxl10n] add winphone

---
+++ UsingResxLocalization/WinPhone/Localize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+[assembly: Xamarin.Forms.Dependency(typeof(UsingResxLocalization.WinPhone.Localize))]
+
+namespace UsingResxLocalization.WinPhone
+{
+    public class Localize : UsingResxLocalization.ILocalize
+    {
+        public void SetLocale()
+        {
+            //
+        }
+
+        public System.Globalization.CultureInfo GetCurrentCultureInfo()
+        {
+            return System.Threading.Thread.CurrentThread.CurrentUICulture;
+        }
+    }
+}
+
