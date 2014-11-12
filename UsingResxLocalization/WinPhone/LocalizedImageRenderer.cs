4323c9da1fa6c33cc26f1cc5957fb6d59d1ba7a9
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-11-12 10:28:42 -0800

    [todol10n] add WinPhone localized image renderer

---
+++ UsingResxLocalization/WinPhone/LocalizedImageRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinPhone;
+
+[assembly: ExportRenderer(typeof(Image), typeof(UsingResxLocalization.WinPhone.LocalizedImageRenderer))]
+
+/*
+ * This code is provided as a suggestion for how to support localized images on Windows Phone.
+ * 
+ * You are encouraged to modify it to suit your specific localization needs, in particular
+ * error handling and fall-back when a specific image file is not available.
+ */
+namespace UsingResxLocalization.WinPhone
+{
+    public class LocalizedImageRenderer : ImageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var s = e.NewElement.Source as FileImageSource;
+                if (s != null)
+                {
+                    var fileName = s.File;
+                    string ci = System.Threading.Thread.CurrentThread.CurrentUICulture.ToString();
+                    if (ci == "pt-BR" | ci == "zh-Hans" | ci == "zh-Hant")  {
+                        // use the complete string
+                    } else { 
+                        ci = System.Threading.Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName;
+                    }
+                    e.NewElement.Source = Path.Combine("Assets/" + ci + "/" + fileName);
+                }
+            }
+        }
+    }
+}
