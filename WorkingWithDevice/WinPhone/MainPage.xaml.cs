50921662a45c1437ec133b6c7495e741698b5732
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-11-20 17:50:16 -0800

    [working-with] device winphone

---
+++ WorkingWithDevice/WinPhone/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace WorkingWithPlatformSpecifics.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = WorkingWithPlatformSpecifics.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
