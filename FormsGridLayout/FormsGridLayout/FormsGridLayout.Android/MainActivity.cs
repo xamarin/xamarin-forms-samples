9ee49af548722b1b2b59a82f7178ea522f1e487e
Craig Dunn <craig.dunn@conceptdevelopment.net>
2014-12-04 13:39:39 -0800

    [formsgridlayout] update to XF1.3/Unified

--- FormsGridLayout/FormsGridLayout/FormsGridLayout.Android/MainActivity.cs
+++ FormsGridLayout/FormsGridLayout/FormsGridLayout.Android/MainActivity.cs
@@ -12,7 +12,7 @@ using Xamarin.Forms.Platform.Android;
 namespace GridLayoutDemo.Droid
 {
     [Activity(Label = "GridLayoutDemo", MainLauncher = true)]
-    public class MainActivity : AndroidActivity
+	public class MainActivity : FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
@@ -20,7 +20,7 @@ namespace GridLayoutDemo.Droid
 
             Xamarin.Forms.Forms.Init(this, bundle);
 
-            SetPage(App.GetMainPage());
+			LoadApplication (new App ());
         }
     }
 }
--- FormsGridLayout/FormsGridLayout/FormsGridLayout/App.cs
+++ FormsGridLayout/FormsGridLayout/FormsGridLayout/App.cs
@@ -7,11 +7,11 @@ using Xamarin.Forms;
 
 namespace GridLayoutDemo
 {
-    public class App
+	public class App : Application
     {
-        public static Page GetMainPage()
+		public App ()
         {
-            return new GridLayoutPage();
+			MainPage = new GridLayoutPage();
         }
     }
 }
