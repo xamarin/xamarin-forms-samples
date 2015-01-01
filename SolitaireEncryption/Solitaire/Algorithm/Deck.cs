c96bad59568f53619259dd63d409e4deb435adc7
Nate Guerin <nathan.guerin@riseup.net>
2015-01-01 15:03:21 +0100

    Add Unit Tests for Deck.cs
    Each method in Deck.cs now has a unit test.  Fixed a few allocation bugs
    in Deck.cs and improved method documentation and parameter names for
    clarity.  Original formatting kept.

--- SolitaireEncryption/Solitaire/Algorithm/Deck.cs
+++ SolitaireEncryption/Solitaire/Algorithm/Deck.cs
@@ -10,13 +10,12 @@ namespace ConceptDevelopment.Net.Collections {
 		/// <summary>Array of Card structs that represents the Deck.
 		/// Position 0 is the _bottom_ of the deck, </summary>
 		private Card[] _cardArray;
-		private int _numCards;
+		private const int _numCards = 54;
 		// IEnumerator: current position in array set to initial position
 		private int _position = -1;
 
 		/// <summary>Constructor: Create a deck of cards</summary>
 		public Deck() {
-			_numCards = 54;
 			_cardArray = new Card[_numCards];
 			for (int suits = 3; suits >= 0; suits--) {
 				for (int faces = 12; faces >= 0; faces--){
@@ -27,19 +26,7 @@ namespace ConceptDevelopment.Net.Collections {
 			_cardArray[52] = new Card(CardSuit.Joker, CardFace.Ace);
 			_cardArray[53] = new Card(CardSuit.Joker, CardFace.Two);
 		}
-		/// <summary>Constructor: Create a deck of cards (reverse sort order)</summary>
-		public Deck(bool reverseOrder) { 
-			_numCards = 54;
-			_cardArray = new Card[_numCards];
-			for (int suits = 0; suits <= 3; suits++) {
-				for (int faces = 0; faces <= 12; faces++){
-					_cardArray[(suits * 13) + faces] = new Card(suits, faces + 1);
-				}
-			}
-			// Add the jokers to the end of the deck
-			_cardArray[52] = new Card(CardSuit.Joker, CardFace.Ace);
-			_cardArray[53] = new Card(CardSuit.Joker, CardFace.Two);
-		}
+
 		/// <summary>Return the number of Cards in the Deck</summary>
 		/// <returns>Number of Cards in the Deck</returns>
 		public int Count() {
@@ -63,9 +50,6 @@ namespace ConceptDevelopment.Net.Collections {
 		/// <returns>Last face-down Card on the Deck</returns>
 		public Card PeekBottom(int position) {
 			if (position > 0 && position <= _numCards) {
-				// _numCards less one, since it's the number of elements,
-				// and not the array index. Since position can be zero
-				// we must subtract one to avoid a OutOfRange exception.
 				return _cardArray[(_numCards) - position ];
 			} else {
 				throw new ArgumentOutOfRangeException("position", 
@@ -80,13 +64,13 @@ namespace ConceptDevelopment.Net.Collections {
 		/// <returns>int [1 .. 54]</returns>
 		public int FindBottom(Card findCard) {
 			return (_numCards - FindTop(findCard)) + 1;    // not base 0
-		} // FindTop(Card)
+		} // FindBottom(Card)
 
 		/// <summary>Return the position of the specified card if it can
 		///  be found in the deck.  Otherwise return a negative number.
 		///  _cardArray[0] is the top of the Deck.</summary>
 		/// <param name="findCard">Card</param>
-		/// <returns>int [1 .. 54]</returns>
+		/// <returns>int [1, 54]</returns>
 		public int FindTop(Card findCard) {
 			int i = 0;
 			while (i < _numCards ) {
@@ -96,14 +80,14 @@ namespace ConceptDevelopment.Net.Collections {
 				i++;
 			} // while (i <= UpperBound)
 			return -1;
-		} // FindBottom(Card)
+		} // FindTop(Card)
 
 		/// <summary>Changes the order of the cards to a random
 		/// pattern using the built-in random number generator.</summary>
 		public void Shuffle() {
 			Random rand = new Random();
-			for (int i = 0; i < _cardArray.GetUpperBound(0); i++) {
-				int swap = rand.Next (0, 53); 
+			for (int i = 0; i < _cardArray.Length; i++) {
+				int swap = rand.Next(0, 54);
 				Card buffer = _cardArray[i];
 				_cardArray[i] = _cardArray[swap];
 				_cardArray[swap] = buffer;
@@ -112,23 +96,23 @@ namespace ConceptDevelopment.Net.Collections {
 
 
 		/// <summary>Swaps Card at specified position with the Card 
-		/// below it.</summary>
-		/// <param name="position">Position in the Deck
-		/// [ 1 ... 54 ] 1 being the top Card</param>
+		/// below it. If the card is on the bottom of the deck, it is moved to the top 
+		/// and then swapped with the one below it.</summary>
+		/// <param name="position">Position in the Deck [1,54] 1 being the top Card</param>
 		public void MoveDown(int cardPosition) {
 			int position = cardPosition; //_numCards - cardPosition;
 			if (position > 0 && position < _cardArray.Length) {
 				Card buffer = _cardArray[position - 1];
 				_cardArray[position - 1] = _cardArray[position];
 				_cardArray[position] = buffer;
-			} else if (position == _cardArray.Length ){
+			} else if (position == _cardArray.Length ){ 
 				// It's already at the bottom, need to move it to the top
 				// Remembering that this doesn't actually constitute a 'MoveDown'
-				Card buffer = _cardArray[_cardArray.Length - 1];
+				Card lastCard = _cardArray[_cardArray.Length - 1];
 				for (int j = (_cardArray.Length - 1); j >= 1; j--) {
 					_cardArray[j] = _cardArray[j - 1];
 				}
-				_cardArray[0] = buffer;
+				_cardArray[0] = lastCard;
 				// now it's on the top, perform the MoveDown
 				MoveDown(1);
 			} else {
@@ -140,79 +124,88 @@ namespace ConceptDevelopment.Net.Collections {
 		/// <summary>Swaps Card at specified position with the Card 
 		/// below it. Repeats 'count' times.</summary>
 		/// <param name="position">Position in the Deck 
-		/// [ 1 ... 54 ] 1 being to top Card</param>
+		/// [1, 54] 1 being to top Card</param>
 		/// <param name="count">Number of times to move the Card down [ 1 ...]</param>
 		public void MoveDown(int position, int count) {
-			while (count > 0) {
+			while (count-- > 0) {
 				MoveDown(position);
 				if (position == _numCards){
-					position = 2; // MoveDown(_numCards) puts the Card in position 1
-					// so must skip this and go to position 2.
+					position = 2; // MoveDown(_numCards) puts the Card in position 2
 				} else {
 					position++;
 				}
-				count--;
 			} // while (count > 0)
 		} // MoveDown(int)
+		
+		/// <summary>Switch cards 1 to upperLimitBottom with 
+		/// cards from upperLimitBottom + 1 to upperLimitTop.
+		/// A upperLimitBottom of 1 or 54 will not change the deck.</summary>
+		/// <param name="upperLimitBottom">The last bottom card.
+		/// [1,54] are valid arguments</param>
+		/// <param name="upperLimitTop">The last top card. 
+		/// [1,54] are valid arguments </param>
+		public void BasicCut(int upperLimitBottom, int upperLimitTop) {
 
-		/// <summary>Switch cards below the specified position with 
-		/// the cards above, up to the specified limit.
-		/// A position of 1 or limit will not change the deck.</summary>
-		/// <param name="fromPosition">Where to start the cut.
-		/// 0 or 54 will not cause any cut.</param>
-		/// <param name="toPosition">Where to stop the cut</param>
-		public void BasicCut(int fromPosition, int toPosition) {
-			if ((fromPosition > 0) && (fromPosition < toPosition)) {
-				// number of elements must be +1 because 
-				// position 9 requires an array size 
-				// of 10 (don't forget position 0).
-				Card[] buffer = new Card[toPosition+1];
+            		if (upperLimitBottom < 1 || upperLimitBottom > 54 || upperLimitTop > 54) {
+				throw new ArgumentOutOfRangeException(String.Format("toPosition and fromPosition must be in the range [1,54], you passed {0} and {1}", 
+			upperLimitBottom, upperLimitTop));
+            		}
+
+            		if (upperLimitBottom < upperLimitTop) {
+				Card[] buffer = new Card[upperLimitTop];
+
 				// First move the lower cards to the top 
-				// [0 .. from_position] -> [to_position-from_position ... ]
-				for (int i = 0; i < fromPosition; i++) {
-					buffer[i + (toPosition - fromPosition)] = _cardArray[i];
-				} // for i
+				// [0, upperLimitBotom) -> [upperLimitTop-upperLimitBottom, upperLimitTop)
+				for (int i = 0; i < upperLimitBottom; i++) {
+					buffer[i + (upperLimitTop - upperLimitBottom)] = _cardArray[i];
+				}
 
 				// Then move the upper cards down.
-				// [ from_position .. to_position ] -> [ 0 .. ]
-				for (int i = fromPosition; i < toPosition; i++) {
-					buffer[i - fromPosition] = _cardArray[i];
-				} // for i
+				// [upperLimitBottom, upperLimitTop) -> [0, upperLimitTop-upperLimitBottom)
+				for (int i = upperLimitBottom; i < upperLimitTop; i++) {
+					buffer[i - upperLimitBottom] = _cardArray[i];
+				}
 
 				// Then replace the range in the original deck.
-				for (int i = 0; i < toPosition; i++) {
+				for (int i = 0; i < upperLimitTop; i++) {
 					_cardArray[i] = buffer[i];
-				} // for i
+				}
 			} // if positionInDeck
 		} // BasicCut(int, int)
 
-		/// <summary>Switch cards below the specified position with the
-		/// cards above.
-		/// A size of zero or marker will not change the deck.</summary>
-		/// <param name="size">[ 1 .. 54 ] where 1 is the top of the Deck</param>
-		public void CutTop(int size) {
-			BasicCut(size, _numCards);
-			//BasicCut(_numCards - size, _numCards - 1);
+		/// <summary>Pull all cards between 1 and count (inclusive) and move them to the bottom of the deck</summary>
+		/// <param name="size">[1,54] where 1 is the top of the Deck</param>
+		public void CutTop(int count) {
+			if (count < 1 || count > 54) {
+				throw new ArgumentOutOfRangeException("Count must be between 1 and 53");
+			}
+            		BasicCut(count, _numCards);
 		} // cutTop(int)
 
-		/// <summary>Switch cards below the specified position with the
-		/// cards above.
-		/// A size of zero or marker will not change the deck.</summary>
-		/// <param name="size">[ 1 .. 54 ] where 1 is the bottom of the Deck</param>
-		public void CutBottom(int size) {
-			BasicCut(_numCards - size, _numCards);
+		/// <summary>Pull count cards from bottom and move them to the top of the deck</summary>
+		/// <param name="count">[1,54] where 1 is the bottom of the Deck</param>
+		public void CutBottom(int count) {
+			if (count < 1 || count > 54) {
+				throw new ArgumentOutOfRangeException("Count must be between 1 and 53");
+			}
+			CutTop(_numCards - count);
 		} // cutBottom(int)
 
 		/// <summary>Swaps the cards above the highest of the
 		/// specified positions with cards below the lowest.</summary>
-		/// <param name="low">[ 1 .. 54 ]</param>
-		/// <param name="high">[ 1 .. 54 ] > low</param>
+		/// <param name="low">[1,54]</param>
+		/// <param name="high">[2,54] && greater than low</param>
 		public void TripleCut(int low, int high) {
-			// Requires: 0 < low <= high <= this.Count()
-			if ((0 < low) && (low <= high) && (high <= _numCards)) {
-				BasicCut(low - 1, high);
-				BasicCut(high, _numCards);    // must subtract to make 53
-			} // if low && high
+			if (low < high) {
+				if ((1 < low)) {
+					BasicCut(low - 1, high);
+				}
+				if (high < _numCards) {
+					CutTop(high);
+				}
+			} else {
+				throw new ArgumentException("0 < low <= high <= 54");
+			}
 		} // tripleCut();
 
 
---
+++ SolitaireEncryption/sol-test/DeckTest.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ConceptDevelopment.Net.Collections;
+
+namespace soltest
+{
+    [TestFixture()]
+    public class DeckTest
+    {
+        private Random _rand;
+
+        public DeckTest()
+        {
+            _rand = new Random();
+        }
+
+        [SetUp()]
+        public void reset()
+        {
+            referenceDeck = new Deck();
+            variableDeck = new Deck();
+        }
+
+        [Test()]
+        public void TestDeck_CreatedDeterministically()
+        {
+            CollectionAssert.AreEqual(referenceDeck, variableDeck);
+            // Test this assertion method works
+            assertCardMovedFromTo(53, 53);
+        }
+
+        [Test()]
+        public void TestDeck_BasicCut()
+        {
+            var bottomCard = variableDeck.PeekBottom(1);
+            var secondToBottom = variableDeck.PeekBottom(2);
+            var topCard = variableDeck.PeekTop(1);
+
+            variableDeck.BasicCut(53, variableDeck.Count());
+            // Original Card 53 must move to place 1
+            Assert.AreEqual(bottomCard, variableDeck.PeekTop(1));
+            // original Card 1 must move to place 2 
+            Assert.AreEqual(variableDeck.PeekTop(2), topCard);
+            // original card 52 must move to place 53
+            Assert.AreEqual(secondToBottom, variableDeck.PeekBottom(1));
+            reset();
+
+            variableDeck.BasicCut(6, 9);
+
+            assertCardMovedFromTo(1, 4);
+            assertCardMovedFromTo(2, 5);
+            assertCardMovedFromTo(3, 6);
+            assertCardMovedFromTo(4, 7);
+            assertCardMovedFromTo(5, 8);
+            assertCardMovedFromTo(6, 9);
+            assertCardMovedFromTo(8, 2);
+            assertCardMovedFromTo(9, 3);
+            assertCardMovedFromTo(10, 10);
+        }
+
+        private void assertCardMovedFromTo(int from, int to)
+        {
+            Assert.AreEqual(referenceDeck.PeekTop(from), variableDeck.PeekTop(to));
+        }
+
+        [Test()]
+        public void TestDeck_BasicCutArgumentBoundaries()
+        {
+            // Passing a toPosition of greater than variableDeck.Count() must throw an exception
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                referenceDeck.BasicCut(variableDeck.Count(), 55);
+            });
+
+            reset();
+
+            // Passing a fromPosition of greater than variableDeck.Count() must throw an exception
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                referenceDeck.BasicCut(55, 56);
+            });
+
+            reset();
+
+            Assert.Catch<ArgumentOutOfRangeException>(() =>
+            {
+                referenceDeck.BasicCut(0, 1);
+            });
+
+            reset();
+        }
+
+        [Test()]
+        public void TestDeck_CutTop()
+        {
+            for (int i = 0; i < variableDeck.Count(); i++)
+            {
+                var random = _rand.Next(1, variableDeck.Count());
+                variableDeck.CutTop(random);
+
+                int j = random + 1, k = 1;
+                for (; j <= variableDeck.Count(); j++, k++)
+                {
+                    assertCardMovedFromTo(j, k);
+                }
+
+                j = 1;
+                for (; j <= random; j++)
+                {
+                    assertCardMovedFromTo(j, j + (variableDeck.Count() - random));
+                }
+                referenceDeck.CutTop(random);
+            }
+        }
+
+        [Test()]
+        public void TestDeck_CutBottom()
+        {
+            for (int i = 0; i < variableDeck.Count(); i++)
+            {
+                var random = _rand.Next(1, variableDeck.Count());
+                variableDeck.CutBottom(random);
+                referenceDeck.CutTop(variableDeck.Count() - random);
+                CollectionAssert.AreEqual(referenceDeck, variableDeck);
+            }
+        }
+
+        [Test()]
+        public void TestDeck_FindBottom()
+        {
+            Assert.AreEqual(1, referenceDeck.FindBottom(new Card(CardSuit.Joker, CardFace.Two)));
+            Assert.AreEqual(3, referenceDeck.FindBottom(new Card(3, 13)));
+            Assert.AreEqual(54, referenceDeck.FindBottom(new Card(0, 1)));
+        }
+
+        [Test()]
+        public void TestDeck_FindTop()
+        {
+            Assert.AreEqual(1, referenceDeck.FindTop(new Card(0, 1)));
+            Assert.AreEqual(3, referenceDeck.FindTop(new Card(0, 3)));
+            Assert.AreEqual(54, referenceDeck.FindTop(new Card(CardSuit.Joker, CardFace.Two)));
+        }
+
+        [Test()]
+        public void TestDeck_FindTopAndBottomAreInverses()
+        {
+            for (int i = 0; i < referenceDeck.Count(); i++)
+            {
+                int suit = rand.Next(0, 4), face = rand.Next(1, 14);
+                int distanceFromTop = referenceDeck.FindTop(new Card(suit, face));
+                int distanceFromBottom = referenceDeck.FindBottom(new Card(suit, face));
+                Assert.AreEqual(55, distanceFromTop + distanceFromBottom);
+            }
+        }
+
+        [Test()]
+        public void TestDeck_MoveDown()
+        {
+            variableDeck.MoveDown(1);
+            assertCardMovedFromTo(1, 2);
+            assertCardMovedFromTo(2, 1);
+            for (int i = 3; i <= 54; i++)
+            {
+                assertCardMovedFromTo(i, i);
+            }
+
+            reset();
+            variableDeck.MoveDown(54);
+            for (int i = 1; i <= 54; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        assertCardMovedFromTo(i, i);
+                        break;
+                    case 54:
+                        assertCardMovedFromTo(i, 2);
+                        break;
+                    default:
+                        assertCardMovedFromTo(i, i + 1);
+                        break;
+                }
+            }
+        }
+
+        [Test()]
+        public void TestDeck_MoveDownMultiple()
+        {
+            variableDeck.MoveDown(1, 2);
+            assertCardMovedFromTo(1, 3);
+            assertCardMovedFromTo(2, 1);
+            assertCardMovedFromTo(3, 2);
+
+            reset();
+            variableDeck.MoveDown(53, 3);
+            assertCardMovedFromTo(54, 54);
+            assertCardMovedFromTo(53, 3);
+            assertCardMovedFromTo(2, 2);
+            assertCardMovedFromTo(1, 1);
+
+            for (int i = 3; i <= 52; i++)
+            {
+                assertCardMovedFromTo(i, i + 1);
+            }
+        }
+
+        [Test()]
+        public void TestDeck_MoveNext()
+        {
+            for (int i = 0; i < variableDeck.Count(); i++)
+            {
+                Assert.IsTrue(variableDeck.MoveNext());
+            }
+            Assert.IsFalse(variableDeck.MoveNext());
+        }
+
+        [Test()]
+        public void TestDeck_Shuffle()
+        {
+            variableDeck.Shuffle();
+            CollectionAssert.AreNotEqual(referenceDeck, variableDeck);
+        }
+
+        [Test()]
+        public void TestDeck_TripleCut()
+        {
+            for (int i = 0; i < 100; i++) {
+                reset();
+                variableDeck.Shuffle();
+                var oneJoker = variableDeck.FindTop(new Card(CardSuit.Joker, CardFace.Ace));
+                var twoJoker = variableDeck.FindTop(new Card(CardSuit.Joker, CardFace.Two));
+                var firstJoker = Math.Min(oneJoker, twoJoker);
+                var secondJoker = Math.Max(oneJoker, twoJoker);
+                var cards = variableDeck.Cast<Card>().ToList();
+                variableDeck.Reset();
+
+                List<Card> firstStack = cards.Take(firstJoker - 1).ToList();
+                List<Card> lastStack = cards.Skip(secondJoker).ToList();
+                List<Card> middleStack = cards.Skip(firstJoker - 1).Take(cards.Count() - firstStack.Count() - lastStack.Count()).ToList();
+                List<Card> output = lastStack.Concat(middleStack).Concat(firstStack).ToList();
+                variableDeck.TripleCut(firstJoker, secondJoker);
+                CollectionAssert.AreEqual(output, variableDeck.Cast<Card>().ToList());
+            }
+        }
+
+        public Deck referenceDeck { get; set; }
+
+        public Deck variableDeck { get; set; }
+
+        public Random rand
+        {
+            get { return _rand; }
+        }
+    }
+}
