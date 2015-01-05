7c16939a2253b89617cdfc848d6f809a7b2a0564
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-01-05 21:03:07 +1100

    [working-with] listview contextaction xaml

--- WorkingWithListview/WorkingWithListview/App.cs
+++ WorkingWithListview/WorkingWithListview/App.cs
@@ -11,27 +11,21 @@ namespace WorkingWithListview
 			// USE C#
 
             var tabs = new TabbedPage { Title = "Working with ListView" };
-
 			tabs.Children.Add (new BasicListPage {Title="Basic", Icon = "csharp.png" });
-
 			tabs.Children.Add (new UnevenRowsPage {Title="Uneven", Icon = "csharp.png" });
-
 			tabs.Children.Add (new ContextActionsPage {Title="Context", Icon = "csharp.png" });
-
 			tabs.Children.Add (new CustomCellPage {Title="Button", Icon = "csharp.png" });
 
 			MainPage = tabs;
 
 
-			// USE XAML
+			// USE XAML - uncomment the MainPage line below to use the XAML version
 
-			//var tabsXaml = new TabbedPage ();
-
-			//tabsXaml.Children.Add (new BasicListXaml {Title="BasicX", Icon = "xaml.png" });
-
-			//tabsXaml.Children.Add (new UnevenRowsXaml {Title="UnevenX", Icon = "xaml.png" });
-
-			//tabsXaml.Children.Add (new CustomCellXaml {Title="ButtonX", Icon = "xaml.png" });
+			var tabsXaml = new TabbedPage ();
+			tabsXaml.Children.Add (new BasicListXaml {Title="BasicX", Icon = "xaml.png" });
+			tabsXaml.Children.Add (new UnevenRowsXaml {Title="UnevenX", Icon = "xaml.png" });
+			tabsXaml.Children.Add (new ContextActionsXaml {Title="ContextX", Icon = "xaml.png" });
+			tabsXaml.Children.Add (new CustomCellXaml {Title="ButtonX", Icon = "xaml.png" });
 
 			//MainPage = tabsXaml;
 
---
+++ WorkingWithListview/WorkingWithListview/ContextActionsXaml.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WorkingWithListview
+{
+	public partial class ContextActionsXaml : ContentPage
+	{
+		public ContextActionsXaml ()
+		{
+			InitializeComponent ();
+
+			listView.ItemsSource = new [] { "alpha", "beta", "gamma", "delta" };
+		}
+
+		public void OnItemSelected (object sender, SelectedItemChangedEventArgs e) {
+			if (e.SelectedItem == null) return; // has been set to null, do not 'process' tapped event
+			DisplayAlert("Tapped", e.SelectedItem + " row was tapped", "OK");
+			((ListView)sender).SelectedItem = null; // de-select the row
+		}
+
+		public void OnMore (object sender, EventArgs e) {
+			var mi = ((MenuItem)sender);
+			DisplayAlert("More Context Action", mi.CommandParameter + " more context action", "OK");
+		}
+
+		public void OnDelete (object sender, EventArgs e) {
+			var mi = ((MenuItem)sender);
+			DisplayAlert("Delete Context Action", mi.CommandParameter + " delete context action", "OK");
+		}
+	}
+}
+
