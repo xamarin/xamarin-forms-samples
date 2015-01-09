235b4bb411eb74fbe954601a9fbdea527b227743
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-01-09 17:07:25 +1100

    [working-with] fonts updated for 1.3

--- WorkingWithFonts/WorkingWithFonts/FontPageCs.cs
+++ WorkingWithFonts/WorkingWithFonts/FontPageCs.cs
@@ -12,49 +12,64 @@ using Xamarin.Forms;
 
 namespace WorkingWithFonts
 {
+	/// <summary>
+	/// Using fonts in Xamarin.Forms 1.3 is slightly different to earlier versions.
+	/// The 'Font' property has been replaced with three separate properties:
+	/// - FontFamily
+	/// - FontSize
+	/// - FontAttributes
+	/// Use these three properties directly and avoid using the Font class.
+	/// </summary>
 	public class FontPageCs : ContentPage
 	{
 		public FontPageCs ()
 		{
 			var label = new Label {
 				Text = "Hello, Forms!",
-				Font = Device.OnPlatform (
-					Font.OfSize ("SF Hollywood Hills", 24),
-					Font.SystemFontOfSize (NamedSize.Medium),
-					Font.SystemFontOfSize (NamedSize.Large)
-				),
+				FontFamily = Device.OnPlatform (
+					"SF Hollywood Hills",
+					null,
+					null
+				), // set only for iOS
 				VerticalOptions = LayoutOptions.CenterAndExpand,
 				HorizontalOptions = LayoutOptions.CenterAndExpand,
 
 			};
+			label.FontSize = Device.OnPlatform (
+				24, 
+				Device.GetNamedSize (NamedSize.Medium, label), 
+				Device.GetNamedSize (NamedSize.Large, label)
+			);
 
 			var myLabel = new MyLabel {
 				Text = "MyLabel for Android!",
-				// temporarily disable the size setting, since it's breaking the custom font on 1.2.2 :-(
-//				Font = Device.OnPlatform (
-//					Font.SystemFontOfSize (NamedSize.Small),
-//					Font.SystemFontOfSize (NamedSize.Medium), // will get overridden in custom Renderer
-//					Font.SystemFontOfSize (NamedSize.Large)
-//				),
 				VerticalOptions = LayoutOptions.CenterAndExpand,
 				HorizontalOptions = LayoutOptions.CenterAndExpand,
 			};
+			myLabel.FontSize = Device.OnPlatform (
+				Device.GetNamedSize (NamedSize.Small, myLabel),
+				Device.GetNamedSize (NamedSize.Medium, myLabel), // will get overridden in custom Renderer
+				Device.GetNamedSize (NamedSize.Large, myLabel)
+			);
 
 			var labelBold = new Label {
 				Text = "Bold",
-				Font = Font.SystemFontOfSize (14, FontAttributes.Bold),
+				FontSize = 14, 
+				FontAttributes = FontAttributes.Bold,
 				VerticalOptions = LayoutOptions.CenterAndExpand,
 				HorizontalOptions = LayoutOptions.CenterAndExpand,
 			};
 			var labelItalic = new Label {
 				Text = "Italic",
-				Font = Font.SystemFontOfSize (14, FontAttributes.Italic),
+				FontSize = 14, 
+				FontAttributes = FontAttributes.Italic,
 				VerticalOptions = LayoutOptions.CenterAndExpand,
 				HorizontalOptions = LayoutOptions.CenterAndExpand,
 			};
 			var labelBoldItalic = new Label {
 				Text = "BoldItalic",
-				Font = Font.SystemFontOfSize (14, FontAttributes.Bold | FontAttributes.Italic),
+				FontSize = 14, 
+				FontAttributes = FontAttributes.Bold | FontAttributes.Italic,
 				VerticalOptions = LayoutOptions.CenterAndExpand,
 				HorizontalOptions = LayoutOptions.CenterAndExpand,
 			};
@@ -63,9 +78,9 @@ namespace WorkingWithFonts
 			// Span formatting support
 			var labelFormatted = new Label ();
 			var fs = new FormattedString ();
-			fs.Spans.Add (new Span { Text="Red, ", ForegroundColor = Color.Red, Font = Font.SystemFontOfSize(20, FontAttributes.Italic) });
-			fs.Spans.Add (new Span { Text=" blue, ", ForegroundColor = Color.Blue, Font = Font.SystemFontOfSize(32) });
-			fs.Spans.Add (new Span { Text=" and green!", ForegroundColor = Color.Green, Font = Font.SystemFontOfSize(12) });
+			fs.Spans.Add (new Span { Text="Red, ", ForegroundColor = Color.Red, FontSize = 20, FontAttributes = FontAttributes.Italic });
+			fs.Spans.Add (new Span { Text=" blue, ", ForegroundColor = Color.Blue, FontSize = 32 });
+			fs.Spans.Add (new Span { Text=" and green!", ForegroundColor = Color.Green, FontSize = 12 });
 			labelFormatted.FormattedText = fs;
 
 
--- WorkingWithFonts/WorkingWithFonts/FontPageXaml.xaml.cs
+++ WorkingWithFonts/WorkingWithFonts/FontPageXaml.xaml.cs
@@ -1,9 +1,17 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace WorkingWithFonts
 {	
+	/// <summary>
+	/// Using fonts in Xamarin.Forms 1.3 is slightly different to earlier versions.
+	/// The 'Font' property has been replaced with three separate properties:
+	/// - FontFamily
+	/// - FontSize
+	/// - FontAttributes
+	/// Use these three properties directly and avoid using the Font class.
+	/// </summary>
 	public partial class FontPageXaml : ContentPage
 	{	
 		public FontPageXaml ()
