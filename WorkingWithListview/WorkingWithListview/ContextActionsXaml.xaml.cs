731ec8c9fbc08daef8dd19d28fe74f06017b6bb0
biste5 <biste5@gmail.com>
2015-01-11 15:27:35 +0100

    ListView Context Item XAML Implementation

---
+++ WorkingWithListview/WorkingWithListview/ContextActionsXaml.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace WorkingWithListview
+{
+    public partial class ContextActionsXaml
+    {
+        private class BindItem
+        {
+            public string Title { get; set; }
+            public ICommand ContextItemTapped { get; set; }
+        }
+
+        public ContextActionsXaml()
+        {
+            InitializeComponent();
+
+            var showAlert = new Command<object>((parameter) =>
+            {
+                DisplayAlert("Context Item", string.Format("on item {0} Tapped", parameter), "Ok");
+            });
+
+            var items = new List<BindItem>();
+            items.Add(new BindItem { Title = "first", ContextItemTapped = showAlert });
+            items.Add(new BindItem { Title = "second", ContextItemTapped = showAlert });
+            items.Add(new BindItem { Title = "third", ContextItemTapped = showAlert });
+            this.BindingContext = items;
+
+        }
+
+
+    }
+}
