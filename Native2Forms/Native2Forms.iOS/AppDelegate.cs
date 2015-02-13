713518bede182ea715f657161323ea96716e1c26
olegoid <olegoid91@gmail.com>
2015-02-14 02:52:06 +0300

    [Forms2Native] Change type of the delegate

--- Native2Forms/Native2Forms.iOS/AppDelegate.cs
+++ Native2Forms/Native2Forms.iOS/AppDelegate.cs
@@ -14,7 +14,7 @@ namespace Native2Forms
 	// User Interface of the application, as well as listening (and optionally responding) to
 	// application events from iOS.
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : FormsApplicationDelegate
+	public partial class AppDelegate : UIApplicationDelegate
 	{
 		// class-level declarations
 		UIWindow window;
