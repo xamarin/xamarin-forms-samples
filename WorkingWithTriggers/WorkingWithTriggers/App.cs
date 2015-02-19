583eb0e4f04a199953cf9d324672867198417ce3
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-02-19 10:37:35 -0800

    [working-with] triggers update sample

--- WorkingWithTriggers/WorkingWithTriggers/App.cs
+++ WorkingWithTriggers/WorkingWithTriggers/App.cs
@@ -16,21 +16,21 @@ namespace WorkingWithTriggers
 			// NOTE: uncomment the relevant page that you'd like to test
 			//
 
-			// C# examples
-			var csTab = new TabbedPage ();
-			csTab.Children.Add(new SimpleTriggerPage {Title = "Property", Icon="csharp.png"});
-			csTab.Children.Add(new RequiredFieldTriggerPage { Title = "Data", Icon="csharp.png"});
+			// C# examples - triggers are really designed for XAML
+//			var csTab = new TabbedPage ();
+//			csTab.Children.Add(new SimpleTriggerPage {Title = "Property", Icon="csharp.png"});
+//			csTab.Children.Add(new RequiredFieldTriggerPage { Title = "Data", Icon="csharp.png"});
 //			csTab.Children.Add(new NumericValidationTriggerPage { Title = "Valid", Icon="csharp.png"});
-
 //			MainPage = csTab;
 
 
 			// Xaml examples
 			var xamlTab = new TabbedPage ();
-			xamlTab.Children.Add(new SimpleTriggerXaml { Title = "Property", Icon="xaml.png"});
-			xamlTab.Children.Add(new RequiredFieldDataTriggerXaml { Title = "Data", Icon="xaml.png"});
+			xamlTab.Children.Add(new PropertyTriggerXaml { Title = "Property", Icon="xaml.png"});
+			xamlTab.Children.Add(new DataTriggerXaml { Title = "Data", Icon="xaml.png"});
 			xamlTab.Children.Add(new NumericValidationTriggerXaml { Title = "Event1", Icon="xaml.png"});
 			xamlTab.Children.Add(new OnClickEventTriggerXaml { Title = "Event2", Icon="xaml.png"});
+			xamlTab.Children.Add(new MultiTriggerXaml { Title = "Multi", Icon="xaml.png"});
 
 			MainPage = xamlTab;
 		}
