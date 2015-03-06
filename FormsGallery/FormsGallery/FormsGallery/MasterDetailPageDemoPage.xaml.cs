020e8b0aca6c5d9173bc1c2e98192c3dffd70855
Michael-DST <michael@dragonspark.us>
2015-03-06 09:10:21 -0500

    Cleaning up MasterDetailsPage.

--- FormsGallery/FormsGallery/FormsGallery/MasterDetailPageDemoPage.xaml.cs
+++ FormsGallery/FormsGallery/FormsGallery/MasterDetailPageDemoPage.xaml.cs
@@ -1,31 +1,10 @@
-﻿
-using System;
-using Xamarin.Forms;
-
-namespace FormsGallery
+﻿namespace FormsGallery
 {
 	public partial class MasterDetailPageDemoPage
 	{
 		public MasterDetailPageDemoPage()
 		{
 			InitializeComponent();
-		}
-
-		void ListView_OnItemSelected( object sender, SelectedItemChangedEventArgs e )
-		{
-			// Show the detail page.
-			if ( e.SelectedItem != null )
-			{
-				IsPresented = false;
-			}
-		}
-
-		void MasterDetailPageDemoPage_OnIsPresentedChanged( object sender, EventArgs e )
-		{
-			if ( IsPresented )
-			{
-				Items.SelectedItem = null;
-			}
 		}
 	}
 }
