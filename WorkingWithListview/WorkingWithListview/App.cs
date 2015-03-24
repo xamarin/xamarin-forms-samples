8053f15f366ac5ab631c23a6e69241ab02720e48
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-03-24 15:08:38 -0700

    [working-with] listview add Header/Footer example

--- WorkingWithListview/WorkingWithListview/App.cs
+++ WorkingWithListview/WorkingWithListview/App.cs
@@ -16,24 +16,26 @@ namespace WorkingWithListview
 
 			// USE C#
 
-            var tabs = new TabbedPage { Title = "Working with ListView" };
-			tabs.Children.Add (new BasicListPage {Title="Basic", Icon = "csharp.png" });
-			tabs.Children.Add (new UnevenRowsPage {Title="Uneven", Icon = "csharp.png" });
-			tabs.Children.Add (new ContextActionsPage {Title="Context", Icon = "csharp.png" });
-			tabs.Children.Add (new CustomCellPage {Title="Button", Icon = "csharp.png" });
+			var tabsCs = new TabbedPage { Title = "Working with ListView" };
+			tabsCs.Children.Add (new BasicListPage {Title="Basic", Icon = "csharp.png" });
+			tabsCs.Children.Add (new UnevenRowsPage {Title="Uneven", Icon = "csharp.png" });
+			tabsCs.Children.Add (new ContextActionsPage {Title="Context", Icon = "csharp.png" });
+			tabsCs.Children.Add (new CustomCellPage {Title="Button", Icon = "csharp.png" });
+			tabsCs.Children.Add (new HeaderFooterPage {Title="HeadFoot", Icon = "csharp.png" });
 
-			MainPage = tabs;
+			MainPage = tabsCs;
 
 
-			// USE XAML - uncomment the MainPage line below to use the XAML version
+			// USE XAML - uncomment above or below MainPage line below to use the XAML or C# versions
 
 			var tabsXaml = new TabbedPage ();
 			tabsXaml.Children.Add (new BasicListXaml {Title="BasicX", Icon = "xaml.png" });
 			tabsXaml.Children.Add (new UnevenRowsXaml {Title="UnevenX", Icon = "xaml.png" });
 			tabsXaml.Children.Add (new ContextActionsXaml {Title="ContextX", Icon = "xaml.png" });
 			tabsXaml.Children.Add (new CustomCellXaml {Title="ButtonX", Icon = "xaml.png" });
+			tabsXaml.Children.Add (new HeaderFooterXaml {Title="HeadFootX", Icon = "xaml.png" });
 
-			MainPage = tabsXaml;
+			//MainPage = tabsXaml;
 
 		}
 	}
---
+++ WorkingWithListview/WorkingWithListview/HeaderFooter/HeaderFooterXaml.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WorkingWithListview
+{
+	class Monkey {
+		public string Name {get;set;}
+		public string Description {get;set;}
+	}
+
+	public partial class HeaderFooterXaml : ContentPage
+	{
+		public HeaderFooterXaml ()
+		{
+			InitializeComponent ();
+
+			listView.ItemsSource = new Monkey[] {
+				new Monkey {Name="a", Description="aaa"},
+				new Monkey {Name="b", Description="bbb"},
+				new Monkey {Name="c", Description="ccc"},
+				new Monkey {Name="d", Description="ddd"},
+				new Monkey {Name="e", Description="eee"},
+				new Monkey {Name="f", Description="fff"},
+//				new Monkey {Name="g", Description="ggg"},
+//				new Monkey {Name="i", Description="iii"},
+//				new Monkey {Name="j", Description="jjj"},
+//				new Monkey {Name="k", Description="kkk"},
+//				new Monkey {Name="l", Description="lll"},
+//				new Monkey {Name="m", Description="mmm"},
+//				new Monkey {Name="n", Description="nnn"},
+//				new Monkey {Name="o", Description="ooo"},
+			};
+		}
+	}
+}
+
