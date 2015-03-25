d5b9db2a5b3976a4889fd4fec85a50f8a1dc7c96
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-03-25 20:53:42 +0300

    [WorkingWithImages] fix warnings

--- WorkingWithImages/WorkingWithImages/EmbeddedImages.cs
+++ WorkingWithImages/WorkingWithImages/EmbeddedImages.cs
@@ -8,31 +8,34 @@ using System.Reflection;
 
 namespace WorkingWithImages
 {
-    public class EmbeddedImages : ContentPage
-    {
-        public EmbeddedImages()
-        {
+	public class EmbeddedImages : ContentPage
+	{
+		public EmbeddedImages ()
+		{
 			var embeddedImage = new Image { Aspect = Aspect.AspectFit };
 
 			// resource identifiers start with assembly-name DOT filename
-			embeddedImage.Source = ImageSource.FromResource("WorkingWithImages.beach.jpg");
-            
-            Content = new StackLayout
-            {
-                Children = {
-					new Label {Text = "ImageSource.FromResource", Font=Font.BoldSystemFontOfSize(NamedSize.Medium)},
+			embeddedImage.Source = ImageSource.FromResource ("WorkingWithImages.beach.jpg");
+
+			Content = new StackLayout {
+				Children = {
+					new Label {
+						Text = "ImageSource.FromResource",
+						FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label)),
+						FontAttributes = FontAttributes.Bold
+					},
 					embeddedImage, 
-					new Label {Text = "WorkingWithImages.beach.jpg embedded resource"}
-                },
-                Padding = new Thickness(0, 20, 0, 0),
-                VerticalOptions = LayoutOptions.StartAndExpand,
-                HorizontalOptions = LayoutOptions.CenterAndExpand
-            };
+					new Label { Text = "WorkingWithImages.beach.jpg embedded resource" }
+				},
+				Padding = new Thickness (0, 20, 0, 0),
+				VerticalOptions = LayoutOptions.StartAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand
+			};
 
 			// NOTE: use for debugging, not in released app code!
 			//var assembly = typeof(EmbeddedImages).GetTypeInfo().Assembly;
 			//foreach (var res in assembly.GetManifestResourceNames()) 
 			//	System.Diagnostics.Debug.WriteLine("found resource: " + res);
-        }
-    }
+		}
+	}
 }
