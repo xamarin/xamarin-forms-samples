cbc2c67ee22adc1862609e32a6f5a3a01e79bf65
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-03-25 18:50:07 +0300

    [SolitaireEncryption] fixed warnings

--- SolitaireEncryption/iOS/AppDelegate.cs
+++ SolitaireEncryption/iOS/AppDelegate.cs
@@ -13,16 +13,10 @@ namespace Solitaire
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : FormsApplicationDelegate
 	{
-		UIWindow window;
-
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			Forms.Init ();
-
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
-			
 			LoadApplication (new App());
-			
 			return base.FinishedLaunching (app, options);
 		}
 	}
