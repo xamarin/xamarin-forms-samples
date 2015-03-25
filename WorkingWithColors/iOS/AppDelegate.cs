2c754fd39e9114cc895c4761bd44775bfb04ad19
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-03-25 20:23:18 +0300

    [WorkingWithColors] fix warnings

--- WorkingWithColors/iOS/AppDelegate.cs
+++ WorkingWithColors/iOS/AppDelegate.cs
@@ -13,14 +13,10 @@ namespace WorkingWithColors.iOS
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : FormsApplicationDelegate
 	{
-		UIWindow window;
-
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			Forms.Init ();
-
 			LoadApplication (new App ());
-			
 			return base.FinishedLaunching (app, options);
 		}
 	}
