ba03e04390f54ee23352273694972193207eb3ac
David Britch <davidbritch@Davids-MacBook-Pro.local>
2015-07-20 17:52:07 +0100

    Phoneword and PhonewordMultiscreen samples added (XAML).

---
+++ Phoneword/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class MainPage : ContentPage
+    {
+        string translatedNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                    "Dial a Number",
+                    "Would you like to call " + translatedNumber + "?",
+                    "Yes",
+                    "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                    dialer.Dial(translatedNumber);
+            }
+        }
+    }
+}
