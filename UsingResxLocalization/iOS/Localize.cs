759de06f5a14ba670564c497e6c1e835c9088bf5
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-07-21 11:59:21 -0700

    [L10n] minor Portuguese fix

--- UsingResxLocalization/iOS/Localize.cs
+++ UsingResxLocalization/iOS/Localize.cs
@@ -28,28 +28,7 @@ namespace UsingResxLocalization.iOS
 
 		public System.Globalization.CultureInfo GetCurrentCultureInfo ()
 		{
-//			#region not sure why this isn't working for me (in simulator at least)
-//			var iosLocaleAuto = NSLocale.AutoUpdatingCurrentLocale.LocaleIdentifier;
-//			var iosLanguageAuto = NSLocale.AutoUpdatingCurrentLocale.LanguageCode;
-//			Console.WriteLine ("nslocaleid:" + iosLocaleAuto);
-//			Console.WriteLine ("nslanguage:" + iosLanguageAuto);
-//
-//			var iosLocale = NSLocale.CurrentLocale.LocaleIdentifier;
-//			var iosLanguage = NSLocale.CurrentLocale.LanguageCode;
-//
-//			var netLocale = iosLocale.Replace ("_", "-");
-//			var netLanguage = iosLanguage.Replace ("_", "-");
-//
-//			Console.WriteLine ("ios:" + iosLanguage + " " + iosLocale);
-//			Console.WriteLine ("net:" + netLanguage + " " +  netLocale);
-//
-//			// doesn't seem to affect anything (well, i didn't expect it to affect UIKit controls)
-////			var ci = new System.Globalization.CultureInfo ("JA-jp");
-////			System.Threading.Thread.CurrentThread.CurrentCulture = ci;
-
-//			#endregion
 			var netLanguage = "en";
-			// HACK: not sure why NSLocale isn't ever returning correct data
 			if (NSLocale.PreferredLanguages.Length > 0) {
 				var pref = NSLocale.PreferredLanguages [0];
 
@@ -64,7 +43,7 @@ namespace UsingResxLocalization.iOS
 						pref = "pt-PT"; // Portugal
 				}
 				netLanguage = pref.Replace ("_", "-");
-				Console.WriteLine ("preferred:" + netLanguage);
+				Console.WriteLine ("preferred language:" + netLanguage);
 			}
 			return new System.Globalization.CultureInfo(netLanguage);
 		}
