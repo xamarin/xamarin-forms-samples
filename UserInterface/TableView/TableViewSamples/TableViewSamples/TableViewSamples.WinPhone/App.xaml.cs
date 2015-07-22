96d4cb3fc6e8259636404c137ab60fa292c88c54
Nathan C. Castle <nathan.castle@outlook.com>
2015-07-22 07:59:41 -0700

    Made tableview sample work with win phone and added screenshots

--- UserInterface/TableView/TableViewSamples/TableViewSamples/TableViewSamples.WinPhone/App.xaml.cs
+++ UserInterface/TableView/TableViewSamples/TableViewSamples/TableViewSamples.WinPhone/App.xaml.cs
@@ -39,7 +39,7 @@ namespace TableViewSamples.WinPhone
             if (Debugger.IsAttached)
             {
                 // Display the current frame rate counters.
-                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+                //Application.Current.Host.Settings.EnableFrameRateCounter = true;
 
                 // Show the areas of the app that are being redrawn in each frame.
                 //Application.Current.Host.Settings.EnableRedrawRegions = true;
