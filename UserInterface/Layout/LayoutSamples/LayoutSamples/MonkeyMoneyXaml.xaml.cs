ee4ef800a1d9bfa7f786e38c7dd18c8896209870
Nathan C. Castle <nathan.castle@outlook.com>
2015-09-09 14:21:03 -0700

    first attempt at some layout samples

---
+++ UserInterface/Layout/LayoutSamples/LayoutSamples/MonkeyMoneyXaml.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LayoutSamples
+{
+	public partial class MonkeyMoneyXaml : ContentPage
+	{
+		public MonkeyMoneyXaml ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
