b2dbdb694a4e8f7b89777c6560687a1fa760b308
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-09-23 12:41:33 -0700

    [forms] L10n update for iOS
    https://bugzilla.xamarin.com/show_bug.cgi?id=34074

--- UsingResxLocalization/iOS/Localize.cs
+++ UsingResxLocalization/iOS/Localize.cs
@@ -29,13 +29,15 @@ namespace UsingResxLocalization.iOS
 		public System.Globalization.CultureInfo GetCurrentCultureInfo ()
 		{
 			var netLanguage = "en";
+			var prefLanguageOnly = "en";
 			if (NSLocale.PreferredLanguages.Length > 0) {
 				var pref = NSLocale.PreferredLanguages [0];
 
 				// HACK: Apple treats portuguese fallbacks in a strange way
 				// https://developer.apple.com/library/ios/documentation/MacOSX/Conceptual/BPInternational/LocalizingYourApp/LocalizingYourApp.html
 				// "For example, use pt as the language ID for Portuguese as it is used in Brazil and pt-PT as the language ID for Portuguese as it is used in Portugal"
-				if (pref.Substring(0,2) == "pt")
+				prefLanguageOnly = pref.Substring(0,2);
+				if (prefLanguageOnly == "pt")
 				{
 					if (pref == "pt")
 						pref = "pt-BR"; // get the correct Brazilian language strings from the PCL RESX (note the local iOS folder is still "pt")
@@ -45,7 +47,18 @@ namespace UsingResxLocalization.iOS
 				netLanguage = pref.Replace ("_", "-");
 				Console.WriteLine ("preferred language:" + netLanguage);
 			}
-			return new System.Globalization.CultureInfo(netLanguage);
+
+			// this gets called a lot - try/catch can be expensive so consider caching or something
+			System.Globalization.CultureInfo ci = null;
+			try {
+				ci = new System.Globalization.CultureInfo(netLanguage);
+			} catch {
+				// iOS locale not valid .NET culture (eg. "en-ES" : English in Spain)
+				// fallback to first characters, in this case "en"
+				ci = new System.Globalization.CultureInfo(prefLanguageOnly);
+			}
+				
+			return ci;
 		}
 	}
 }
