22b5427cfe6a52649595e419fcfbc9c86f0dca1d
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-11-30 20:57:40 -0800

    [todo] green theme

--- Todo/SharedProject/Todo/App.cs
+++ Todo/SharedProject/Todo/App.cs
@@ -13,9 +13,17 @@ namespace Todo
 			}
 		}
 
-		public App()
+		public App ()
 		{
-			MainPage = new NavigationPage (new TodoListPage ());
+			Resources = new ResourceDictionary ();
+			Resources.Add ("primaryGreen", Color.FromHex("91CA47"));
+			Resources.Add ("primaryDarkGreen", Color.FromHex ("6FA22E"));
+
+			var nav = new NavigationPage (new TodoListPage ());
+			nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+			nav.BarTextColor = Color.White;
+
+			MainPage = nav;
 		}
 	}
 }
