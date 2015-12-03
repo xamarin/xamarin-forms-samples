99d6a3828be56f9286b9dc880f945012752c8b58
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-12-03 14:33:45 -0800

    [todo] update launchscreen/tint
    also fix deprecated YAlign property

--- Todo/PCL/Todo/Views/TodoItemCell.cs
+++ Todo/PCL/Todo/Views/TodoItemCell.cs
@@ -8,31 +8,24 @@ namespace Todo
 		public TodoItemCell ()
 		{
 			var label = new Label {
-				YAlign = TextAlignment.Center
+				VerticalTextAlignment = TextAlignment.Center,
+				HorizontalOptions = LayoutOptions.StartAndExpand
 			};
 			label.SetBinding (Label.TextProperty, "Name");
 
 			var tick = new Image {
-				Source = FileImageSource.FromFile ("check.png"),
+				Source = FileImageSource.FromFile ("check"),
+				HorizontalOptions = LayoutOptions.End
 			};
 			tick.SetBinding (Image.IsVisibleProperty, "Done");
 
 			var layout = new StackLayout {
-				Padding = new Thickness(20, 0, 0, 0),
+				Padding = new Thickness(20, 0, 20, 0),
 				Orientation = StackOrientation.Horizontal,
-				HorizontalOptions = LayoutOptions.StartAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
 				Children = {label, tick}
 			};
 			View = layout;
-		}
-
-		protected override void OnBindingContextChanged ()
-		{
-			// Fixme : this is happening because the View.Parent is getting 
-			// set after the Cell gets the binding context set on it. Then it is inheriting
-			// the parents binding context.
-			View.BindingContext = BindingContext;
-			base.OnBindingContextChanged ();
 		}
 	}
 }
