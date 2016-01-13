8c8ce6e21c4772b7d14c5c8a93f8652ebf911cc7
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-01-13 13:56:26 -0800

    [todo] styling and updated deprecated YAlign

--- Todo/PCL/Todo.iOS/AppDelegate.cs
+++ Todo/PCL/Todo.iOS/AppDelegate.cs
@@ -16,6 +16,9 @@ namespace Todo
 	{
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
+			// affects all UISwitch controls in the app
+			UISwitch.Appearance.OnTintColor = UIColor.FromRGB(0x91, 0xCA, 0x47);
+
 			Forms.Init ();
 			LoadApplication (new App ());
 			return base.FinishedLaunching (app, options);
--- Todo/SharedProject/Todo/Views/TodoItemCell.cs
+++ Todo/SharedProject/Todo/Views/TodoItemCell.cs
@@ -8,34 +8,27 @@ namespace Todo
 		public TodoItemCell ()
 		{
 			var label = new Label {
-				YAlign = TextAlignment.Center
+				VerticalTextAlignment = TextAlignment.Center,
+				HorizontalOptions = LayoutOptions.StartAndExpand
 			};
 
 			label.SetBinding (Label.TextProperty, "Name");
 
 			var tick = new Image {
-				Source = FileImageSource.FromFile ("check.png")
+				Source = FileImageSource.FromFile ("check.png"),
+				HorizontalOptions = LayoutOptions.End
 			};
 
 			tick.SetBinding (Image.IsVisibleProperty, "Done");
 
 			var layout = new StackLayout {
-				Padding = new Thickness (20, 0, 0, 0),
+				Padding = new Thickness (20, 0, 20, 0),
 				Orientation = StackOrientation.Horizontal,
 				HorizontalOptions = LayoutOptions.StartAndExpand,
 				Children = { label, tick }
 			};
 
 			View = layout;
-		}
-
-		protected override void OnBindingContextChanged ()
-		{
-			// Fixme : this is happening because the View.Parent is getting 
-			// set after the Cell gets the binding context set on it. Then it is inheriting
-			// the parents binding context.
-			View.BindingContext = BindingContext;
-			base.OnBindingContextChanged ();
 		}
 	}
 }
