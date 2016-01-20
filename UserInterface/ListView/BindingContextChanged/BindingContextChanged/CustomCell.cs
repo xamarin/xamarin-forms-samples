5620f5637fcb8cb6148704ece2f94106becf9d02
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-01-20 13:05:49 +0000

    [BindingContextChanged] ListView custom cell binding context changed sample added.

---
+++ UserInterface/ListView/BindingContextChanged/BindingContextChanged/CustomCell.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace BindingContextChanged
+{
+	public class CustomCell : ViewCell
+	{
+		public static readonly BindableProperty TitleProperty = BindableProperty.Create<CustomCell, string> (p => p.Title, "Test");
+
+		public string Title {
+			get { return(string)GetValue (TitleProperty); }
+			set { SetValue (TitleProperty, value); }
+		}
+
+		//		public CustomCell ()
+		//		{
+		//			View = new Label { Text = Title };
+		//		}
+
+		protected override void OnBindingContextChanged ()
+		{
+			base.OnBindingContextChanged ();
+
+			if (BindingContext != null) {
+				View = new Label { Text = Title };
+			}
+		}
+	}
+}
---
+++ UserInterface/ListView/BindingContextChanged/BindingContextChanged/ListPageCS.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BindingContextChanged
+{
+	public class ListPageCS : ContentPage
+	{
+		public ListPageCS ()
+		{
+			var customCell = new DataTemplate (typeof(CustomCell));
+			customCell.SetBinding (CustomCell.TitleProperty, ".");
+
+			var listView = new ListView {
+				ItemsSource = new List<string> { "Apples", "Oranges", "Pears", "Bananas", "Mangos" },
+				ItemTemplate = customCell
+			};
+
+			Content = new StackLayout { 
+				Padding = new Thickness (0, 20, 0, 0),
+				Children = {
+					new Label {
+						Text = "Binding Context Changed Demo",
+						HorizontalOptions = LayoutOptions.Center,
+						FontAttributes = FontAttributes.Bold
+					},
+					listView
+				}
+			};
+		}
+	}
+}
