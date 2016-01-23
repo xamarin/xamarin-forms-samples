318800b8f338fd54b9dfe3289ee0ddd54f9002df
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-01-22 21:43:11 -0800

    [todo] add Windows+UWP projects

---
+++ Todo/PCL/Todo.UWP/SQLite_UWP.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Todo;
+using Xamarin.Forms;
+using Windows.Storage;
+using Todo.UWP;
+
+[assembly: Dependency (typeof (SQLite_UWP))]
+
+namespace Todo.UWP
+{
+	public class SQLite_UWP : ISQLite
+	{
+		public SQLite_UWP ()
+		{
+		}
+		#region ISQLite implementation
+		public SQLite.SQLiteConnection GetConnection ()
+		{
+			var sqliteFilename = "TodoSQLite.db3";
+            string path = Path.Combine(ApplicationData.Current.LocalFolder.Path, sqliteFilename);
+
+            var conn = new SQLite.SQLiteConnection(path);
+
+			// Return the database connection 
+			return conn;
+		}
+		#endregion
+	}
+}
--- Todo/PCL/Todo/Views/TodoListPage.cs
+++ Todo/PCL/Todo/Views/TodoListPage.cs
@@ -30,7 +30,7 @@ namespace Todo
 			};
 
 			var layout = new StackLayout();
-			if (Device.OS == TargetPlatform.WinPhone) { // WinPhone doesn't have the title showing
+			if (Device.OS == TargetPlatform.WinPhone || Device.OS == TargetPlatform.Windows) { // WinPhone doesn't have the title showing
 				layout.Children.Add(new Label{
 					Text="Todo", 
 					FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))});
@@ -59,7 +59,7 @@ namespace Todo
 					Navigation.PushAsync(todoPage);
 				}, 0, 0);
 			}
-			if (Device.OS == TargetPlatform.WinPhone)
+			if (Device.OS == TargetPlatform.WinPhone || Device.OS == TargetPlatform.Windows)
 			{
 				tbi = new ToolbarItem("Add", "add.png", () =>
 					{
