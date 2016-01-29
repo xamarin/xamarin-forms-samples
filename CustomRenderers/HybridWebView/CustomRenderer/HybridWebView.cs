d84ef1d795c41d84d9bfd73087bf7a4b8f8f2186
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-01-28 17:45:31 -0800

    [customrenderer] fix deprecated BindableProperty method

--- CustomRenderers/HybridWebView/CustomRenderer/HybridWebView.cs
+++ CustomRenderers/HybridWebView/CustomRenderer/HybridWebView.cs
@@ -7,8 +7,12 @@ namespace CustomRenderer
 	{
 		Action<string> action;
 
-		public static readonly BindableProperty UriProperty = BindableProperty.Create<HybridWebView, string> (p => p.Uri, default(string));
-
+		public static readonly BindableProperty UriProperty = BindableProperty.Create (
+			propertyName: "Uri",
+			returnType: typeof(string),
+			declaringType: typeof(HybridWebView),
+			defaultValue: default(string));
+		
 		public string Uri {
 			get { return (string)GetValue (UriProperty); }
 			set { SetValue (UriProperty, value); }
