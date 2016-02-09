0ed90408329ba50cd031dafc4801075015784705
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-02-09 16:38:16 +0000

    [BindingContextChanged] Sample updated.

--- UserInterface/ListView/BindingContextChanged/BindingContextChanged/CustomCell.cs
+++ UserInterface/ListView/BindingContextChanged/BindingContextChanged/CustomCell.cs
@@ -4,24 +4,53 @@ namespace BindingContextChanged
 {
 	public class CustomCell : ViewCell
 	{
-		public static readonly BindableProperty TitleProperty = BindableProperty.Create<CustomCell, string> (p => p.Title, "Test");
+		Label nameLabel, ageLabel, locationLabel;
 
-		public string Title {
-			get { return(string)GetValue (TitleProperty); }
-			set { SetValue (TitleProperty, value); }
+		public static readonly BindableProperty NameProperty = BindableProperty.Create ("Name", typeof(string), typeof(CustomCell), "Name");
+		public static readonly BindableProperty AgeProperty = BindableProperty.Create ("Age", typeof(int), typeof(CustomCell), 0);
+		public static readonly BindableProperty LocationProperty = BindableProperty.Create ("Location", typeof(string), typeof(CustomCell), "Location");
+
+		public string Name {
+			get { return(string)GetValue (NameProperty); }
+			set { SetValue (NameProperty, value); }
 		}
 
-		//		public CustomCell ()
-		//		{
-		//			View = new Label { Text = Title };
-		//		}
+		public int Age {
+			get { return(int)GetValue (AgeProperty); }
+			set { SetValue (AgeProperty, value); }
+		}
+
+		public string Location {
+			get { return(string)GetValue (LocationProperty); }
+			set { SetValue (LocationProperty, value); }
+		}
+
+		public CustomCell ()
+		{
+			var grid = new Grid { Padding = new Thickness (10) };
+			grid.ColumnDefinitions.Add (new ColumnDefinition { Width = new GridLength (0.5, GridUnitType.Star) });
+			grid.ColumnDefinitions.Add (new ColumnDefinition { Width = new GridLength (0.2, GridUnitType.Star) });
+			grid.ColumnDefinitions.Add (new ColumnDefinition { Width = new GridLength (0.3, GridUnitType.Star) });
+
+			nameLabel = new Label { FontAttributes = FontAttributes.Bold };
+			ageLabel = new Label ();
+			locationLabel = new Label { HorizontalTextAlignment = TextAlignment.End };
+
+			grid.Children.Add (nameLabel);
+			grid.Children.Add (ageLabel, 1, 0);
+			grid.Children.Add (locationLabel, 2, 0);
+
+			View = grid;
+		}
 
 		protected override void OnBindingContextChanged ()
 		{
 			base.OnBindingContextChanged ();
-
+		
 			if (BindingContext != null) {
-				View = new Label { Text = Title };
+				nameLabel.Text = Name;
+				ageLabel.Text = Age.ToString ();
+				locationLabel.Text = Location;
 			}
 		}
 	}
--- UserInterface/ListView/BindingContextChanged/BindingContextChanged/ListPage.xaml.cs
+++ UserInterface/ListView/BindingContextChanged/BindingContextChanged/ListPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace BindingContextChanged
@@ -8,7 +9,20 @@ namespace BindingContextChanged
 		public ListPage ()
 		{
 			InitializeComponent ();
-			listView.ItemsSource = new List<string> { "Apples", "Oranges", "Pears", "Bananas", "Mangos" };
+		}
+
+		void OnButtonClicked (object sender, EventArgs e)
+		{
+			var people = new List<Person> {
+				new Person ("Steve", 21, "USA"),
+				new Person ("John", 37, "USA"),
+				new Person ("Tom", 42, "UK"),
+				new Person ("Lucas", 29, "Germany"),
+				new Person ("Tariq", 39, "UK"),
+				new Person ("Jane", 30, "USA")
+			};
+
+			listView.ItemsSource = people;
 		}
 	}
 }
