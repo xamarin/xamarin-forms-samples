a1cbf6a022c5a05b385c8ac0b72dbff000f03ca4
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-02-09 16:48:04 +0000

    [BindingContextChanged] Changes cherry picked to 2.1.0 branch.

--- UserInterface/ListView/BindingContextChanged/BindingContextChanged/ListPageCS.cs
+++ UserInterface/ListView/BindingContextChanged/BindingContextChanged/ListPageCS.cs
@@ -8,14 +8,29 @@ namespace BindingContextChanged
 		public ListPageCS ()
 		{
 			var customCell = new DataTemplate (typeof(CustomCell));
-			customCell.SetBinding (CustomCell.TitleProperty, ".");
+			customCell.SetBinding (CustomCell.NameProperty, "Name");
+			customCell.SetBinding (CustomCell.AgeProperty, "Age");
+			customCell.SetBinding (CustomCell.LocationProperty, "Location");
 
 			var listView = new ListView {
-				ItemsSource = new List<string> { "Apples", "Oranges", "Pears", "Bananas", "Mangos" },
 				ItemTemplate = customCell
 			};
 
-			Content = new StackLayout { 
+			var button = new Button { Text = "Change Binding Context" };
+			button.Clicked += (sender, e) => {
+				var people = new List<Person> {
+					new Person ("Steve", 21, "USA"),
+					new Person ("John", 37, "USA"),
+					new Person ("Tom", 42, "UK"),
+					new Person ("Lucas", 29, "Germany"),
+					new Person ("Tariq", 39, "UK"),
+					new Person ("Jane", 30, "USA")
+				};
+
+				listView.ItemsSource = people;
+			};
+
+			Content = new StackLayout {
 				Padding = new Thickness (0, 20, 0, 0),
 				Children = {
 					new Label {
@@ -23,7 +38,8 @@ namespace BindingContextChanged
 						HorizontalOptions = LayoutOptions.Center,
 						FontAttributes = FontAttributes.Bold
 					},
-					listView
+					listView,
+					button
 				}
 			};
 		}
---
+++ UserInterface/ListView/BindingContextChanged/BindingContextChanged/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace BindingContextChanged
+{
+	public class Person
+	{
+		public string Name { get; private set; }
+
+		public int Age { get; private set; }
+
+		public string Location { get; private set; }
+
+		public Person (string name, int age, string location)
+		{
+			Name = name;
+			Age = age;
+			Location = location;
+		}
+	}
+}
+
