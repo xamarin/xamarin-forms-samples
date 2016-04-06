011b63325fb2a0356e9b149bd63d385a7d22db16
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-04-06 14:40:54 +0100

    [Behaviors] New samples added.

---
+++ Behaviors/NumericValidationBehaviorStyle/WorkingWithBehaviors.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+namespace WorkingWithBehaviors
+{
+    [Activity(Label = "Behaviors", 
+		MainLauncher = true,
+		ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize
+	)]
+	public class MainActivity : FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication (new App ());
+        }
+    }
+}
+
