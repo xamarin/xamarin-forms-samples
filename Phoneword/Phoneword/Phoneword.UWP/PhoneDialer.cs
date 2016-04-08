4dbca0977358e81f5debfc0de9b791b3f4f0bd6d
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-04-08 17:14:30 +0100

    [UWP] UWP support added to Styles, Phoneword, and Template samples.

---
+++ Phoneword/Phoneword/Phoneword.UWP/PhoneDialer.cs
@@ -0,0 +1,45 @@
+﻿using Phoneword.UWP;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Calls;
+using Windows.UI.Popups;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace Phoneword.UWP
+{
+    public class PhoneDialer : IDialer
+    {
+        bool dialled = false;
+
+        public bool Dial(string number)
+        {
+            DialNumber(number);
+            return dialled;
+        }
+
+        async void DialNumber(string number)
+        {
+            var phoneLine = await GetDefaultPhoneLineAsync();
+            if (phoneLine != null)
+            {
+                phoneLine.Dial(number, number);
+                dialled = true;
+            }
+            else
+            {
+                var dialog = new MessageDialog("No line found to place the call");
+                await dialog.ShowAsync();
+                dialled = false;
+            }
+        }
+
+
+        async Task<PhoneLine> GetDefaultPhoneLineAsync()
+        {
+            var phoneCallStore = await PhoneCallManager.RequestStoreAsync();
+            var lineId = await phoneCallStore.GetDefaultLineAsync();
+            return await PhoneLine.FromIdAsync(lineId);
+        }
+    }
+}
---
+++ Templates/DataTemplateSelector/UWP/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Selector.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.LoadApplication(new Selector.App());
+        }
+    }
+}
