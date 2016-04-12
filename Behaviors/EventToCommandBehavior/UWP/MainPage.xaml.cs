2c3c89a08639ce45ca6cb96b164fb11f72bd4b15
David Britch <davidbritch@Davids-MacBook-Pro.local>
2016-04-12 09:33:12 +0100

    [Behaviors] Added UWP projects to Behaviors samples.

---
+++ Behaviors/EventToCommandBehavior/UWP/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using PCL = EventToCommandBehavior;
+
+namespace EventToCommandBehavior.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.LoadApplication(new PCL.App());
+        }
+    }
+}
