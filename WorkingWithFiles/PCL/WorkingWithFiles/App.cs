ec525a04f2270ba3aeb6745c5b385fc23b41b157
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-04-14 22:00:47 -0700

    [working-with] files, add Json example

--- WorkingWithFiles/PCL/WorkingWithFiles/App.cs
+++ WorkingWithFiles/PCL/WorkingWithFiles/App.cs
@@ -13,6 +13,8 @@ namespace WorkingWithFiles
 
 			tabs.Children.Add (new LoadResourceXml {Title = "Resource", Icon = "xml.png"});
 
+			tabs.Children.Add(new LoadResourceJson { Title = "Resource", Icon = "json.png" });
+
 			tabs.Children.Add (new SaveAndLoadText {Title = "Save/Load", Icon = "saveload.png"});
 
 			MainPage = tabs;
