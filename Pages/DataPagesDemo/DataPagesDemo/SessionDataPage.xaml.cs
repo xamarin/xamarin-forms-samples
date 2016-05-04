5ad0b5c5df99977e65c3ffa0a370af06ca3e075a
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-05-04 15:43:29 -0700

    [datapages] demo sample from Evolve

---
+++ Pages/DataPagesDemo/DataPagesDemo/SessionDataPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DataPagesDemo
+{
+	public partial class SessionDataPage : Xamarin.Forms.Pages.ListDataPage
+	{
+		public SessionDataPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ Pages/DataPagesDemo/iOS/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace DataPagesDemo.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+			LoadApplication (new App ());
+
+			var x = typeof(Xamarin.Forms.Themes.DarkThemeResources);
+			x = typeof(Xamarin.Forms.Themes.LightThemeResources);
+			x = typeof(Xamarin.Forms.Themes.iOS.UnderlineEffect);
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
