470c6ab0a70e0e73c2d8ea7915ec6584e61224e1
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-05-04 17:19:56 -0700

    [themes] basic demo

---
+++ Themes/ThemesDemo/Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ThemesDemo.Droid
+{
+	[Activity (Label = "ThemesDemo.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+
+			var x = typeof(Xamarin.Forms.Themes.DarkThemeResources);
+			x = typeof(Xamarin.Forms.Themes.LightThemeResources);
+			x = typeof(Xamarin.Forms.Themes.Android.UnderlineEffect);
+		}
+	}
+}
+
---
+++ Themes/ThemesDemo/README.md
@@ -0,0 +1,8 @@
+ThemesDemo
+=========
+
+This sample demonstrates the new Themes functionality in Xamarin.Forms 2.3.
+
+Refer to the [getting started doc](https://developer.xamarin.com/guides/xamarin-forms/themes/) for more info.
+
+![light theme example](Screenshots/basic-light-sml.png) ![dark theme example](Screenshots/basic-dark-sml.png)
