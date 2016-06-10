d6ebf298052e0c06112a22741c907f49c2f3c334
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-06-10 13:49:56 -0700

    [working-with] files, add WinRT projects

---
+++ WorkingWithFiles/PCL/WinPhone81/SaveAndLoad_WinApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using WorkingWithFiles;
+using Windows.Storage;
+using WinPhone81;
+
+[assembly: Dependency(typeof(SaveAndLoad_WinApp))]
+
+namespace WinPhone81
+{
+    // https://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh758325.aspx
+
+    public class SaveAndLoad_WinApp : ISaveAndLoad
+    {
+
+        #region ISaveAndLoad implementation
+
+        public async Task SaveTextAsync(string filename, string text)
+        {
+            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await localFolder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(sampleFile, text);
+        }
+
+        public async Task<string> LoadTextAsync(string filename)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await storageFolder.GetFileAsync(filename);
+            string text = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+            return text;
+        }
+
+        #endregion
+    }
+}
