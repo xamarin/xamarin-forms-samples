36a922365e8f2c94c82da635b116e6f8c5996f29
David Britch <davidbritch@Davids-MBP.lan>
2016-08-09 10:53:45 +0100

    [Map] Sample updated to ensure custom pin displays correctly in iOS9.

---
+++ CustomRenderers/Map/iOS/CustomMKAnnotationView.cs
@@ -0,0 +1,16 @@
+﻿using MapKit;
+
+namespace CustomRenderer.iOS
+{
+	public class CustomMKAnnotationView : MKAnnotationView
+	{
+		public string Id { get; set; }
+
+		public string Url { get; set; }
+
+		public CustomMKAnnotationView(IMKAnnotation annotation, string id)
+			: base(annotation, id)
+		{
+		}
+	}
+}
