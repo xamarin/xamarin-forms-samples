f6db4332f357c686ba9abbe3c4b5252d67e0f7c7
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-09-02 22:24:26 -0700

    [Localization] improve locale handling iOS Android

--- UsingResxLocalization/iOS/Localize.cs
+++ UsingResxLocalization/iOS/Localize.cs
@@ -1,8 +1,7 @@
 using System;
-using Xamarin.Forms;
-using Foundation;
-using System.Runtime.CompilerServices;
+using System.Globalization;
 using System.Threading;
+using Foundation;
 
 [assembly:Xamarin.Forms.Dependency(typeof(UsingResxLocalization.iOS.Localize))]
 
@@ -10,56 +9,93 @@ namespace UsingResxLocalization.iOS
 {
 	public class Localize : UsingResxLocalization.ILocalize
 	{
-		public void SetLocale ()
+		public void SetLocale (CultureInfo ci)
 		{
-			var iosLocaleAuto = NSLocale.AutoUpdatingCurrentLocale.LocaleIdentifier;
-			var netLocale = iosLocaleAuto.Replace ("_", "-");
-			System.Globalization.CultureInfo ci;
-			try {
-				ci = new System.Globalization.CultureInfo (netLocale);
-			} catch {
-				ci = GetCurrentCultureInfo ();
-			}
 			Thread.CurrentThread.CurrentCulture = ci;
 			Thread.CurrentThread.CurrentUICulture = ci;
 
-			Console.WriteLine ("SetLocale: " + ci.Name);
+			Console.WriteLine ("CurrentCulture set: " + ci.Name);
 		}
 
-		public System.Globalization.CultureInfo GetCurrentCultureInfo ()
+		public CultureInfo GetCurrentCultureInfo ()
 		{
 			var netLanguage = "en";
-			var prefLanguageOnly = "en";
-			if (NSLocale.PreferredLanguages.Length > 0) {
+			if (NSLocale.PreferredLanguages.Length > 0)
+			{
 				var pref = NSLocale.PreferredLanguages [0];
 
-				// HACK: Apple treats portuguese fallbacks in a strange way
-				// https://developer.apple.com/library/ios/documentation/MacOSX/Conceptual/BPInternational/LocalizingYourApp/LocalizingYourApp.html
-				// "For example, use pt as the language ID for Portuguese as it is used in Brazil and pt-PT as the language ID for Portuguese as it is used in Portugal"
-				prefLanguageOnly = pref.Substring(0,2);
-				if (prefLanguageOnly == "pt")
-				{
-					if (pref == "pt")
-						pref = "pt-BR"; // get the correct Brazilian language strings from the PCL RESX (note the local iOS folder is still "pt")
-					else
-						pref = "pt-PT"; // Portugal
-				}
-				netLanguage = pref.Replace ("_", "-");
-				Console.WriteLine ("preferred language:" + netLanguage);
+				netLanguage = iOSToDotnetLanguage(pref);
 			}
 
 			// this gets called a lot - try/catch can be expensive so consider caching or something
 			System.Globalization.CultureInfo ci = null;
-			try {
+			try 
+			{
 				ci = new System.Globalization.CultureInfo(netLanguage);
-			} catch {
+			}
+			catch (CultureNotFoundException e1)
+			{
 				// iOS locale not valid .NET culture (eg. "en-ES" : English in Spain)
 				// fallback to first characters, in this case "en"
-				ci = new System.Globalization.CultureInfo(prefLanguageOnly);
+				try
+				{
+					var fallback = ToDotnetFallbackLanguage(new PlatformCulture(netLanguage));
+					Console.WriteLine(netLanguage + " failed, trying " + fallback + " (" + e1.Message + ")");
+					ci = new System.Globalization.CultureInfo(fallback);
+				}
+				catch (CultureNotFoundException e2)
+				{
+					// iOS language not valid .NET culture, falling back to English
+					Console.WriteLine(netLanguage + " couldn't be set, using 'en' (" + e2.Message + ")");
+					ci = new System.Globalization.CultureInfo("en");
+				}
 			}
 				
 			return ci;
 		}
+
+		string iOSToDotnetLanguage(string iOSLanguage)
+		{
+			Console.WriteLine("iOS Language:" + iOSLanguage);
+			var netLanguage = iOSLanguage; 
+
+			//certain languages need to be converted to CultureInfo equivalent
+			switch (iOSLanguage)
+			{
+				case "ms-MY":   // "Malaysian (Malaysia)" not supported .NET culture
+				case "ms-SG":	// "Malaysian (Singapore)" not supported .NET culture
+					netLanguage = "ms"; // closest supported
+					break;
+				case "gsw-CH":  // "Schwiizertüütsch (Swiss German)" not supported .NET culture
+					netLanguage = "de-CH"; // closest supported
+					break;
+				// add more application-specific cases here (if required)
+				// ONLY use cultures that have been tested and known to work
+			}
+
+			Console.WriteLine(".NET Language/Locale:" + netLanguage);
+			return netLanguage;
+		}
+		string ToDotnetFallbackLanguage (PlatformCulture platCulture)
+		{
+			Console.WriteLine(".NET Fallback Language:" + platCulture.LanguageCode);
+			var netLanguage = platCulture.LanguageCode; // use the first part of the identifier (two chars, usually);
+
+			switch (platCulture.LanguageCode)
+			{
+				// 
+				case "pt":
+					netLanguage = "pt-PT"; // fallback to Portuguese (Portugal)
+					break;
+				case "gsw":
+					netLanguage = "de-CH"; // equivalent to German (Switzerland) for this app
+					break;
+				// add more application-specific cases here (if required)
+				// ONLY use cultures that have been tested and known to work
+			}
+		
+			Console.WriteLine(".NET Fallback Language/Locale:" + netLanguage + " (application-specific)");
+			return netLanguage;
+		}
 	}
 }
-
