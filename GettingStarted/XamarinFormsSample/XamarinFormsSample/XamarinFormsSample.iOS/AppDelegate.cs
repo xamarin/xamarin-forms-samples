b6c08864fc9ff72a1fd8f46632ff79679c30b688
olegoid <olegoid91@gmail.com>
2016-09-08 17:11:47 -0300

    [XamarinFormsSample] Migrate sample to unified

--- GettingStarted/XamarinFormsSample/XamarinFormsSample/XamarinFormsSample.iOS/AppDelegate.cs
+++ GettingStarted/XamarinFormsSample/XamarinFormsSample/XamarinFormsSample.iOS/AppDelegate.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
+using Foundation;
+using UIKit;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
