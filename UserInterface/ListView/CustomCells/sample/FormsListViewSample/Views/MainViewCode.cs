8c7f051a33af59b7c7f54b3c03506a1279f8bfe2
Gonzalo Martin <gonzalo.martin@globallogic.com>
2016-10-20 16:23:33 -0300

    Updating PR based on feedback
    - Using tabs instead of space
    - Added screenshots for all the platforms

--- UserInterface/ListView/CustomCells/sample/FormsListViewSample/Views/MainViewCode.cs
+++ UserInterface/ListView/CustomCells/sample/FormsListViewSample/Views/MainViewCode.cs
@@ -4,60 +4,56 @@ using Xamarin.Forms;
 
 namespace FormsListViewSample
 {
-    public class MainViewCode : ContentPage
-    {
-        public ObservableCollection<VeggieViewModel> veggies { get; set; }
-        public MainViewCode()
-        {
-            veggies = new ObservableCollection<VeggieViewModel>();
-            ListView lstView = new ListView();
-            lstView.RowHeight = 60;
-            this.Title = "ListView Code Sample";
+		public class MainViewCode : ContentPage
+		{
+				public ObservableCollection<VeggieViewModel> veggies { get; set; }
+				public MainViewCode()
+				{
+					veggies = new ObservableCollection<VeggieViewModel>();
+					ListView lstView = new ListView();
+					lstView.RowHeight = 60;
+					this.Title = "ListView Code Sample";
+					lstView.ItemTemplate = new DataTemplate(typeof(CustomVeggieCell));
+					veggies.Add(new VeggieViewModel { Name = "Tomato", Type = "Fruit", Image = "tomato.png" });
+					veggies.Add(new VeggieViewModel { Name = "Romaine Lettuce", Type = "Vegetable", Image = "lettuce.png" });
+					veggies.Add(new VeggieViewModel { Name = "Zucchini", Type = "Vegetable", Image = "zucchini.png" });
+					lstView.ItemsSource = veggies;
+					Content = lstView;
+				}
 
-            lstView.ItemTemplate = new DataTemplate(typeof(CustomVeggieCell));
+				public class CustomVeggieCell : ViewCell
+				{
+						public CustomVeggieCell()
+						{
+							//instantiate each of our views
+							var image = new Image();
+							var nameLabel = new Label();
+							var typeLabel = new Label();
+							var verticaLayout = new StackLayout();
+							var horizontalLayout = new StackLayout() { BackgroundColor = Color.Olive };
 
-            veggies.Add(new VeggieViewModel { Name = "Tomato", Type = "Fruit", Image = "tomato.png" });
-            veggies.Add(new VeggieViewModel { Name = "Romaine Lettuce", Type = "Vegetable", Image = "lettuce.png" });
-            veggies.Add(new VeggieViewModel { Name = "Zucchini", Type = "Vegetable", Image = "zucchini.png" });
+							//set bindings
+							nameLabel.SetBinding(Label.TextProperty, new Binding("Name"));
+							typeLabel.SetBinding(Label.TextProperty, new Binding("Type"));
+							image.SetBinding(Image.SourceProperty, new Binding("Image"));
 
-            lstView.ItemsSource = veggies;
-            Content = lstView;
-        }
+							//Set properties for desired design
+							horizontalLayout.Orientation = StackOrientation.Horizontal;
+							horizontalLayout.HorizontalOptions = LayoutOptions.Fill;
+							image.HorizontalOptions = LayoutOptions.End;
+							nameLabel.FontSize = 24;
 
-        public class CustomVeggieCell : ViewCell
-        {
-            public CustomVeggieCell()
-            {
-                //instantiate each of our views
-                var image = new Image();
-                var nameLabel = new Label();
-                var typeLabel = new Label();
-                var verticaLayout = new StackLayout();
-                var horizontalLayout = new StackLayout() { BackgroundColor = Color.Olive };
+							//add views to the view hierarchy
+							verticaLayout.Children.Add(nameLabel);
+							verticaLayout.Children.Add(typeLabel);
+							horizontalLayout.Children.Add(verticaLayout);
+							horizontalLayout.Children.Add(image);
 
-                //set bindings
-                nameLabel.SetBinding(Label.TextProperty, new Binding("Name"));
-                typeLabel.SetBinding(Label.TextProperty, new Binding("Type"));
-                image.SetBinding(Image.SourceProperty, new Binding("Image"));
-
-                //Set properties for desired design
-                horizontalLayout.Orientation = StackOrientation.Horizontal;
-                horizontalLayout.HorizontalOptions = LayoutOptions.Fill;
-                image.HorizontalOptions = LayoutOptions.End;
-                nameLabel.FontSize = 24;
-
-                //add views to the view hierarchy
-                verticaLayout.Children.Add(nameLabel);
-                verticaLayout.Children.Add(typeLabel);
-                horizontalLayout.Children.Add(verticaLayout);
-                horizontalLayout.Children.Add(image);
-
-                // add to parent view
-                View = horizontalLayout;
-            }
-
-        }
-    }
+							// add to parent view
+							View = horizontalLayout;
+						}
+				}
+		}
 }
 
 
