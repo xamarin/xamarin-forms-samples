1d1d6e87e088706ef79dc8f2c16dd69d41ff7944
David Britch <davidbritch@Davids-MBP.lan>
2016-10-21 16:11:54 +0100

    [Phoneword] Enabled XAMLC and removed some unused namespaces.

--- PhonewordMultiscreen/Phoneword/Phoneword/App.cs
+++ PhonewordMultiscreen/Phoneword/Phoneword/App.cs
@@ -1,30 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Phoneword
 {
 	public class App : Application
 	{
 		public static List<string> PhoneNumbers { get; set; }
 
-		public App ()
+		public App()
 		{
-			PhoneNumbers = new List<string> ();
-			MainPage = new NavigationPage (new Phoneword.MainPage ());
+			PhoneNumbers = new List<string>();
+			MainPage = new NavigationPage(new Phoneword.MainPage());
 		}
 
-		protected override void OnStart ()
+		protected override void OnStart()
 		{
 			// Handle when your app starts
 		}
 
-		protected override void OnSleep ()
+		protected override void OnSleep()
 		{
 			// Handle when your app sleeps
 		}
 
-		protected override void OnResume ()
+		protected override void OnResume()
 		{
 			// Handle when your app resumes
 		}
