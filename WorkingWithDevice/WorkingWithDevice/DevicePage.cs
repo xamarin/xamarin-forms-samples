ae98de75af96a88d66702d4a8d8f65b24b467e07
David Britch <davidbritch@Davids-MBP.lan>
2017-01-18 14:11:29 +0000

    [WorkingWithDevice] Updated to use the Forms 2.3.4 Device class.

--- WorkingWithDevice/WorkingWithDevice/DevicePage.cs
+++ WorkingWithDevice/WorkingWithDevice/DevicePage.cs
@@ -10,120 +10,165 @@ namespace WorkingWithPlatformSpecifics
 		/// to perform platform-specific operations like changing the UI
 		/// based on which 
 		/// </summary>
-		public DevicePage ()
+		public DevicePage()
 		{
-
-			var heading = new Label { 
-				Text = "Heading", 
-				FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
+			var heading = new Label
+			{
+				Text = "Heading",
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
 			};
 
 			switch (Device.Idiom)
 			{
-			case TargetIdiom.Phone:
-				heading.Text += " Phone ";
-				break;
-			case TargetIdiom.Tablet:
-				heading.Text += " Tablet ";
-				break;
-			case TargetIdiom.Desktop:
-				heading.Text += " Desktop ";
-				break;
-			default:
-				heading.Text += " unknown ";
-				break;
-			}
-			//
-			// Device.OS == TargetPlatform iOS Android WinPhone
-			//
-			if (Device.OS == TargetPlatform.iOS) {
-				heading.Text += "iOS";
-			} else { // could be TargetPlatform.Android or TargetPlatform.WinPhone
-				heading.Text += Device.OS;
+				case TargetIdiom.Phone:
+					heading.Text += " Phone ";
+					break;
+				case TargetIdiom.Tablet:
+					heading.Text += " Tablet ";
+					break;
+				case TargetIdiom.Desktop:
+					heading.Text += " Desktop ";
+					break;
+				default:
+					heading.Text += " unknown ";
+					break;
 			}
 
-			var timer = new Label { 
+			if (Device.RuntimePlatform == Device.iOS)
+			{
+				heading.Text += "iOS";
+			}
+			else
+			{
+				heading.Text += Device.RuntimePlatform;
+			}
+
+			var timer = new Label
+			{
 				Text = "click start below"
 			};
 
+			double boxViewWidth;
+			switch (Device.RuntimePlatform)
+			{
+				case Device.iOS:
+					boxViewWidth = 30;
+					break;
+				case Device.Android:
+					boxViewWidth = 40;
+					break;
+				case Device.WinPhone:
+				case Device.Windows:
+				default:
+					boxViewWidth = 50;
+					break;
+			}
 
-			//
-			// Device.OnPlatform (Action)
-			//
-			var box = new BoxView {
+			var box = new BoxView
+			{
 				Color = Color.Green,
-				WidthRequest = Device.OnPlatform (30, 40, 50),
+				WidthRequest = boxViewWidth,
 				HorizontalOptions = LayoutOptions.Center
 			};
-			Device.OnPlatform(
-				iOS: () =>{
+
+			switch (Device.RuntimePlatform)
+			{
+				case Device.iOS:
 					box.Color = box.Color.MultiplyAlpha(0.5);
 					heading.TextColor = Color.Blue;
-				},
-				Android: () =>{
+					break;
+				case Device.Android:
 					box.Color = box.Color.AddLuminosity(0.3);
 					heading.TextColor = Color.FromRgb(115, 129, 130);
-				},
-				WinPhone: () =>{
+					break;
+				case Device.WinPhone:
+				case Device.Windows:
 					box.Color = box.Color.AddLuminosity(0.3);
 					heading.TextColor = Color.Accent;
-				}, 
-				Default: () =>{
-					heading.Text = "what platform is this?!" + Device.OS;
-				}
-			);
-
-
-
+					break;
+				default:
+					heading.Text = "what platform is this?!" + Device.RuntimePlatform;
+					break;
+			}
 
 			//
 			// Device.OnPlatform<T>
 			//
-			var timerButton = new Button { 
+			var timerButton = new Button
+			{
 				Text = "Start Timer",
 				BackgroundColor = Color.Gray.MultiplyAlpha(0.5),
 				HorizontalOptions = LayoutOptions.Center
 			};
-			timerButton.WidthRequest = Device.OnPlatform (200, 300, 100);
 
+			double timerWidth;
+			switch (Device.RuntimePlatform)
+			{
+				case Device.iOS:
+					timerWidth = 200;
+					break;
+				case Device.Android:
+					timerWidth = 300;
+					break;
+				case Device.WinPhone:
+				case Device.Windows:
+				default:
+					timerWidth = 100;
+					break;
+			}
+
+			timerButton.WidthRequest = timerWidth;
 
 			//
 			// Device.StartTimer     and     Device.BeginInvokeOnMainThread
 			//
-			timerButton.Clicked += (sender, e) => {
+			timerButton.Clicked += (sender, e) =>
+			{
 				timer.Text = "timer running...";
-				Device.StartTimer (new TimeSpan (0, 0, 10), () => {
+				Device.StartTimer(new TimeSpan(0, 0, 10), () =>
+				{
 					// do something every 10 seconds
-
-					Device.BeginInvokeOnMainThread ( () => {
+					Device.BeginInvokeOnMainThread(() =>
+					{
 						// interact with UI elements
-						timer.Text = 
-							DateTime.Now.ToString("mm:ss") + " past the hour";
+						timer.Text = DateTime.Now.ToString("mm:ss") + " past the hour";
 					});
-
 
 					return true; // runs again, or false to stop
 				});
 			};
 
-
-
 			//
 			// Device.OpenUri
 			//
-			var webButton = new Button { 
+			var webButton = new Button
+			{
 				Text = "Open Uri"
 			};
-			webButton.Clicked += ((sender, e) => 
-				Device.OpenUri(new Uri("http://xamarin.com/evolve")));
-
-
+			webButton.Clicked += ((sender, e) => Device.OpenUri(new Uri("http://xamarin.com/evolve")));
 
 			//
 			// Device.OnPlatform<T>
 			//
-			Content = new StackLayout { 
-				Padding = new Thickness (5, Device.OnPlatform(20,0,0), 5, 0),
+			double top;
+			switch (Device.RuntimePlatform)
+			{
+				case Device.iOS:
+					top = 20;
+					break;
+				case Device.Android:
+					top = 0;
+					break;
+				case Device.WinPhone:
+				case Device.Windows:
+				default:
+					top = 0;
+					break;
+			}
+
+			Content = new StackLayout
+			{
+				Padding = new Thickness(5, top, 5, 0),
 				Children = {
 					heading,
 					box,
@@ -135,4 +180,3 @@ namespace WorkingWithPlatformSpecifics
 		}
 	}
 }
-
--- WorkingWithDevice/WorkingWithDevice/DevicePageXaml.xaml.cs
+++ WorkingWithDevice/WorkingWithDevice/DevicePageXaml.xaml.cs
@@ -1,33 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace WorkingWithPlatformSpecifics
-{	
+{
 	public partial class DevicePageXaml : ContentPage
-	{	
-		public DevicePageXaml ()
+	{
+		public DevicePageXaml()
 		{
-			InitializeComponent ();
+			InitializeComponent();
 		}
 
-		void OpenUriClicked (object s, EventArgs e) {
+		void OpenUriClicked(object s, EventArgs e)
+		{
 			Device.OpenUri(new Uri("http://xamarin.com/evolve"));
 		}
 
-		void TimerClicked (object s, EventArgs e) {
+		void TimerClicked(object s, EventArgs e)
+		{
 			timer.Text = "timer running...";
-			Device.StartTimer (new TimeSpan (0, 0, 10), () => {
-				// do something every 10 seconds
-
-				Device.BeginInvokeOnMainThread ( () => {
-					// interact with UI elements
-					timer.Text = 
-						DateTime.Now.ToString("mm:ss") + " past the hour";
+			Device.StartTimer(new TimeSpan(0, 0, 10), () =>
+			{
+				// Do something every 10 seconds
+				Device.BeginInvokeOnMainThread(() =>
+				{
+					// Interact with UI elements
+					timer.Text = DateTime.Now.ToString("mm:ss") + " past the hour";
 				});
 
-
-				return true; // runs again, or false to stop
+				return true; // Runs again, or false to stop
 			});
 		}
 	}
