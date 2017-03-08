21a51f6de2b1094cadaba21ff1180fe2545a2100
Charles Petzold <charles.petzold@xamarin.com>
2017-03-08 09:59:54 -0500

    [Photo Picker Dependency Service] Code cleanup

--- DependencyService/DependencyServiceSample/Droid/MainActivity.cs
+++ DependencyService/DependencyServiceSample/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 namespace DependencyServiceSample.Droid
 {
     [Activity (Label = "DependencyServiceSample.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity // , IPicturePicker
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate (Bundle bundle)
         {
