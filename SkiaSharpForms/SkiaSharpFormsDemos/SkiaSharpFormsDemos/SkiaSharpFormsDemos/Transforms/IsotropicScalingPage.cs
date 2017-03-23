8fdf30d2fe94048c4d936353b9d1e4d999210dae
Charles Petzold <chape@microsoft.com>
2017-03-23 16:34:05 -0400

    [SkiaSharpForms] Finishing up transforms

--- SkiaSharpForms/SkiaSharpFormsDemos/SkiaSharpFormsDemos/SkiaSharpFormsDemos/Transforms/IsotropicScalingPage.cs
+++ SkiaSharpForms/SkiaSharpFormsDemos/SkiaSharpFormsDemos/SkiaSharpFormsDemos/Transforms/IsotropicScalingPage.cs
@@ -35,10 +35,21 @@ namespace SkiaSharpFormsDemos.Transforms
 
                 float scale = Math.Min(info.Width / pathBounds.Width,
                                        info.Height / pathBounds.Height);
-                canvas.Translate(-pathBounds.Left, -pathBounds.Top);
 
-                canvas.DrawPath(path, fillPaint);
-          //      canvas.DrawPath(path, strokePaint);
+                for (int i = 0; i <= 10; i++)
+                {
+                    fillPaint.Color = new SKColor((byte)(255 * (10 - i) / 10),
+                                                  0,
+                                                  (byte)(255 * i / 10));
+                    canvas.Save();
+                    canvas.Translate(info.Width / 2, info.Height / 2);
+                    canvas.Scale(scale);
+                    canvas.Translate(-pathBounds.MidX, -pathBounds.MidY);
+                    canvas.DrawPath(path, fillPaint);
+                    canvas.Restore();
+
+                    scale *= 0.9f;
+                }
             }
         }
     }
