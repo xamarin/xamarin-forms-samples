c4a7d361105d1c15c31863782a6248a02d93d8c3
David Britch <davidbritch@Davids-MBP.lan>
2017-03-29 15:42:28 +0100

    [CustomLayout] WrapLayout sample added.

---
+++ UserInterface/CustomLayout/WrapLayout/ImageWrapLayout/Layout/LayoutData.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace ImageWrapLayout
+{
+	struct LayoutData
+	{
+		public int VisibleChildCount { get; private set; }
+
+		public Size CellSize { get; private set; }
+
+		public int Rows { get; private set; }
+
+		public int Columns { get; private set; }
+
+		public LayoutData(int visibleChildCount, Size cellSize, int rows, int columns) : this()
+		{
+			VisibleChildCount = visibleChildCount;
+			CellSize = cellSize;
+			Rows = rows;
+			Columns = columns;
+		}
+
+	}
+}
