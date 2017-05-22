90fa62634b1296cb6014285c00a337cd5ecff911
David Britch <davidbritch@Davids-MBP.lan>
2017-05-22 12:28:10 +0100

    Updated TodoREST service to use ASP.NET Core.

--- WebServices/TodoREST/TodoRESTService/src/ToDoApi/Models/TodoItem.cs
+++ WebServices/TodoREST/TodoRESTService/src/ToDoApi/Models/TodoItem.cs
@@ -1,11 +1,16 @@
-﻿namespace TodoRESTService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApi.Models
 {
-    public class TodoItem
+    public class ToDoItem
     {
+        [Required]
         public string ID { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Notes { get; set; }
 
         public bool Done { get; set; }
