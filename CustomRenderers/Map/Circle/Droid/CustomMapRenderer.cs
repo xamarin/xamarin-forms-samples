1d73cd617c37a45de817d92cea907c26a000a497
David Britch <davidbritch@Davids-MBP.lan>
2017-08-16 11:25:12 +0100

    Fixed Android map renderers.

--- CustomRenderers/Map/Circle/Droid/CustomMapRenderer.cs
+++ CustomRenderers/Map/Circle/Droid/CustomMapRenderer.cs
@@ -1,45 +1,50 @@
-﻿using Android.Gms.Maps;
-using Android.Gms.Maps.Model;
+﻿using Android.Gms.Maps.Model;
 using MapOverlay;
 using MapOverlay.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps.Android;
 
-[assembly:ExportRenderer (typeof(CustomMap), typeof(CustomMapRenderer))]
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
 namespace MapOverlay.Droid
 {
-	public class CustomMapRenderer : MapRenderer, IOnMapReadyCallback
-	{
-		GoogleMap map;
-		CustomCircle circle;
+    public class CustomMapRenderer : MapRenderer
+    {
+        CustomCircle circle;
+        bool isDrawn;
 
-		protected override void OnElementChanged (Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Xamarin.Forms.Maps.Map> e)
-		{
-			base.OnElementChanged (e);
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Xamarin.Forms.Maps.Map> e)
+        {
+            base.OnElementChanged(e);
 
-			if (e.OldElement != null) {
-				// Unsubscribe
-			}
+            if (e.OldElement != null)
+            {
+                // Unsubscribe
+            }
 
-			if (e.NewElement != null) {
-				var formsMap = (CustomMap)e.NewElement;
-				circle = formsMap.Circle;
+            if (e.NewElement != null)
+            {
+                var formsMap = (CustomMap)e.NewElement;
+                circle = formsMap.Circle;
+                Control.GetMapAsync(this);
+            }
+        }
 
-				((MapView)Control).GetMapAsync (this);
-			}
-		}
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
 
-		public void OnMapReady (GoogleMap googleMap)
-		{
-			map = googleMap;
+            if (e.PropertyName.Equals("VisibleRegion") && !isDrawn)
+            {
+                var circleOptions = new CircleOptions();
+                circleOptions.InvokeCenter(new LatLng(circle.Position.Latitude, circle.Position.Longitude));
+                circleOptions.InvokeRadius(circle.Radius);
+                circleOptions.InvokeFillColor(0X66FF0000);
+                circleOptions.InvokeStrokeColor(0X66FF0000);
+                circleOptions.InvokeStrokeWidth(0);
 
-			var circleOptions = new CircleOptions ();
-			circleOptions.InvokeCenter (new LatLng (circle.Position.Latitude, circle.Position.Longitude));
-			circleOptions.InvokeRadius (circle.Radius);
-			circleOptions.InvokeFillColor (0X66FF0000);
-			circleOptions.InvokeStrokeColor (0X66FF0000);
-			circleOptions.InvokeStrokeWidth (0);
-			map.AddCircle (circleOptions);
-		}
-	}
+                NativeMap.AddCircle(circleOptions);
+                isDrawn = true;
+            }
+        }
+    }
 }
--- CustomRenderers/Map/Polyline/Droid/CustomMapRenderer.cs
+++ CustomRenderers/Map/Polyline/Droid/CustomMapRenderer.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Android.Gms.Maps;
 using Android.Gms.Maps.Model;
 using MapOverlay;
 using MapOverlay.Droid;
@@ -7,42 +6,48 @@ using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Maps.Android;
 
-[assembly:ExportRenderer (typeof(CustomMap), typeof(CustomMapRenderer))]
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
 namespace MapOverlay.Droid
 {
-	public class CustomMapRenderer : MapRenderer, IOnMapReadyCallback
-	{
-		GoogleMap map;
-		List<Position> routeCoordinates;
+    public class CustomMapRenderer : MapRenderer
+    {
+        List<Position> routeCoordinates;
+        bool isDrawn;
 
-		protected override void OnElementChanged (Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Map> e)
-		{
-			base.OnElementChanged (e);
+        protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Map> e)
+        {
+            base.OnElementChanged(e);
 
-			if (e.OldElement != null) {
-				// Unsubscribe
-			}
+            if (e.OldElement != null)
+            {
+                // Unsubscribe
+            }
 
-			if (e.NewElement != null) {
-				var formsMap = (CustomMap)e.NewElement;
-				routeCoordinates = formsMap.RouteCoordinates;
+            if (e.NewElement != null)
+            {
+                var formsMap = (CustomMap)e.NewElement;
+                routeCoordinates = formsMap.RouteCoordinates;
+                Control.GetMapAsync(this);
+            }
+        }
 
-				((MapView)Control).GetMapAsync (this);
-			}
-		}
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
 
-		public void OnMapReady (GoogleMap googleMap)
-		{
-			map = googleMap;
+            if (e.PropertyName.Equals("VisibleRegion") && !isDrawn)
+            {
+                var polylineOptions = new PolylineOptions();
+                polylineOptions.InvokeColor(0x66FF0000);
 
-			var polylineOptions = new PolylineOptions ();
-			polylineOptions.InvokeColor (0x66FF0000);
+                foreach (var position in routeCoordinates)
+                {
+                    polylineOptions.Add(new LatLng(position.Latitude, position.Longitude));
+                }
 
-			foreach (var position in routeCoordinates) {
-				polylineOptions.Add (new LatLng (position.Latitude, position.Longitude));
-			}
-
-			map.AddPolyline (polylineOptions);
-		}
-	}
+                NativeMap.AddPolyline(polylineOptions);
+                isDrawn = true;
+            }
+        }
+    }
 }
