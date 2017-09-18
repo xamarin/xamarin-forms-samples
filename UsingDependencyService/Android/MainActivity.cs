72b2c399c5b200fd372be18c55531bee09df455e
David Britch <davidbritch@Davids-MBP.lan>
2017-09-18 16:38:00 +0100

    Replaced deprecated TextToSpeech.Speak API.

--- UsingDependencyService/Android/MainActivity.cs
+++ UsingDependencyService/Android/MainActivity.cs
@@ -1,31 +1,20 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
-
-using Xamarin.Forms.Platform.Android;
 using Android.Content.PM;
-
 
 namespace UsingDependencyService.Android
 {
-	[Activity (Label = "UsingDependencyService.Android.Android", Icon = "@drawable/icon", MainLauncher = true, 
-		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : 
-	global::Xamarin.Forms.Platform.Android.FormsApplicationActivity // superclass new in 1.3
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
-
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-
-			LoadApplication (new App ()); // method is new in 1.3
-		}
-	}
+    [Activity(Label = "UsingDependencyService.Android.Android", Icon = "@drawable/icon", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity :
+    global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
 }
 
--- UsingDependencyService/Android/TextToSpeech_Android.cs
+++ UsingDependencyService/Android/TextToSpeech_Android.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using Android.Speech.Tts;
+﻿using Android.Speech.Tts;
 using Xamarin.Forms;
 using UsingDependencyService.Android;
-using System.Collections.Generic;
+using System.Diagnostics;
 
-[assembly: Dependency (typeof (TextToSpeech_Android))]
-
+[assembly: Dependency(typeof(TextToSpeech_Android))]
 namespace UsingDependencyService.Android
 {
-	public class TextToSpeech_Android : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
-	{
-		TextToSpeech speaker; string toSpeak;
-		public TextToSpeech_Android () {}
+    public class TextToSpeech_Android : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    {
+        TextToSpeech speaker;
+        string toSpeak;
 
-		public void Speak (string text)
-		{
-			var c = Forms.Context; 
-			toSpeak = text;
-			if (speaker == null) {
-				speaker = new TextToSpeech (c, this);
-			} else {
-				var p = new Dictionary<string,string> ();
-				speaker.Speak (toSpeak, QueueMode.Flush, p);
-				System.Diagnostics.Debug.WriteLine ("spoke " + toSpeak);
-			}
-		}
+        public void Speak(string text)
+        {
+            toSpeak = text;
+            if (speaker == null)
+            {
+                speaker = new TextToSpeech(Forms.Context, this);
+            }
+            else
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+                Debug.WriteLine("spoke " + toSpeak);
+            }
+        }
 
-		#region IOnInitListener implementation
-		public void OnInit (OperationResult status)
-		{
-			if (status.Equals (OperationResult.Success)) {
-				System.Diagnostics.Debug.WriteLine ("speaker init");
-				var p = new Dictionary<string,string> ();
-				speaker.Speak (toSpeak, QueueMode.Flush, p);
-			} else {
-				System.Diagnostics.Debug.WriteLine ("was quiet");
-			}
-		}
-		#endregion
-	}
+        #region IOnInitListener implementation
+        public void OnInit(OperationResult status)
+        {
+            if (status.Equals(OperationResult.Success))
+            {
+                Debug.WriteLine("speaker init");
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+            else
+            {
+                Debug.WriteLine("was quiet");
+            }
+        }
+        #endregion
+    }
 }
-
