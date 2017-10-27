35077cac5f29cc99796751706c4b64bf097f7794
David Britch <davidbritch@Davids-MBP.lan>
2017-10-27 11:13:36 +0100

    Removed deprecated APIs.

--- UsingResxLocalization/UsingResxLocalization/App.cs
+++ UsingResxLocalization/UsingResxLocalization/App.cs
@@ -6,30 +6,30 @@ using Xamarin.Forms.Xaml;
 namespace UsingResxLocalization
 {
     public class App : Application
-	{
-		public App ()
-		{
-			System.Diagnostics.Debug.WriteLine("====== resource debug info =========");
-			var assembly = typeof(App).GetTypeInfo().Assembly;
-			foreach (var res in assembly.GetManifestResourceNames()) 
-				System.Diagnostics.Debug.WriteLine("found resource: " + res);
-			System.Diagnostics.Debug.WriteLine("====================================");
+    {
+        public App()
+        {
+            System.Diagnostics.Debug.WriteLine("====== resource debug info =========");
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+                System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            System.Diagnostics.Debug.WriteLine("====================================");
 
-			// This lookup NOT required for Windows platforms - the Culture will be automatically set
-            if (Device.OS == TargetPlatform.iOS || Device.OS == TargetPlatform.Android)
+            // This lookup NOT required for Windows platforms - the Culture will be automatically set
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
             {
-				// determine the correct, supported .NET culture
-				var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
-				Resx.AppResources.Culture = ci; // set the RESX for resource localization
-				DependencyService.Get<ILocalize>().SetLocale(ci); // set the Thread for locale-aware methods
-			}
+                // determine the correct, supported .NET culture
+                var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+                Resx.AppResources.Culture = ci; // set the RESX for resource localization
+                DependencyService.Get<ILocalize>().SetLocale(ci); // set the Thread for locale-aware methods
+            }
 
-			var tabs = new TabbedPage ();
-			tabs.Children.Add (new FirstPage {Title = "C#", Icon="csharp.png"});
-			tabs.Children.Add (new FirstPageXaml {Title = "Xaml", Icon="xaml.png"});
+            var tabs = new TabbedPage();
+            tabs.Children.Add(new FirstPage { Title = "C#", Icon = "csharp.png" });
+            tabs.Children.Add(new FirstPageXaml { Title = "Xaml", Icon = "xaml.png" });
 
-			MainPage = tabs;
-		}
-	}
+            MainPage = tabs;
+        }
+    }
 }
 
--- UsingResxLocalization/UsingResxLocalization/PagesXaml/TranslateExtension.cs
+++ UsingResxLocalization/UsingResxLocalization/PagesXaml/TranslateExtension.cs
@@ -8,29 +8,30 @@ using Xamarin.Forms.Xaml;
 namespace UsingResxLocalization
 {
     // You exclude the 'Extension' suffix when using in Xaml markup
-    [ContentProperty ("Text")]
-	public class TranslateExtension : IMarkupExtension
-	{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
         readonly CultureInfo ci = null;
-		const string ResourceId = "UsingResxLocalization.Resx.AppResources";
+        const string ResourceId = "UsingResxLocalization.Resx.AppResources";
 
-		public TranslateExtension() {
-            if (Device.OS == TargetPlatform.iOS || Device.OS == TargetPlatform.Android)
+        public TranslateExtension()
+        {
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
             {
                 ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
             }
-		}
+        }
 
-		public string Text { get; set; }
+        public string Text { get; set; }
 
-		public object ProvideValue (IServiceProvider serviceProvider)
-		{
-			if (Text == null)
-				return "";
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
 
-			ResourceManager temp = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
+            ResourceManager temp = new ResourceManager(ResourceId, typeof(TranslateExtension).GetTypeInfo().Assembly);
 
-			var translation = temp.GetString (Text, ci);
+            var translation = temp.GetString(Text, ci);
             if (translation == null)
             {
 #if DEBUG
@@ -42,6 +43,6 @@ namespace UsingResxLocalization
 #endif
             }
             return translation;
-		}
-	}
+        }
+    }
 }
