76359e868d82205d4844adeb7612f2309d4231fa
sung-su.kim <sung-su.kim@samsung.com>
2017-11-13 18:47:05 +0900

    Add Tizen project for FocusEffect

---
+++ Effects/FocusEffect/EffectsDemo.Tizen/FocusEffect.cs
@@ -0,0 +1,58 @@
+﻿using EffectsDemo.Tizen;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Tizen;
+using EColor = ElmSharp.Color;
+
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(FocusEffect), "FocusEffect")]
+namespace EffectsDemo.Tizen
+{
+	public class FocusEffect : PlatformEffect
+	{
+		EColor backgroundColor;
+
+		protected override void OnAttached()
+		{
+			try
+			{
+				(Control as ElmSharp.Entry).BackgroundColor = backgroundColor = EColor.Lime;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+			}
+		}
+
+		protected override void OnDetached()
+		{
+		}
+
+		protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+		{
+			base.OnElementPropertyChanged(args);
+
+			try
+			{
+				if (args.PropertyName == "IsFocused")
+				{
+					var control = Control as ElmSharp.Entry;
+					if (control.BackgroundColor.Equals(backgroundColor))
+					{
+						control.BackgroundColor = EColor.White;
+					}
+					else
+					{
+						control.BackgroundColor = backgroundColor;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+			}
+		}
+	}
+}
+
