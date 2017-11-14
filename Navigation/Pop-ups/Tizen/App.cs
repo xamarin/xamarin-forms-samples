1270a47e61d77804020e23d48ac2410e87e3c17e
Joonghyun Cho <jh5.cho@samsung.com>
2017-11-14 16:58:58 +0900

    Add Tizen project for Navigation/Pop-ups

---
+++ Navigation/Pop-ups/Tizen/App.cs
@@ -0,0 +1,21 @@
+﻿using Tizen.Applications;
+using ElmSharp;
+
+namespace WorkingWithPopups.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
