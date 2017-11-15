06158fcf3b14003be2b37ba1ebb6b323739ec863
sung-su.kim <sung-su.kim@samsung.com>
2017-11-15 11:53:48 +0900

    Add Tizen project for BindablePicker

---
+++ UserInterface/BindablePicker/BindablePicker/BindablePicker.Tizen/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms.Platform.Tizen;
+
+namespace BindablePicker
+{
+	public class Program : FormsApplication
+	{
+		protected override void OnCreate()
+		{
+			base.OnCreate();
+			LoadApplication(new App());
+		}
+
+		static void Main(string[] args)
+		{
+			var app = new Program();
+			Forms.Init(app);
+			app.Run(args);
+		}
+	}
+}
