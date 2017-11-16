fc59f22112130bba6c7fa26570579ee4a3288667
darkleem <cdark.lim@samsung.com>
2017-11-16 20:27:37 +0900

    Add Tizen project for DependencyServiceSample

---
+++ DependencyService/DependencyServiceSample/Tizen/App.cs
@@ -0,0 +1,18 @@
+namespace DependencyServiceSample.Tizen
+{
+    public class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app, true);
+            app.Run(args);
+        }
+    }
+}
---
+++ DependencyService/DependencyServiceSample/Tizen/DeviceOrientationImplementation.cs
@@ -0,0 +1,17 @@
+﻿using DependencyServiceSample.Tizen;
+using Xamarin.Forms.Platform.Tizen;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DeviceOrientationImplementation))]
+namespace DependencyServiceSample.Tizen
+{
+    public class DeviceOrientationImplementation : IDeviceOrientation
+	{
+		public DeviceOrientations GetOrientation()
+		{
+            var rotation = Forms.Context.MainWindow.Rotation;
+            bool isLandscape = rotation == 90 || rotation == 270;
+			return isLandscape ? DeviceOrientations.Landscape : DeviceOrientations.Portrait;
+		}
+	}
+}
+
