650da209ee7d7d1e4da7ed2623348dce46a8b93a
David Britch <davidbritch@Davids-MBP.lan>
2017-11-17 16:15:37 +0000

    Sample uses merged resource dictionaries.

---
+++ XAML/ResourceDictionaries/ResourceDictionaryDemo/MyResourceDictionary.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ResourceDictionaryDemo
+{
+    public partial class MyResourceDictionary : ResourceDictionary
+    {
+        public MyResourceDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
