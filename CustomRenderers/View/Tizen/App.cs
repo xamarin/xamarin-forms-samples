aeecd8b986149964d82fe12554a4f731cf04bbfa
darkleem <cdark.lim@samsung.com>
2017-11-17 18:20:48 +0900

    Add Tizen project for CustomRenderers/View

---
+++ CustomRenderers/View/Tizen/App.cs
@@ -0,0 +1,21 @@
+using Xamarin.Forms.Platform.Tizen;
+
+namespace CustomRenderer.Tizen
+{
+    public class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            Forms.Context.MainWindow.AvailableRotations = ElmSharp.DisplayRotation.Degree_0;
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
---
+++ CustomRenderers/View/Tizen/CameraPreviewRenderer.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms.Platform.Tizen;
+using System.Linq;
+using Tizen.Multimedia;
+
+[assembly: ExportRenderer(typeof(CustomRenderer.CameraPreview), typeof(CustomRenderer.CameraPreviewRenderer))]
+
+namespace CustomRenderer
+{
+    class CameraPreviewRenderer : VisualElementRenderer<CameraPreview>
+    {
+        protected Camera Camera;
+        protected CameraDevice SelectedCamera;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CameraPreview> e)
+        {
+            base.OnElementChanged(e);
+            if (NativeView == null)
+            {
+                SelectedCamera = CameraDevice.Rear;
+                try
+                {
+                    Camera = new Camera(SelectedCamera);
+                    var mediaView = new MediaView(Forms.Context.MainWindow);
+                    Camera.Display = new Display(mediaView);
+                    SetNativeControl(mediaView);
+                }
+                catch (System.Exception ex)
+                {
+                    Camera?.Dispose();
+                    Camera = null;
+                    SetNativeControl(new ElmSharp.Label(Forms.Context.MainWindow)
+                    {
+                        Text = $"Camera preview is not available.<br><br>{ex}",
+                        LineWrapType = ElmSharp.WrapType.Word
+                    });
+                }
+            }
+            if (e.OldElement != null)
+            {
+                if (Camera != null)
+                {
+                    if (Camera.State == CameraState.Captured)
+                    {
+                        Camera.StartPreview();
+                        Camera.StopPreview();
+                    }
+                    else if (Camera.State == CameraState.Preview)
+                    {
+                        Camera.StopPreview();
+                    }
+
+                    Camera.Dispose();
+                }
+            }
+            if (e.NewElement != null)
+            {
+                UpdateCameraProperty();
+                Camera?.StartPreview();
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == CameraPreview.CameraProperty.PropertyName)
+            {
+                UpdateCameraProperty();
+                return;
+            }
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        private void UpdateCameraProperty()
+        {
+            if (Camera == null)
+                return;
+
+            CameraDevice previousCamera = SelectedCamera;
+
+            if (Element.Camera == CameraOptions.Rear)
+                SelectedCamera = CameraDevice.Rear;
+            else if (Element.Camera == CameraOptions.Front)
+                SelectedCamera = CameraDevice.Front;
+
+            if (SelectedCamera != previousCamera)
+                Camera.ChangeDevice(SelectedCamera);
+        }
+    }
+}
