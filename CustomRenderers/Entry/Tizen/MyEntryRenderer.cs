da21894cd98fe7c1ce82f2f506f0c333c1048810
darkleem <cdark.lim@samsung.com>
2017-11-28 13:38:51 +0900

    Add Tizen project for CustomRenderers/Entry

---
+++ CustomRenderers/Entry/Tizen/MyEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Tizen;
+using CustomRenderer;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+
+namespace CustomRenderer
+{
+    class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && Control != null)
+            {
+                Control.BackgroundColor = ElmSharp.Color.FromRgb(204, 153, 255);
+            }
+        }
+    }
+}
