0faa09626e9ace1a717ef10a86345da3a89fe412
David Britch <davidbritch@Davids-MacBook-Pro.local>
2017-12-01 10:28:02 +0000

    Master page now specified completely in XAML.

---
+++ Navigation/MasterDetailPage/MasterDetailPageNavigation/XAML/MasterPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace MasterDetailPageNavigation
+{
+    public partial class MasterPage : ContentPage
+    {
+        public ListView ListView { get { return listView; } }
+
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
