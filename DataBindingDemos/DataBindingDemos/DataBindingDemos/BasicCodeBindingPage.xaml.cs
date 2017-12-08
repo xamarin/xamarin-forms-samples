fca57bc712e3fbe96ec6483bd5901f07234c2f1d
Charles Petzold <chape@microsoft.com>
2017-12-08 16:13:34 -0500

    [DataBindingDemos] First several samples

---
+++ DataBindingDemos/DataBindingDemos/DataBindingDemos/BasicCodeBindingPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBindingDemos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BasicCodeBindingPage : ContentPage
+    {
+        public BasicCodeBindingPage()
+        {
+            InitializeComponent();
+
+            label.BindingContext = slider;
+            label.SetBinding(Label.RotationProperty, "Value");
+        }
+    }
+}
