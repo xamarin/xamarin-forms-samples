c4da9e2b50f72cef5a80195a06f71321688fce04
Charles Petzold <chape@microsoft.com>
2017-12-12 16:54:21 -0500

    [DataBindingDemos] Path and Binding Mode

---
+++ DataBindingDemos/DataBindingDemos/DataBindingDemos/HslColorViewModel.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataBindingDemos
+{
+    public class NamedColor
+    {
+        // Instance members.
+        private NamedColor()
+        {
+        }
+
+        public string Name { private set; get; }
+
+        public string FriendlyName { private set; get; }
+
+        public Color Color { private set; get; }
+
+        public string RgbDisplay { private set; get; }
+
+        // Static members.
+        static NamedColor()
+        {
+            List<NamedColor> all = new List<NamedColor>();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // Loop through the public static fields of the Color structure.
+            foreach (FieldInfo fieldInfo in typeof(Color).GetRuntimeFields())
+            {
+                if (fieldInfo.IsPublic &&
+                    fieldInfo.IsStatic &&
+                    fieldInfo.FieldType == typeof(Color))
+                {
+                    // Convert the name to a friendly name.
+                    string name = fieldInfo.Name;
+                    stringBuilder.Clear();
+                    int index = 0;
+
+                    foreach (char ch in name)
+                    {
+                        if (index != 0 && Char.IsUpper(ch))
+                        {
+                            stringBuilder.Append(' ');
+                        }
+                        stringBuilder.Append(ch);
+                        index++;
+                    }
+
+                    // Instantiate a NamedColor object.
+                    Color color = (Color)fieldInfo.GetValue(null);
+
+                    NamedColor namedColor = new NamedColor
+                    {
+                        Name = name,
+                        FriendlyName = stringBuilder.ToString(),
+                        Color = color,
+                        RgbDisplay = String.Format("{0:X2}-{1:X2}-{2:X2}",
+                                                   (int)(255 * color.R),
+                                                   (int)(255 * color.G),
+                                                   (int)(255 * color.B))
+                    };
+
+                    // Add it to the collection.
+                    all.Add(namedColor);
+                }
+            }
+            all.TrimExcess();
+            All = all;
+        }
+
+        public static IList<NamedColor> All { private set; get; }
+    }
+
+
+
+
+
+    public class HslColorViewModel : INotifyPropertyChanged
+    {
+        Color color;
+        string name; 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Hue
+        {
+            set
+            {
+                if (color.Hue != value)
+                {
+       //             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Hue"));
+                    Color = Color.FromHsla(value, color.Saturation, color.Luminosity);
+            //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Color"));
+                }
+            }
+            get
+            {
+                return color.Hue;
+            }
+        }
+
+        public double Saturation
+        {
+            set
+            {
+                if (color.Saturation != value)
+                {
+       //             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Saturation"));
+                    Color = Color.FromHsla(color.Hue, value, color.Luminosity);
+             //       PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Color"));
+                }
+            }
+            get
+            {
+                return color.Saturation;
+            }
+        }
+
+        public double Luminosity
+        {
+            set
+            {
+                if (color.Luminosity != value)
+                {
+         //           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Luminosity"));
+                    Color = Color.FromHsla(color.Hue, color.Saturation, value);
+           //         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Color"));
+                }
+            }
+            get
+            {
+                return color.Luminosity;
+            }
+        }
+
+        public Color Color
+        {
+            set
+            {
+                if (color != value)
+                {
+                    color = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Hue"));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Saturation"));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Luminosity"));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Color"));
+
+
+                    double shortestDistance = 1000;
+                    NamedColor closestColor = null;
+
+                    foreach (NamedColor namedColor in NamedColor.All)
+                    {
+                        double distance = Math.Sqrt(Math.Pow(color.R - namedColor.Color.R, 2) +
+                                                    Math.Pow(color.G - namedColor.Color.G, 2) +
+                                                    Math.Pow(color.B - namedColor.Color.B, 2));
+
+                        if (distance < shortestDistance)
+                        {
+                            shortestDistance = distance;
+                            closestColor = namedColor;
+                        }
+                    }
+                    Name = closestColor.Name;
+              //      System.Diagnostics.Debug.WriteLine(closestColor.Name);
+                }
+            }
+            get
+            {
+                return color;
+            }
+        }
+
+        public string Name
+        {
+            private set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+            get
+            {
+                return name;
+            }
+        }
+    }
+}
---
+++ DataBindingDemos/DataBindingDemos/DataBindingDemos/TwoWayBindingPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBindingDemos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TwoWayBindingPage : ContentPage
+    {
+        public TwoWayBindingPage()
+        {
+            InitializeComponent();
+
+
+            var x = System.Globalization.CultureInfo.CurrentCulture;
+
+            var z = new System.Globalization.RegionInfo("fr-fr");
+
+            var a = new System.Globalization.CultureInfo("fr-fr");
+
+            var y = x.DateTimeFormat.DayNames[3];
+        }
+    }
+}
