5f4d9b2901e5c1b81592f1ef75d21106d5a913f9
David Britch <davidbritch@Davids-MacBook-Pro.local>
2017-12-15 09:08:45 +0000

    Removed unused using statement.

--- CustomRenderers/HybridWebView/Droid/HybridWebViewRenderer.cs
+++ CustomRenderers/HybridWebView/Droid/HybridWebViewRenderer.cs
@@ -1,42 +1,45 @@
-﻿using Android.Webkit;
-using CustomRenderer;
+﻿using CustomRenderer;
 using CustomRenderer.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
-[assembly: ExportRenderer (typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
 namespace CustomRenderer.Droid
 {
-	public class HybridWebViewRenderer : ViewRenderer<HybridWebView, Android.Webkit.WebView>
-	{
-		const string JavaScriptFunction = "function invokeCSharpAction(data){jsBridge.invokeAction(data);}";
+    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, Android.Webkit.WebView>
+    {
+        const string JavaScriptFunction = "function invokeCSharpAction(data){jsBridge.invokeAction(data);}";
 
-		protected override void OnElementChanged (ElementChangedEventArgs<HybridWebView> e)
-		{
-			base.OnElementChanged (e);
+        protected override void OnElementChanged(ElementChangedEventArgs<HybridWebView> e)
+        {
+            base.OnElementChanged(e);
 
-			if (Control == null) {
-				var webView = new Android.Webkit.WebView (Forms.Context);
-				webView.Settings.JavaScriptEnabled = true;
-				SetNativeControl (webView);
-			}
-			if (e.OldElement != null) {
-				Control.RemoveJavascriptInterface ("jsBridge");
-				var hybridWebView = e.OldElement as HybridWebView;
-				hybridWebView.Cleanup ();
-			}
-			if (e.NewElement != null) {
-				Control.AddJavascriptInterface (new JSBridge (this), "jsBridge");
-				Control.LoadUrl (string.Format ("file:///android_asset/Content/{0}", Element.Uri));
-				InjectJS (JavaScriptFunction);
-			}
-		}
+            if (Control == null)
+            {
+                var webView = new Android.Webkit.WebView(Forms.Context);
+                webView.Settings.JavaScriptEnabled = true;
+                SetNativeControl(webView);
+            }
+            if (e.OldElement != null)
+            {
+                Control.RemoveJavascriptInterface("jsBridge");
+                var hybridWebView = e.OldElement as HybridWebView;
+                hybridWebView.Cleanup();
+            }
+            if (e.NewElement != null)
+            {
+                Control.AddJavascriptInterface(new JSBridge(this), "jsBridge");
+                Control.LoadUrl(string.Format("file:///android_asset/Content/{0}", Element.Uri));
+                InjectJS(JavaScriptFunction);
+            }
+        }
 
-		void InjectJS (string script)
-		{
-			if (Control != null) {
-				Control.LoadUrl (string.Format ("javascript: {0}", script));
-			}
-		}
-	}
+        void InjectJS(string script)
+        {
+            if (Control != null)
+            {
+                Control.LoadUrl(string.Format("javascript: {0}", script));
+            }
+        }
+    }
 }
