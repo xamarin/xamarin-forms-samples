2f02b95f2ba60c23211f58e1495b07ffa031c465
Charles Petzold <chape@microsoft.com>
2017-12-18 16:50:28 -0500

    [MarkupExtensions] Begun

---
+++ XAML/MarkupExtensions/MarkupExtensions/MarkupExtensions/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MarkupExtensions
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigateCommand = new Command<Type>(async (Type pageType) =>
+            {
+                Page page = (Page)Activator.CreateInstance(pageType);
+                await Navigation.PushAsync(page);
+            });
+
+            BindingContext = this;
+        }
+
+        public ICommand NavigateCommand { private set; get; }
+    }
+}
+
