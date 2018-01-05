f1ec76bce057d129387f5178cf32d5660e0225b4
Charles Petzold <chape@microsoft.com>
2018-01-05 15:01:42 -0500

    [RPN Calculator] In Progress

---
+++ RpnCalculator/RpnCalculator/RpnCalculator/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace RpnCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SizeChanged += (sender, args) =>
+            {
+                bool isLandscape = Width > Height;
+                portrait.IsVisible = portrait.IsEnabled = !isLandscape;
+                landscape.IsVisible = landscape.IsEnabled = isLandscape;
+            };
+
+        }
+    }
+}
