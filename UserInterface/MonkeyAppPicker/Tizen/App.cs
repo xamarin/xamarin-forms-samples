30659e6b4f85e117d62c2cd4780309e524b8164d
Mariia Borowiec <m.borowiec2@samsung.com>
2018-01-24 11:05:03 +0100

    Add MonkeyAppPicker project

---
+++ UserInterface/MonkeyAppPicker/Tizen/App.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MonkeyApp.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            LoadApplication(new App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
