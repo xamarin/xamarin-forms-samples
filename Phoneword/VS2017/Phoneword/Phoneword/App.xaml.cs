c323150da0d4d405c90523e28f873cbaff54f77d
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-02-06 10:38:12 +0000

    VSMac and VS2017 Phoneword solutions added.

---
+++ Phoneword/VS2017/Phoneword/Phoneword/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Phoneword
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ PhonewordMultiscreen/VS2017/Phoneword/Phoneword.iOS/PhoneDialer.cs
@@ -0,0 +1,17 @@
+﻿using Foundation;
+using Phoneword.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace Phoneword.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new NSUrl("tel:" + number));
+        }
+    }
+}
