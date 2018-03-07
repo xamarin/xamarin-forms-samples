48b2b959fdb26542b1ddf9c965433d97974c54dd
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-03-07 16:50:12 +0000

    Fix "unsupported parameter combination" error in iOS implementation
    The error occurs if a wrong value for bytesPerRow is passed to the
    CGBitmapContext function and leads to a crash.
    
    The fixed code calculates the correct value by casting width to int
    before multiplying it.

--- XamFormsImageResize/XamFormsImageResize/ImageResizer.cs
+++ XamFormsImageResize/XamFormsImageResize/ImageResizer.cs
@@ -32,16 +32,16 @@ using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace XamFormsImageResize
 {
-	public static class ImageResizer
-	{
-		static ImageResizer()
-		{
-		}
+    public static class ImageResizer
+    {
+        static ImageResizer()
+        {
+        }
 
-		public static async Task<byte[]> ResizeImage(byte[] imageData, float width, float height)
-		{
+        public static async Task<byte[]> ResizeImage(byte[] imageData, float width, float height)
+        {
 #if __IOS__
-			return ResizeImageIOS(imageData, width, height);
+            return ResizeImageIOS(imageData, width, height);
 #endif
 #if __ANDROID__
 			return ResizeImageAndroid ( imageData, width, height );
@@ -55,53 +55,53 @@ namespace XamFormsImageResize
 #if WINDOWS_UWP
             return await ResizeImageWindows(imageData, width, height);
 #endif
-		}
+        }
 
 
 #if __IOS__
-		public static byte[] ResizeImageIOS(byte[] imageData, float width, float height)
-		{
-			UIImage originalImage = ImageFromByteArray(imageData);
-			UIImageOrientation orientation = originalImage.Orientation;
+        public static byte[] ResizeImageIOS(byte[] imageData, float width, float height)
+        {
+            UIImage originalImage = ImageFromByteArray(imageData);
+            UIImageOrientation orientation = originalImage.Orientation;
 
-			//create a 24bit RGB image
-			using (CGBitmapContext context = new CGBitmapContext(IntPtr.Zero,
-												 (int)width, (int)height, 8,
-												 (int)(4 * width), CGColorSpace.CreateDeviceRGB(),
-												 CGImageAlphaInfo.PremultipliedFirst))
-			{
+            //create a 24bit RGB image
+            using (CGBitmapContext context = new CGBitmapContext(IntPtr.Zero,
+                                                 (int)width, (int)height, 8,
+                                                 4 * (int)width, CGColorSpace.CreateDeviceRGB(),
+                                                 CGImageAlphaInfo.PremultipliedFirst))
+            {
 
-				RectangleF imageRect = new RectangleF(0, 0, width, height);
+                RectangleF imageRect = new RectangleF(0, 0, width, height);
 
-				// draw the image
-				context.DrawImage(imageRect, originalImage.CGImage);
+                // draw the image
+                context.DrawImage(imageRect, originalImage.CGImage);
 
-				UIKit.UIImage resizedImage = UIKit.UIImage.FromImage(context.ToImage(), 0, orientation);
+                UIKit.UIImage resizedImage = UIKit.UIImage.FromImage(context.ToImage(), 0, orientation);
 
-				// save the image as a jpeg
-				return resizedImage.AsJPEG().ToArray();
-			}
-		}
+                // save the image as a jpeg
+                return resizedImage.AsJPEG().ToArray();
+            }
+        }
 
-		public static UIKit.UIImage ImageFromByteArray(byte[] data)
-		{
-			if (data == null)
-			{
-				return null;
-			}
+        public static UIKit.UIImage ImageFromByteArray(byte[] data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
 
-			UIKit.UIImage image;
-			try
-			{
-				image = new UIKit.UIImage(Foundation.NSData.FromArray(data));
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("Image load failed: " + e.Message);
-				return null;
-			}
-			return image;
-		}
+            UIKit.UIImage image;
+            try
+            {
+                image = new UIKit.UIImage(Foundation.NSData.FromArray(data));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Image load failed: " + e.Message);
+                return null;
+            }
+            return image;
+        }
 #endif
 
 #if __ANDROID__
@@ -197,6 +197,6 @@ namespace XamFormsImageResize
         }
 
 #endif
-	}
+    }
 }
 
