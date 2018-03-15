71c0431691bf43c01541acd4dc405d05e7919d11
Charles Petzold <chape@microsoft.com>
2018-03-15 10:33:06 -0400

    [Slider Demos] Initial publication

---
+++ UserInterface/SliderDemos/SliderDemos/SliderDemos/BasicSliderCodePage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SliderDemos
+{
+public class BasicSliderCodePage : ContentPage
+{
+    public BasicSliderCodePage()
+    {
+        Label rotationLabel = new Label
+        {
+            Text = "ROTATING TEXT",
+            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.CenterAndExpand
+        };
+
+        Label displayLabel = new Label
+        {
+            Text = "(uninitialized)",
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.CenterAndExpand
+        };
+
+        Slider slider = new Slider
+        {
+            Maximum = 360
+        };
+        slider.ValueChanged += (sender, args) =>
+        {
+            rotationLabel.Rotation = slider.Value;
+            displayLabel.Text = String.Format("The Slider value is {0}", args.NewValue);
+        };
+
+        Title = "Basic Slider Code";
+        Padding = new Thickness(10, 0);
+        Content = new StackLayout
+        {
+            Children =
+            {
+                rotationLabel,
+                slider,
+                displayLabel
+            }
+        };
+    }
+}
+}
