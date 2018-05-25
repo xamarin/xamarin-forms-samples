09333635efe0eaf744f4b6979c9d3ee26bd25859
Charles Petzold <chape@microsoft.com>
2018-05-25 16:00:46 -0400

    [Button Demos] Image Buttons and complete (?)

---
+++ UserInterface/ButtonDemos/ButtonDemos/ButtonDemos/ImageButtonDemoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ButtonDemos
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImageButtonDemoPage : ContentPage
+	{
+		public ImageButtonDemoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
