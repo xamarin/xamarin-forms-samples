8693bc6b4ec9a68af71eafadb55bfc5317555259
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-05-29 13:44:58 +0100

    ListView.SelectionMode platform-specific added.

---
+++ UserInterface/PlatformSpecifics/PlatformSpecifics/UWP/CS/WindowsListViewPageCS.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
+
+namespace PlatformSpecifics
+{
+    public class WindowsListViewPageCS : ContentPage
+    {
+        public WindowsListViewPageCS()
+        {
+			var personDataTemplate = new DataTemplate(() =>
+            {
+                var grid = new Grid();
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.7, GridUnitType.Star) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.3, GridUnitType.Star) });
+
+                var nameLabel = new Xamarin.Forms.Label();
+				var ageLabel = new Xamarin.Forms.Label { HorizontalOptions = LayoutOptions.EndAndExpand };
+
+				nameLabel.SetBinding(Xamarin.Forms.Label.TextProperty, "Name");
+				ageLabel.SetBinding(Xamarin.Forms.Label.TextProperty, "Age");
+
+                grid.Children.Add(nameLabel);
+                grid.Children.Add(ageLabel, 1, 0);
+
+                return new ViewCell { View = grid };
+            });
+
+
+            var listView = new Xamarin.Forms.ListView { IsGroupingEnabled = true, ItemTemplate = personDataTemplate };
+            listView.SetBinding(ItemsView<Cell>.ItemsSourceProperty, "GroupedEmployees");
+            listView.GroupDisplayBinding = new Binding("Key");
+			listView.On<Windows>().SetSelectionMode(ListViewSelectionMode.Inaccessible);
+
+            Title = "ListView Selection Mode";
+            Content = new StackLayout
+            {
+                Margin = new Thickness(20),
+                Children = { listView }
+            };
+			BindingContext = new ListViewViewModel();
+        }
+    }
+}
---
+++ UserInterface/PlatformSpecifics/PlatformSpecifics/UWP/XAML/WindowsListViewPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace PlatformSpecifics
+{
+    public partial class WindowsListViewPage : ContentPage
+    {
+        public WindowsListViewPage()
+        {
+            InitializeComponent();
+			BindingContext = new ListViewViewModel();
+        }
+    }
+}
