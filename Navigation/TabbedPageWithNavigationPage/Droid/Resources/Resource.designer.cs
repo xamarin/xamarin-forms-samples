d3fc36ffe0d8e666113af364f8ee01f01a0a5cca
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-06-15 17:28:00 +0100

    Missing images added.

--- Navigation/TabbedPageWithNavigationPage/Droid/Resources/Resource.designer.cs
+++ Navigation/TabbedPageWithNavigationPage/Droid/Resources/Resource.designer.cs
@@ -2342,26 +2342,26 @@ namespace TabbedPageWithNavigationPage.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020130
-			public const int avd_hide_password_1 = 2130837808;
+			// aapt resource value: 0x7f020133
+			public const int avd_hide_password_1 = 2130837811;
 			
-			// aapt resource value: 0x7f020131
-			public const int avd_hide_password_2 = 2130837809;
+			// aapt resource value: 0x7f020134
+			public const int avd_hide_password_2 = 2130837812;
 			
-			// aapt resource value: 0x7f020132
-			public const int avd_hide_password_3 = 2130837810;
+			// aapt resource value: 0x7f020135
+			public const int avd_hide_password_3 = 2130837813;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020133
-			public const int avd_show_password_1 = 2130837811;
+			// aapt resource value: 0x7f020136
+			public const int avd_show_password_1 = 2130837814;
 			
-			// aapt resource value: 0x7f020134
-			public const int avd_show_password_2 = 2130837812;
+			// aapt resource value: 0x7f020137
+			public const int avd_show_password_2 = 2130837815;
 			
-			// aapt resource value: 0x7f020135
-			public const int avd_show_password_3 = 2130837813;
+			// aapt resource value: 0x7f020138
+			public const int avd_show_password_3 = 2130837816;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -3002,11 +3002,11 @@ namespace TabbedPageWithNavigationPage.Droid
 			// aapt resource value: 0x7f020129
 			public const int notification_icon_background = 2130837801;
 			
-			// aapt resource value: 0x7f02012e
-			public const int notification_template_icon_bg = 2130837806;
+			// aapt resource value: 0x7f020131
+			public const int notification_template_icon_bg = 2130837809;
 			
-			// aapt resource value: 0x7f02012f
-			public const int notification_template_icon_low_bg = 2130837807;
+			// aapt resource value: 0x7f020132
+			public const int notification_template_icon_low_bg = 2130837810;
 			
 			// aapt resource value: 0x7f02012a
 			public const int notification_tile_bg = 2130837802;
@@ -3015,10 +3015,19 @@ namespace TabbedPageWithNavigationPage.Droid
 			public const int notify_panel_notification_icon_bg = 2130837803;
 			
 			// aapt resource value: 0x7f02012c
-			public const int tooltip_frame_dark = 2130837804;
+			public const int schedule = 2130837804;
 			
 			// aapt resource value: 0x7f02012d
-			public const int tooltip_frame_light = 2130837805;
+			public const int settings = 2130837805;
+			
+			// aapt resource value: 0x7f02012e
+			public const int today = 2130837806;
+			
+			// aapt resource value: 0x7f02012f
+			public const int tooltip_frame_dark = 2130837807;
+			
+			// aapt resource value: 0x7f020130
+			public const int tooltip_frame_light = 2130837808;
 			
 			static Drawable()
 			{
