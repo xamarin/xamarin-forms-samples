7b6ef5c3d7336894d42c202aa053ae73ecee681b
Robert Zimnoch <r.zimnoch@partner.samsung.com>
2018-06-18 15:42:24 +0200

    Fix Tizen project for CustomRenderers/ViewCell (API changes)

--- CustomRenderers/ViewCell/Tizen/NativeCellRenderer.cs
+++ CustomRenderers/ViewCell/Tizen/NativeCellRenderer.cs
@@ -5,7 +5,7 @@ namespace CustomRenderer
 {
     class NativeCellRenderer : ViewCellRenderer
     {
-        private static readonly ElmSharp.Size WindowSize = Forms.Context.MainWindow.ScreenSize;
+        private static readonly double PageHeight = App.Current.MainPage.Height;
 
         protected override ElmSharp.EvasObject OnGetContent(Cell cell, string part)
         {
@@ -14,42 +14,42 @@ namespace CustomRenderer
                 return null;
             }
             NativeCell viewCell = (NativeCell)cell;
-            var minimumHeight = WindowSize.Height > 800 ? 96 : 76;
+            var minimumHeight = PageHeight > 800 ? 96 : 76;
 
-            ElmSharp.Box mainBox = new ElmSharp.Box(Forms.Context.MainWindow);
+            ElmSharp.Box mainBox = new ElmSharp.Box(Forms.NativeParent);
             mainBox.BackgroundColor = Color.LightYellow.ToNative();
             mainBox.MinimumHeight = minimumHeight;
             mainBox.IsHorizontal = false;
             mainBox.SetAlignment(-1, -1);
             mainBox.Show();
 
-            ElmSharp.Box contentBox = new ElmSharp.Box(Forms.Context.MainWindow);
+            ElmSharp.Box contentBox = new ElmSharp.Box(Forms.NativeParent);
             contentBox.MinimumHeight = minimumHeight;
             contentBox.IsHorizontal = true;
             contentBox.SetAlignment(-1, -1);
             contentBox.Show();
 
-            ElmSharp.Box left = new ElmSharp.Box(Forms.Context.MainWindow);
+            ElmSharp.Box left = new ElmSharp.Box(Forms.NativeParent);
             left.IsHorizontal = false;
             left.Show();
             left.SetWeight(4.0, 1);
             left.SetAlignment(-1, -1);
             contentBox.PackEnd(left);
 
-            ElmSharp.Label titleName = new ElmSharp.Label(Forms.Context.MainWindow);
+            ElmSharp.Label titleName = new ElmSharp.Label(Forms.NativeParent);
             left.PackEnd(titleName);
             titleName.Show();
             titleName.Text = $"<span font_size=34 font_style=italic color=#7F3300>   {viewCell.Name}</span>";
             titleName.MinimumWidth = 250;
             titleName.SetAlignment(-1, -1);
 
-            ElmSharp.Label titleCategory = new ElmSharp.Label(Forms.Context.MainWindow);
+            ElmSharp.Label titleCategory = new ElmSharp.Label(Forms.NativeParent);
             left.PackEnd(titleCategory);
             titleCategory.Show();
             titleCategory.Text = $"<span align=center font_size=24 color=#008000>{viewCell.Category}</span>";
             titleCategory.SetAlignment(-1, -1);
 
-            ElmSharp.Box right = new ElmSharp.Box(Forms.Context.MainWindow);
+            ElmSharp.Box right = new ElmSharp.Box(Forms.NativeParent);
             right.Show();
             right.MinimumWidth = 96;
             right.MinimumHeight = minimumHeight;
