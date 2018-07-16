171b80d3fac1c3f17ef4bf8c59a4489538dfbcba
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-07-16 17:46:37 +0100

    Reformatted code.

--- UserInterface/Text/TextSample/Views/LabelPageCode.cs
+++ UserInterface/Text/TextSample/Views/LabelPageCode.cs
@@ -2,26 +2,26 @@
 
 namespace TextSample
 {
-	public class LabelPageCode : ContentPage
-	{
-		public LabelPageCode ()
-		{
-			var layout = new StackLayout{ Padding = new Thickness (5, 10) };
+    public class LabelPageCode : ContentPage
+    {
+        public LabelPageCode ()
+        {
+            var layout = new StackLayout{ Padding = new Thickness (5, 10) };
 
-			layout.Children.Add (new Label{ TextColor = Color.FromHex ("#77d065"), Text = "This is a green label." });
+            layout.Children.Add (new Label{ TextColor = Color.FromHex ("#77d065"), Text = "This is a green label." });
             layout.Children.Add (new Label{ Text = "This is a default, non-customized label." });
-			layout.Children.Add (new Label{ Text = "This label has a font size of 30.", FontSize = 30 });
-			layout.Children.Add (new Label{ Text = "This is bold text.", FontAttributes = FontAttributes.Bold });
+            layout.Children.Add (new Label{ Text = "This label has a font size of 30.", FontSize = 30 });
+            layout.Children.Add (new Label{ Text = "This is bold text.", FontAttributes = FontAttributes.Bold });
 
             var formattedString = new FormattedString ();
-			formattedString.Spans.Add (new Span{ Text = "Red bold, ", ForegroundColor = Color.Red, FontAttributes = FontAttributes.Bold });
+            formattedString.Spans.Add (new Span{ Text = "Red bold, ", ForegroundColor = Color.Red, FontAttributes = FontAttributes.Bold });
             formattedString.Spans.Add (new Span { Text = "default, " });
-			formattedString.Spans.Add (new Span { Text = "italic small.", FontAttributes = FontAttributes.Italic, FontSize =  Device.GetNamedSize(NamedSize.Small, typeof(Label)) });
-			
+            formattedString.Spans.Add (new Span { Text = "italic small.", FontAttributes = FontAttributes.Italic, FontSize =  Device.GetNamedSize(NamedSize.Small, typeof(Label)) });
+
             layout.Children.Add (new Label { FormattedText = formattedString });
 
             this.Title = "Label Demo - Code";
-			this.Content = layout;
-		}
-	}
+            this.Content = layout;
+        }
+    }
 }
