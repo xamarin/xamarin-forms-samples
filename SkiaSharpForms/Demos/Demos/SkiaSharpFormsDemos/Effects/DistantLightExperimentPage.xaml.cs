a6f2af034d0608ddd00975621455f9f35a2fcc20
Charles Petzold <chape@microsoft.com>
2018-07-30 17:22:53 -0400

    [SkiaSharp Forms] Light and Noise samples

---
+++ SkiaSharpForms/Demos/Demos/SkiaSharpFormsDemos/Effects/DistantLightExperimentPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace SkiaSharpFormsDemos.Effects
+{
+	public partial class DistantLightExperimentPage : InteractivePage
+	{
+        const string TEXT = "Distant Lit";
+
+        public DistantLightExperimentPage ()
+		{
+            touchPoints = new TouchPoint[1];
+
+            touchPoints[0] = new TouchPoint
+            {
+                Center = new SKPoint(200, 200)
+            };
+
+            InitializeComponent ();
+            baseCanvasView = canvasView;
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            canvasView.InvalidateSurface();
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            canvasView.InvalidateSurface();
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            // Base the background color on backgroundSwitch
+            canvas.Clear(backgroundSwitch.IsToggled ? SKColors.Black : new SKColor());
+
+            // Construct direction
+            SKPoint3 direction = new SKPoint3(touchPoints[0].Center.X,
+                                              touchPoints[0].Center.Y,
+                                              (float)zSlider.Value);
+
+            // Get other slider values
+            float surfaceHeight = (float)heightSlider.Value;
+            float lightConstant = (float)constantSlider.Value;
+            float shininess = (float)shininessSlider.Value;
+
+            using (SKPaint paint = new SKPaint())
+            {
+                paint.IsAntialias = true;
+                paint.TextSize = info.Width / (TEXT.Length / 2f);
+                
+                if (!specularSwitch.IsToggled)
+                {
+                    paint.ImageFilter =
+                        /*
+                                                SKImageFilter.CreateDistantLitDiffuse(direction,
+                                                                                      SKColors.White,
+                                                                                      surfaceHeight, 
+                                                                                      lightConstant);
+                        */
+
+                        SKImageFilter.CreatePointLitDiffuse(direction,      // actually location
+                                                            SKColors.White,
+                                                            surfaceHeight,      // actually scale
+                                                            lightConstant);
+
+                }
+                else
+                {
+                    paint.ImageFilter =
+                        /*
+
+                                                SKImageFilter.CreateDistantLitSpecular(direction,
+                                                                                       SKColors.White,
+                                                                                       surfaceHeight, 
+                                                                                       lightConstant, 
+                                                                                       shininess);
+                        */
+
+
+                        SKImageFilter.CreatePointLitSpecular(direction,
+                                                             SKColors.White,
+                                                             surfaceHeight,
+                                                             lightConstant,
+                                                             shininess);
+
+
+                }
+
+                // Center the text in the canvas
+                SKRect textBounds = new SKRect();
+                paint.MeasureText(TEXT, ref textBounds);
+
+                float xText = info.Width / 2 - textBounds.MidX;
+                float yText = info.Height / 2 - textBounds.MidY;
+
+                canvas.DrawText(TEXT, xText, yText, paint);
+            }
+
+            // Draw the touch point
+            using (SKPaint paint = new SKPaint())
+            {
+                paint.Style = SKPaintStyle.Stroke;
+                paint.Color = SKColors.Red;
+                paint.StrokeWidth = 3;
+
+                canvas.DrawCircle(touchPoints[0].Center, touchPoints[0].Radius, paint);
+            }
+        }
+
+    }
+}
---
+++ SkiaSharpForms/Demos/Demos/SkiaSharpFormsDemos/Effects/PerlinNoisePage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace SkiaSharpFormsDemos.Effects
+{
+	public partial class PerlinNoisePage : ContentPage
+	{
+		public PerlinNoisePage ()
+		{
+			InitializeComponent ();
+		}
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            canvasView.InvalidateSurface();
+        }
+
+        void OnStepperValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            canvasView.InvalidateSurface();
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            canvasView.InvalidateSurface();
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+            canvas.Clear();
+
+            // Get values from sliders
+            float baseFreqX = (float)baseFrequencyXSlider.Value;
+            float baseFreqY = (float)baseFrequencyYSlider.Value;
+            int numOctaves = (int)octavesStepper.Value;
+            bool useTiles = tileSwitch.IsToggled;
+
+            using (SKPaint paint = new SKPaint())
+            {
+                SKShader fractalNoiseShader = null;
+                SKShader turbulenceShader = null;
+                SKPointI tileSize = new SKPointI(100, 100);
+
+                if (useTiles)
+                {
+                    fractalNoiseShader =
+                        SKShader.CreatePerlinNoiseFractalNoise(baseFreqX,
+                                                               baseFreqY,
+                                                               numOctaves,
+                                                               0,
+                                                               tileSize);
+
+                    turbulenceShader = 
+                        SKShader.CreatePerlinNoiseTurbulence(baseFreqX,
+                                                             baseFreqY,
+                                                             numOctaves,
+                                                             0,
+                                                             tileSize);
+                }
+                else
+                {
+                    fractalNoiseShader =
+                        SKShader.CreatePerlinNoiseFractalNoise(baseFreqX,
+                                                               baseFreqY,
+                                                               numOctaves,
+                                                               0);
+
+                    turbulenceShader =
+                        SKShader.CreatePerlinNoiseTurbulence(baseFreqX,
+                                                             baseFreqY,
+                                                             numOctaves,
+                                                             0);
+                }
+
+                // Display fractal noise
+                paint.Shader = fractalNoiseShader;
+                SKRect rect = new SKRect(0, 0, info.Width, info.Height / 2);
+                canvas.DrawRect(rect, paint);
+
+                // Display turbulence
+                paint.Shader = turbulenceShader;
+                rect = new SKRect(0, info.Height / 2, info.Width, info.Height);
+                canvas.DrawRect(rect, paint);
+            }
+        }
+    }
+}
