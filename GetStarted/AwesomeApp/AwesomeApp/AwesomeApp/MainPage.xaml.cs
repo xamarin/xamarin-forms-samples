42eec660c6963881407f99812c28be7181c03575
Craig Dunn <craig.dunn@conceptdevelopment.net>
2018-09-25 11:07:29 -0700

    [get started] first app

---
+++ GetStarted/AwesomeApp/AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
