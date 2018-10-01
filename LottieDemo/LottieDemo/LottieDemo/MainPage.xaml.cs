7f4d547045d6ad4f58d1386625258eb4d66637da
LucioMSP <luciomsp@geeks.ms>
2018-10-01 12:06:06 -0500

    AirBnB.Design – Using Lottie [Xamarin.Forms]
    Example of Xamarin.Forms with Lottie
    
    More information:
    
    https://vicenteguzman.mx/2018/04/02/airbnb-design-using-lottie-xamarin-forms/

---
+++ LottieDemo/LottieDemo/LottieDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+using System.Windows.Input;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace LottieDemo
+{
+    public partial class MainPage : ContentPage
+	{
+        public ICommand ClickedCommand { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            playButton.Clicked += (sender, e) => animationView.Play();
+            playSegmentsButton.Clicked += (sender, e) => animationView.PlayProgressSegment(0.65f, 0.0f);
+            playFramesButton.Clicked += (sender, e) => animationView.PlayFrameSegment(50, 1);
+           
+            BindingContext = this;
+
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+        }
+
+        private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            animationView.Progress = (float)e.NewValue;
+        }
+
+        private void Handle_OnFinish(object sender, System.EventArgs e)
+        { }
+
+        private void DisplayAlert(string message)
+        { }
+    }
+}
+
