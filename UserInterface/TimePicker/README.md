05d25582361db9db24bcb15c52834107f18cd0f3
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-10-16 18:44:40 +0100

    New sample.

---
+++ UserInterface/TimePicker/README.md
@@ -0,0 +1,11 @@
+TimePicker
+==========
+
+This sample demonstrates how to use a `TimePicker` view to select a time, and respond to a time being selected.
+
+For more information about this sample, see [Xamarin.Forms TimePicker](https://docs.microsoft.com/xamarin/xamarin-forms/user-interface/timepicker).
+
+Author
+------
+
+Charles Petzold / David Britch
---
+++ UserInterface/TimePicker/SetTimer/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SetTimer
+{
+    public partial class MainPage : ContentPage
+    {
+        DateTime _triggerTime;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        bool OnTimerTick()
+        {
+            if (_switch.IsToggled && DateTime.Now >= _triggerTime)
+            {
+                _switch.IsToggled = false;
+                DisplayAlert("Timer Alert", "The '" + _entry.Text + "' timer has elapsed", "OK");
+            }
+            return true;
+        }
+
+        void OnTimePickerPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == "Time")
+            {
+                SetTriggerTime();
+            }
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            SetTriggerTime();
+        }
+
+        void SetTriggerTime()
+        {
+            if (_switch.IsToggled)
+            {
+                _triggerTime = DateTime.Today + _timePicker.Time;
+                if (_triggerTime < DateTime.Now)
+                {
+                    _triggerTime += TimeSpan.FromDays(1);
+                }
+            }
+        }
+    }
+}
