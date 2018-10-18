fdf34d7c87c968976d04961c27b7fa2fcb6687fd
Tomislav Eric <tomislav.eric@arconsis.com>
2018-10-18 12:10:43 +0200

    create Dependency registration module

---
+++ Navigation/ViewModelFirstNavigation/ViewModelFirstNavigation/Bootstrapping/Modules/DependencyRegistrationModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Autofac;
+using ViewModelFirstNavigation.Interfaces;
+using ViewModelFirstNavigation.Services;
+using Xamarin.Forms;
+
+namespace ViewModelFirstNavigation.Bootstrapping.Modules
+{
+    public class DependencyRegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ViewFactory>().As<IViewFactory>().SingleInstance();
+            builder.RegisterType<Navigator>().As<INavigator>().SingleInstance();
+            builder.Register<INavigation>(context => Application.Current.MainPage.Navigation).SingleInstance();
+        }
+    }
+}
