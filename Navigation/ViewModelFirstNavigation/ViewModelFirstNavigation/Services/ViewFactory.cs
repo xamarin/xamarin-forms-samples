6b54b7ac0da4a0a4f642aa35586c5cf67bb3e08e
Tomislav Eric <tomislav.eric@arconsis.com>
2018-10-18 12:05:58 +0200

    Create ViewFactory service

---
+++ Navigation/ViewModelFirstNavigation/ViewModelFirstNavigation/Services/ViewFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using ViewModelFirstNavigation.Interfaces;
+using Xamarin.Forms;
+
+namespace ViewModelFirstNavigation.Services
+{
+    public class ViewFactory : IViewFactory
+    {
+        private readonly IDictionary<Type, Type> _map = new Dictionary<Type, Type>();
+        private readonly IComponentContext _componentContext;
+
+        public ViewFactory(IComponentContext componentContext)
+        {
+            _componentContext = componentContext;
+        }
+
+        public void Register<TViewModel, TView>() where TViewModel : class, IViewModelBase where TView : Page
+        {
+            _map[typeof(TViewModel)] = typeof(TView);
+        }
+
+        public Page Resolve<TViewModel>() where TViewModel : class, IViewModelBase
+        {
+            var viewModel = _componentContext.Resolve<TViewModel>();
+            var viewType = _map[typeof(TViewModel)];
+            var view = _componentContext.Resolve(viewType) as Page;
+
+            view.BindingContext = viewModel;
+            return view;
+        }
+    }
+}
