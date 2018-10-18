d954efb4735d2418039e53766911d5e8a879c35b
Tomislav Eric <tomislav.eric@arconsis.com>
2018-10-18 13:58:07 +0200

    add label to second page

--- Navigation/ViewModelFirstNavigation/ViewModelFirstNavigation/ViewModels/SecondViewModel.cs
+++ Navigation/ViewModelFirstNavigation/ViewModelFirstNavigation/ViewModels/SecondViewModel.cs
@@ -4,5 +4,6 @@ namespace ViewModelFirstNavigation.ViewModels
     public class SecondViewModel : BaseViewModel
     {
         public string PageTitle => "Second Page";
+        public string ContentText => "Xamarin rocks!";
     }
 }
