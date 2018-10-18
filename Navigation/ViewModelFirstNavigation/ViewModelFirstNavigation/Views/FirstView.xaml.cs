676cf39521d2ba706b8e77b0bd2d93c3769a1047
Tomislav Eric <tomislav.eric@arconsis.com>
2018-10-18 13:52:50 +0200

    create views to corresponding

---
+++ Navigation/ViewModelFirstNavigation/ViewModelFirstNavigation/Views/FirstView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ViewModelFirstNavigation.Views
+{
+    public partial class FirstView : ContentPage
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Navigation/ViewModelFirstNavigation/ViewModelFirstNavigation/Views/SecondView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ViewModelFirstNavigation.Views
+{
+    public partial class SecondView : ContentPage
+    {
+        public SecondView()
+        {
+            InitializeComponent();
+        }
+    }
+}
