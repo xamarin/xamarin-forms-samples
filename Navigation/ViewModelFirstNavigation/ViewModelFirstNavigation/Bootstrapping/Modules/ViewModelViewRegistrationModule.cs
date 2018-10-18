73f3f2addcb2d3c830d2afddfe664e351f2ca0e4
Tomislav Eric <tomislav.eric@arconsis.com>
2018-10-18 12:12:29 +0200

    create viewmodel view registration

---
+++ Navigation/ViewModelFirstNavigation/ViewModelFirstNavigation/Bootstrapping/Modules/ViewModelViewRegistrationModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autofac;
+
+namespace ViewModelFirstNavigation.Bootstrapping.Modules
+{
+    public class ViewModelViewRegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterType<HomePage>().SingleInstance();
+            //builder.RegisterType<HomeBaseViewModel>().SingleInstance();
+
+            //builder.RegisterType<SecondPage>();
+            //builder.RegisterType<SecondViewModel>();
+        }
+    }
+}
