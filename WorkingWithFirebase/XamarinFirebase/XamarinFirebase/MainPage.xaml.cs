e5d24f684f24bd41ec813342a192540394ab24fb
Delpin Susai Raj <susairajs@outlook.com>
2018-10-29 12:36:00 +0530

    XamarinForms With firebase

---
+++ WorkingWithFirebase/XamarinFirebase/XamarinFirebase/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Firebase.Storage;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFirebase
+{
+    public partial class MainPage : ContentPage
+    {
+        MediaFile file;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            imgBanner.Source = ImageSource.FromResource("XamarinFirebase.images.banner.png");
+            imgChoosed.Source = ImageSource.FromResource("XamarinFirebase.images.default.jpg");
+        }
+
+        private async void btnPick_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+                if (file == null)
+                    return;
+                imgChoosed.Source = ImageSource.FromStream(() =>
+                {
+                   var imageStram = file.GetStream();
+                    return imageStram;
+                });
+                await StoreImages(file.GetStream());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private async void btnStore_Clicked(object sender, EventArgs e)
+        {
+            await StoreImages(file.GetStream());
+        }
+
+        public async Task<string> StoreImages(Stream imageStream)
+        {
+            var stroageImage = await new FirebaseStorage("xamarinfirebase-d3352.appspot.com")
+                .Child("XamarinMonkeys")
+                .Child("image.jpg")
+                .PutAsync(imageStream);
+            string imgurl = stroageImage;
+            return imgurl;
+        }
+    }
+}
