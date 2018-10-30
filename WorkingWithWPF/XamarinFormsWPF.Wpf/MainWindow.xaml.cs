45409f9f2a2f2725247706606519a8a4902aeacd
Xamarin Monkeys <xamarinmonkeys@outlook.com>
2018-10-30 18:12:36 +0530

    WPF Sample Added

---
+++ WorkingWithWPF/XamarinFormsWPF.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace XamarinFormsWPF.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            LoadApplication(new XamarinFormsWPF.App());
+        }
+    }
+}
