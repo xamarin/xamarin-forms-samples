75f2833d61703adf61da31e79688103a55c0527e
Delpin Susai Raj <susairajs@outlook.com>
2018-10-30 13:04:31 +0530

    Relam Database sample added

---
+++ WorkingWthRealm/XamarinFormsRelam/XamarinFormsRelam/Models/OptionItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinFormsRelam.Models
+{
+    public class OptionItems
+    {
+        public string OptionText { get; set; }
+
+        public int StudentId { get; set; }
+    }
+}
