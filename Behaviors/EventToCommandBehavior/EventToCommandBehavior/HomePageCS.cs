474e0c46c83fe34c16d329d2d116ee32c26a346d
David Britch <davidbritch@Davids-MacBook-Pro.local>
2018-11-09 09:56:21 +0000

    Formatted files.

--- Behaviors/EventToCommandBehavior/EventToCommandBehavior/HomePageCS.cs
+++ Behaviors/EventToCommandBehavior/EventToCommandBehavior/HomePageCS.cs
@@ -2,41 +2,43 @@
 
 namespace EventToCommandBehavior
 {
-	public class HomePageCS : ContentPage
-	{
-		public HomePageCS ()
-		{
-			BindingContext = new HomePageViewModel ();
+    public class HomePageCS : ContentPage
+    {
+        public HomePageCS()
+        {
+            BindingContext = new HomePageViewModel();
 
-			var listView = new ListView ();
-			listView.SetBinding (ItemsView<Cell>.ItemsSourceProperty, "People");
+            var listView = new ListView();
+            listView.SetBinding(ItemsView<Cell>.ItemsSourceProperty, "People");
             listView.ItemTemplate = new DataTemplate(() =>
             {
                 var textCell = new TextCell();
                 textCell.SetBinding(TextCell.TextProperty, "Name");
                 return textCell;
             });
-			listView.Behaviors.Add (new EventToCommandBehavior {
-				EventName = "ItemSelected",
-				Command = ((HomePageViewModel)BindingContext).OutputAgeCommand,
-				Converter = new SelectedItemEventArgsToSelectedItemConverter ()
-			});
+            listView.Behaviors.Add(new EventToCommandBehavior
+            {
+                EventName = "ItemSelected",
+                Command = ((HomePageViewModel)BindingContext).OutputAgeCommand,
+                Converter = new SelectedItemEventArgsToSelectedItemConverter()
+            });
 
-			var selectedItemLabel = new Label ();
-			selectedItemLabel.SetBinding (Label.TextProperty, "SelectedItemText");
+            var selectedItemLabel = new Label();
+            selectedItemLabel.SetBinding(Label.TextProperty, "SelectedItemText");
 
-			Content = new StackLayout { 
+            Content = new StackLayout
+            {
                 Margin = new Thickness(20),
-				Children = {
-					new Label {
-						Text = "Behaviors Demo",
-						FontAttributes = FontAttributes.Bold,
-						HorizontalOptions = LayoutOptions.Center
-					},
-					listView,
-					selectedItemLabel
-				}
-			};
-		}
-	}
+                Children = {
+                    new Label {
+                        Text = "Behaviors Demo",
+                        FontAttributes = FontAttributes.Bold,
+                        HorizontalOptions = LayoutOptions.Center
+                    },
+                    listView,
+                    selectedItemLabel
+                }
+            };
+        }
+    }
 }
