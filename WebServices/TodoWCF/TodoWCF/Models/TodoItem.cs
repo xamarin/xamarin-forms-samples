8e489ac739c95c9b013aef076e057c59824a2707
Justin Johnson <justin.d.johnson@microsoft.com>
2019-03-22 15:14:09 -0600

    Rebuilt TodoWCF as a PCL and rebound service

---
+++ WebServices/TodoWCF/TodoWCF/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoWCF
+{
+	public class TodoItem
+	{
+		public string ID { get; set; }
+
+		public string Name { get; set; }
+
+		public string Notes { get; set; }
+
+		public bool Done { get; set; }
+	}
+}
---
+++ WebServices/TodoWCF/TodoWCF/Views/TodoListPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TodoWCF
+{
+	public partial class TodoListPage : ContentPage
+	{
+		bool alertShown = false;
+
+		public TodoListPage ()
+		{
+			InitializeComponent ();
+		}
+
+		protected async override void OnAppearing ()
+		{
+			base.OnAppearing ();
+
+			if (Constants.SoapUrl.Contains ("developer.xamarin.com")) {
+				if (!alertShown) {
+					await DisplayAlert (
+						"Hosted Back-End",
+						"This app is running against Xamarin's read-only WCF service. To create, edit, and delete data you must update the service endpoint to point to your own hosted WCF service.",
+						"OK");
+					alertShown = true;				
+				}
+			}
+
+			listView.ItemsSource = await App.TodoManager.GetTodoItemsAsync ();
+		}
+
+		void OnAddItemClicked (object sender, EventArgs e)
+		{
+			var todoItem = new TodoItem () {
+				ID = Guid.NewGuid ().ToString ()
+			};
+			var todoPage = new TodoItemPage (true);
+			todoPage.BindingContext = todoItem;
+			Navigation.PushAsync (todoPage);
+		}
+
+		void OnItemSelected (object sender, SelectedItemChangedEventArgs e)
+		{
+			var todoItem = e.SelectedItem as TodoItem;
+			var todoPage = new TodoItemPage ();
+			todoPage.BindingContext = todoItem;
+			Navigation.PushAsync (todoPage);
+		}
+	}
+}
