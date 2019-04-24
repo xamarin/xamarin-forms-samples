5677584f087038ba6a7dc8c270bf53a3ec460b83
David Britch <davidbritch@Davids-MacBook-Pro.local>
2019-04-24 17:28:30 +0100

    Search handler nav fixed.

--- UserInterface/Xaminals/Xaminals/Controls/AnimalSearchHandler.cs
+++ UserInterface/Xaminals/Xaminals/Controls/AnimalSearchHandler.cs
@@ -35,7 +35,8 @@ namespace Xaminals.Controls
 
             ShellNavigationState state = (App.Current.MainPage as Shell).CurrentState;
             // Note: strings will be URL encoded for navigation (e.g. "Blue Monkey" becomes "Blue%20Monkey"). Therefore, decode at the receiver.
-            await Shell.Current.GoToAsync($"{state.Location.AbsoluteUri}/{GetNavigationTarget()}?name={((Animal)item).Name}"); 
+            // This works because route names are unique in this application.
+            await Shell.Current.GoToAsync($"{GetNavigationTarget()}?name={((Animal)item).Name}");
         }
 
         string GetNavigationTarget()
