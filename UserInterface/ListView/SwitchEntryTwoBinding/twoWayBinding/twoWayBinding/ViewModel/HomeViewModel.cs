ab89505816fbfcc8e864300d15e595cd634e7592
David Britch <davidbritch@Davids-MacBook-Pro.local>
2019-04-26 14:12:30 +0100

    Reformatting.

--- UserInterface/ListView/SwitchEntryTwoBinding/twoWayBinding/twoWayBinding/ViewModel/HomeViewModel.cs
+++ UserInterface/ListView/SwitchEntryTwoBinding/twoWayBinding/twoWayBinding/ViewModel/HomeViewModel.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.ComponentModel;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 
 namespace twoWayBinding
 {
 	public static class HomeViewModel
-	{
-		
+	{		
 		public static ObservableCollection<light> lights { get; set; }
 
 		static HomeViewModel ()
 		{
 			HomeViewModel.lights = DataSource.getLights ();
 		}
-
-
 	}
 }
-
