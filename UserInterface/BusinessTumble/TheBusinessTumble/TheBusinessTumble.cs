21ead2598596f6cd6e1659fe8640fe18c801f939
David Britch <davidbritch@Davids-MacBook-Pro.local>
2019-04-26 13:28:51 +0100

    Minor update.

--- UserInterface/BusinessTumble/TheBusinessTumble/TheBusinessTumble.cs
+++ UserInterface/BusinessTumble/TheBusinessTumble/TheBusinessTumble.cs
@@ -1,48 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly:XamlCompilation(XamlCompilationOptions.Compile)]
 namespace TheBusinessTumble
 {
-	public class App : Application
-	{
-		public App ()
-		{
-			var nav = new NavigationPage ();
-			nav.BarBackgroundColor = Color.Transparent;
-			nav.BarTextColor = Color.White;
+    public class App : Application
+    {
+        public App()
+        {
+            var nav = new NavigationPage();
+            nav.BarBackgroundColor = Color.Transparent;
+            nav.BarTextColor = Color.White;
 
+            var listpage = new ContentPage();
+            listpage.Content = new ListView
+            {
+                ItemsSource = new List<ContentPage> 
+                {
+                    new StackLayoutPage(),
+                    new AbsoluteLayoutPage(),
+                    new RelativeLayoutPage()
+                }
+            };
+            ((ListView)listpage.Content).ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
+            {
+                nav.PushAsync((ContentPage)e.SelectedItem);
+            };
+            nav.PushAsync(listpage);
 
-			var listpage = new ContentPage();
-			listpage.Content = new ListView { ItemsSource = new List<ContentPage> {
-					new StackLayoutPage(),
-					new AbsoluteLayoutPage(),
-					new RelativeLayoutPage()
-				}
-			};
-			((ListView)listpage.Content).ItemSelected += (object sender, SelectedItemChangedEventArgs e) => {
-				nav.PushAsync((ContentPage)e.SelectedItem);
-			};
-			nav.PushAsync (listpage);
+            // The root page of your application
+            MainPage = nav;
+        }
 
-			// The root page of your application
-			MainPage = nav;
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
 
