f5aef2c87f5be1bc7a65284cf00c9fbb056ab025
David Britch <davidbritch@Davids-MacBook-Pro.local>
2019-05-07 12:20:14 +0100

    Sample update.

---
+++ UserInterface/CollectionViewDemos/CollectionViewDemos/Views/Sizing/VerticalListDynamicSizeItemsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CollectionViewDemos.ViewModels;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Views
+{
+    public partial class VerticalListDynamicSizeItemsPage : ContentPage
+    {
+        public VerticalListDynamicSizeItemsPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
+        }
+
+        void OnImageTapped(object sender, EventArgs e)
+        {
+            Image image = sender as Image;
+            image.HeightRequest = image.WidthRequest = image.HeightRequest.Equals(60) ? 100 : 60;
+        }
+    }
+}
