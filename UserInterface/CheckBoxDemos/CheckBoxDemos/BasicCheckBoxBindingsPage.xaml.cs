ba6a02c43c1e74bc11f1286bd1e3d8f0be3120aa
David Britch <davidbritch@Davids-MacBook-Pro.local>
2019-06-10 15:04:19 +0100

    New sample

---
+++ UserInterface/CheckBoxDemos/CheckBoxDemos/BasicCheckBoxBindingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CheckBoxDemos
+{
+    public partial class BasicCheckBoxBindingsPage : ContentPage
+    {
+        public BasicCheckBoxBindingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
