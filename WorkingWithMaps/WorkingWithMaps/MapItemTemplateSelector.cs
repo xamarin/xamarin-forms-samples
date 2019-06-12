89c014e1ba5acc6c1dd049f2821f9a2eff91675c
David Britch <davidbritch@Davids-MacBook-Pro.local>
2019-06-12 15:34:56 +0100

    Added ItemTemplateSelector example.

---
+++ WorkingWithMaps/WorkingWithMaps/MapItemTemplateSelector.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace WorkingWithMaps
+{
+    public class MapItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultTemplate { get; set; }
+        public DataTemplate XamarinTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((Location)item).Address.Contains("San Francisco") ? XamarinTemplate : DefaultTemplate;
+        }
+    }
+}
