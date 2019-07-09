74490be5a40b3ea89633184760156a66c96277de
Justin Johnson <justin.d.johnson@microsoft.com>
2019-07-09 09:51:36 -0600

    Xaml page working and other pages created

---
+++ UserInterface/ProgressBarDemos/ProgressBarDemos/ProgressBarDemos/ProgressBarCodePage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ProgressBarDemos
+{
+    public class ProgressBarCodePage : ContentPage
+    {
+        public ProgressBarCodePage()
+        {
+            
+
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
---
+++ UserInterface/ProgressBarDemos/ProgressBarDemos/ProgressBarDemos/ProgressBarXamlPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProgressBarDemos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgressBarXamlPage : ContentPage
+    {
+        float progress = 0f;
+
+        public ProgressBarXamlPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            progress += 0.2f;
+
+            if (progress > 1)
+            {
+                progress = 0;
+            }
+
+            // directly set the new progress value
+            defaultProgressBar.Progress = progress;
+
+            // animate to the new value over 750 milliseconds using Linear easing
+            styledProgressBar.ProgressTo(progress, 750, Easing.Linear);
+        }
+    }
+}
