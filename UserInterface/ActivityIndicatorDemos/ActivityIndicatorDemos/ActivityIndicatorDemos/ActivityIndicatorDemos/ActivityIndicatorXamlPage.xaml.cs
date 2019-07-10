32f6515ddbc2e2cff435059ec337f42b0fd9bf73
Justin Johnson <justin.d.johnson@microsoft.com>
2019-07-10 11:16:37 -0600

    Initial sample checkin

---
+++ UserInterface/ActivityIndicatorDemos/ActivityIndicatorDemos/ActivityIndicatorDemos/ActivityIndicatorDemos/ActivityIndicatorXamlPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ActivityIndicatorDemos
+{
+    public partial class ActivityIndicatorXamlPage : ContentPage
+    {
+        bool isTaskRunning;
+
+        public ActivityIndicatorXamlPage()
+        {
+            InitializeComponent();
+            UpdateUiState();
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            isTaskRunning = !isTaskRunning;
+            UpdateUiState();
+        }
+
+        void UpdateUiState()
+        {
+            runningStatusLabel.Text = isTaskRunning ? "A task is in progress." : "All tasks complete!";
+            defaultActivityIndicator.IsRunning = isTaskRunning;
+            styledActivityIndicator.IsRunning = isTaskRunning;
+        }
+    }
+}
