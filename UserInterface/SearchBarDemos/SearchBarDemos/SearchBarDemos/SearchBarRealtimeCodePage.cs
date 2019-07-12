65db67df7a4c2df6cae2e18670fa2e657b94e4d2
Justin Johnson <justin.d.johnson@microsoft.com>
2019-07-12 11:40:21 -0600

    Added realtime code page

---
+++ UserInterface/SearchBarDemos/SearchBarDemos/SearchBarDemos/SearchBarRealtimeCodePage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SearchBarDemos
+{
+    public class SearchBarRealtimeCodePage : ContentPage
+    {
+        ListView searchResults;
+
+        public SearchBarRealtimeCodePage()
+        {
+            Title = "Code Realtime SearchBar Page";
+            Padding = 10;
+
+            SearchBar searchBar = new SearchBar
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Placeholder = "Search fruits...",
+                CancelButtonColor = Color.Orange,
+                PlaceholderColor = Color.Orange
+            };
+            searchBar.TextChanged += (sender, e) =>
+            {
+                searchResults.ItemsSource = Constants.GetFilteredFruits(searchBar.Text);
+            };
+
+            Label label = new Label
+            {
+                Text = "Type in the searchbox to filter results in realtime.",
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            searchResults = new ListView
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Fill
+            };
+
+            Content = new StackLayout
+            {
+                Children = {
+                    searchBar,
+                    label,
+                    searchResults
+                }
+            };
+
+            searchResults.ItemsSource = Constants.Fruits;
+        }
+    }
+}
