fd8e2849d8eafde1bee61b4ff04d0157a03dece6
Justin Johnson <justin.d.johnson@microsoft.com>
2019-07-15 08:36:46 -0600

    Tested on iOS and Android

--- UserInterface/SearchBarDemos/SearchBarDemos/SearchBarDemos/SearchBarCodePage.cs
+++ UserInterface/SearchBarDemos/SearchBarDemos/SearchBarDemos/SearchBarCodePage.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Input;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace SearchBarDemos
 {
