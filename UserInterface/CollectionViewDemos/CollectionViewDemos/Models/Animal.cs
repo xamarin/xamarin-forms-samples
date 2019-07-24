b33d3b5ccee22b7407d1eff48663c864ed18dbcd
David Britch <davidbritch@Davids-MacBook-Pro-2.local>
2019-07-24 12:43:36 +0100

    Added pages that demo grouping.

---
+++ UserInterface/CollectionViewDemos/CollectionViewDemos/Models/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CollectionViewDemos.Models
+{
+    public class Animal : List<AnimalDetails>
+    {
+        public string Category { get; private set; }
+
+        public Animal(string category, List<AnimalDetails> details) : base(details)
+        {
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return Category;
+        }
+    }
+
+    public class AnimalDetails
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
