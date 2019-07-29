d9f5a0a1c3420f6c0e32b95735e72092dd423536
Justin Johnson <justin.d.johnson@microsoft.com>
2019-07-29 09:49:51 -0600

    Removed unneeded essentials stuff

--- UserInterface/ToolbarItemDemos/ToolbarItemDemos/ToolbarItemDemos.Android/MainActivity.cs
+++ UserInterface/ToolbarItemDemos/ToolbarItemDemos/ToolbarItemDemos.Android/MainActivity.cs
@@ -19,15 +19,8 @@ namespace ToolbarItemDemos.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
