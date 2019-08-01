b895d2b51e1c864f812d5e47b9ce75194e2a81de
Justin Johnson <justin.d.johnson@microsoft.com>
2019-08-01 15:15:31 -0600

    Initial project commit

---
+++ UserInterface/FrameDemos/FrameDemos/FrameDemos/FrameCodePage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FrameDemos
+{
+    public class FrameCodePage : ContentPage
+    {
+        public FrameCodePage()
+        {
+            Padding = 10;
+            Title = "Frame Code Demo";
+
+            Label label = new Label
+            {
+                Text = "example"
+            };
+
+            Frame frame = new Frame
+            {
+                BackgroundColor = Color.LightGray,
+                BorderColor = Color.Orange,
+                Content = label,
+                CornerRadius = 10,
+                HasShadow = true,
+            };
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    frame
+                }
+            };
+        }
+    }
+}
