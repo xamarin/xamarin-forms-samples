57fcba6595880adf9678ddfa4fa143bcc37f0af8
Justin Johnson <justin.d.johnson@microsoft.com>
2019-08-09 09:35:29 -0600

    Jusjohns xfcontrols menuitem (#503)
    * Basic project structure checkin
    
    * Roughed out MVVM example
    
    * Three diff examples working on UWP
    
    * Minor formatting and usings cleanup
    
    * Added readme and screenshot
    
    * usings cleanup
    
    * Android manifest fix
    
    * Added icons to each platform and added to menuitems
    
    * Changes and cleanup during doc writing
    
    * PR feedback changes
    
    * Add UI to description, remove author, and tweak sample code.
    
    * update yaml to match path

---
+++ UserInterface/MenuItemDemos/MenuItemDemos/MenuItemDemos/MenuItemCodePage.cs
@@ -0,0 +1,84 @@
+﻿using MenuItemDemos.Services;
+using System;
+
+using Xamarin.Forms;
+
+namespace MenuItemDemos
+{
+    public class MenuItemCodePage : ContentPage
+    {
+        Label messageLabel;
+
+        public MenuItemCodePage()
+        {
+            Padding = 10;
+            Title = "MenuItem Code Demo";
+
+            messageLabel = new Label
+            {
+                TextColor = Color.Red,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions = LayoutOptions.Start
+            };
+
+            DataTemplate dataTemplate = new DataTemplate(() =>
+            {
+                Label label = new Label();
+                label.SetBinding(Label.TextProperty, ".");
+                ViewCell viewCell = new ViewCell
+                {
+                    View = label
+                };
+
+                MenuItem menuItem1 = new MenuItem
+                {
+                    Text = "Edit",
+                    IconImageSource = ImageSource.FromFile("icon.png")
+                };
+                menuItem1.Clicked += OnEditClicked;
+
+                MenuItem menuItem2 = new MenuItem
+                {
+                    Text = "Delete"
+                };
+                menuItem2.Clicked += OnDeleteClicked;
+
+                viewCell.ContextActions.Add(menuItem1);
+                viewCell.ContextActions.Add(menuItem2);
+
+                return viewCell;
+            });
+
+            ListView listView = new ListView
+            {
+                ItemsSource = DataService.GetListItems(),
+                ItemTemplate = dataTemplate
+            };
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Reveal the context menu by right-clicking (UWP), long-pressing (Android), or swiping (iOS) an item in the following list."
+                    },
+                    messageLabel,
+                    listView
+                }
+            };
+        }
+
+        void OnDeleteClicked(object sender, EventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            messageLabel.Text = $"Delete handler was called on {item.BindingContext}";
+        }
+
+        void OnEditClicked(object sender, EventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            messageLabel.Text = $"Edit handler was called on {item.BindingContext}";
+        }
+    }
+}
---
+++ UserInterface/MenuItemDemos/MenuItemDemos/MenuItemDemos/Services/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MenuItemDemos.Services
+{
+    public static class DataService
+    {
+        public static List<string> GetListItems(int itemCount = 10)
+        {
+            List<string> items = new List<string>();
+            for (var i = 1; i <= itemCount; i++)
+            {
+                items.Add($"Item {i}");
+            }
+            return items;
+        }
+    }
+}
