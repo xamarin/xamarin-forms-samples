1c2f8d16aed45bd163662af043b7726048e62afb
David Britch <davidbritch@Davids-MacBook-Pro-2.local>
2019-08-09 15:02:21 +0100

    Removed unnecessary lines.

--- UserInterface/FrameDemos/FrameDemos/FrameDemos/FrameXamlPage.xaml.cs
+++ UserInterface/FrameDemos/FrameDemos/FrameDemos/FrameXamlPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace FrameDemos
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FrameXamlPage : ContentPage
     {
         public FrameXamlPage()
