4f6f7212ad377021893ac5395c588466541cf8e0
David Britch <davidbritch@users.noreply.github.com>
2019-08-12 14:13:47 +0100

    Header/footer support.

---
+++ UserInterface/CollectionViewDemos/CollectionViewDemos/Views/HeadersAndFooters/HorizontalGridHeaderFooterViewPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CollectionViewDemos.ViewModels;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Views
+{
+    public partial class HorizontalGridHeaderFooterViewPage : ContentPage
+    {
+        public HorizontalGridHeaderFooterViewPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
+        }
+    }
+}
---
+++ UserInterface/CollectionViewDemos/CollectionViewDemos/Views/HeadersAndFooters/VerticalListHeaderFooterViewPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CollectionViewDemos.ViewModels;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Views
+{
+    public partial class VerticalListHeaderFooterViewPage : ContentPage
+    {
+        public VerticalListHeaderFooterViewPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
+        }
+    }
+}
