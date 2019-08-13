b72efef86c9928df7ed482e18184f543e1389ce1
Justin Johnson <justin.d.johnson@microsoft.com>
2019-08-13 15:45:03 -0600

    MVVM page working

---
+++ UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/CardViewMvvmPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CardViewDemo.Services;
+
+using Xamarin.Forms;
+
+namespace CardViewDemo
+{
+    public partial class CardViewMvvmPage : ContentPage
+    {
+        public CardViewMvvmPage()
+        {
+            InitializeComponent();
+            BindingContext = DataService.GetPersonCollection();
+        }
+    }
+}
