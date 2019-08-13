6f44d5d4ecbcdc8c5567f4788c31e5af1b97cc6f
Justin Johnson <justin.d.johnson@microsoft.com>
2019-08-13 08:29:39 -0600

    Template fixes

--- UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Controls/CardView.xaml.cs
+++ UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Controls/CardView.xaml.cs
@@ -9,7 +9,6 @@ using Xamarin.Forms.Xaml;
 
 namespace CardViewDemo.Controls
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CardView : ContentView
     {
         public CardView()
