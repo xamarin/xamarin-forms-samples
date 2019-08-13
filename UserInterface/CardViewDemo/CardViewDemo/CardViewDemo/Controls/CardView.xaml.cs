723373ab386f98bcb5a53aaa709f5d21a39474c7
Justin Johnson <justin.d.johnson@microsoft.com>
2019-08-13 08:29:57 -0600

    Added bindable property

--- UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Controls/CardView.xaml.cs
+++ UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Controls/CardView.xaml.cs
@@ -11,6 +11,14 @@ namespace CardViewDemo.Controls
 {
     public partial class CardView : ContentView
     {
+        public static readonly BindableProperty CardTitleProperty = BindableProperty.Create("CardTitle", typeof(string), typeof(CardView), string.Empty);
+
+        public string CardTitle
+        {
+            get => (string)GetValue(CardView.CardTitleProperty);
+            set => SetValue(CardView.CardTitleProperty, value);
+        }
+
         public CardView()
         {
             InitializeComponent();
