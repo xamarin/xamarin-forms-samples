81a08ec26168f6e8d7a22065de870d80e11f9dfa
Justin Johnson <justin.d.johnson@microsoft.com>
2019-08-13 14:16:41 -0600

    Starting to wire up more properties and test implementation

--- UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Controls/CardView.xaml.cs
+++ UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Controls/CardView.xaml.cs
@@ -11,12 +11,40 @@ namespace CardViewDemo.Controls
 {
     public partial class CardView : ContentView
     {
-        public static readonly BindableProperty CardTitleProperty = BindableProperty.Create("CardTitle", typeof(string), typeof(CardView), string.Empty);
+        public static readonly BindableProperty CardTitleProperty = BindableProperty.Create(nameof(CardTitle), typeof(string), typeof(CardView), string.Empty);
+        public static readonly BindableProperty CardDescriptionProperty = BindableProperty.Create(nameof(CardDescription), typeof(string), typeof(CardView), string.Empty);
+        public static readonly BindableProperty BorderColorProperty = BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(CardView), Color.DarkGray);
+        public static readonly BindableProperty IconImageSourceProperty = BindableProperty.Create(nameof(IconImageSource), typeof(ImageSource), typeof(CardView), default(ImageSource));
+        public static readonly BindableProperty IconBackgroundColorProperty = BindableProperty.Create(nameof(IconBackgroundColor), typeof(Color), typeof(CardView), Color.LightGray);
 
         public string CardTitle
         {
             get => (string)GetValue(CardView.CardTitleProperty);
             set => SetValue(CardView.CardTitleProperty, value);
+        }
+
+        public string CardDescription
+        {
+            get => (string)GetValue(CardView.CardDescriptionProperty);
+            set => SetValue(CardView.CardDescriptionProperty, value);
+        }
+
+        public Color BorderColor
+        {
+            get => (Color)GetValue(CardView.BorderColorProperty);
+            set => SetValue(CardView.BorderColorProperty, value);
+        }
+
+        public ImageSource IconImageSource
+        {
+            get => (ImageSource)GetValue(CardView.IconImageSourceProperty);
+            set => SetValue(CardView.IconImageSourceProperty, value);
+        }
+
+        public Color IconBackgroundColor
+        {
+            get => (Color)GetValue(CardView.IconBackgroundColorProperty);
+            set => SetValue(CardView.IconBackgroundColorProperty, value);
         }
 
         public CardView()
