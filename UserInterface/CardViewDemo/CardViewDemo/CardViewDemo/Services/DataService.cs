d9aaccf07a59ef94ea32b5cc242f2cfb41fe1871
Justin Johnson <justin.d.johnson@microsoft.com>
2019-08-14 14:33:13 -0600

    Fixed for iOS

--- UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Services/DataService.cs
+++ UserInterface/CardViewDemo/CardViewDemo/CardViewDemo/Services/DataService.cs
@@ -10,22 +10,22 @@ namespace CardViewDemo.Services
             new PersonViewModel() {
                 Name = "Slavko Vlasic",
                 Photo = "user.png",
-                Bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elit dolor, convallis non interdum vitae, sagittis at eros. Nullam egestas pulvinar pharetra."
+                Bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elit dolor, convallis non interdum."
             },
             new PersonViewModel() {
                 Name = "Carolina Pena",
                 Photo = "user.png",
-                Bio = "Phasellus eu convallis mi. In tempus augue eu dignissim fermentum. Morbi ut lacus vitae eros lacinia pharetra. Donec fringilla mi vel sapien scelerisque."
+                Bio = "Phasellus eu convallis mi. In tempus augue eu dignissim fermentum. Morbi ut lacus vitae eros lacinia."
             },
             new PersonViewModel() {
                 Name = "Wade Blanks",
                 Photo = "user.png",
-                Bio = "Aliquam sagittis, odio lacinia fermentum dictum, mi erat scelerisque erat, quis aliquet arcu lectus nec ex. Sed interdum sodales arcu laoreet blandit."
+                Bio = "Aliquam sagittis, odio lacinia fermentum dictum, mi erat scelerisque erat, quis aliquet arcu."
             },
             new PersonViewModel() {
                 Name = "Colette Quint",
                 Photo = "user.png",
-                Bio = "In pellentesque odio eget augue elementum lobortis. Sed augue massa, rhoncus eu nisi vitae, egestas lacinia libero. Vivamus finibus metus vel nibh sollicitudin ultrices."
+                Bio = "In pellentesque odio eget augue elementum lobortis. Sed augue massa, rhoncus eu nisi vitae, egestas."
             }
         };
 
