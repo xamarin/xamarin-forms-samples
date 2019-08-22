cb1516ffaa7929740d0288a2e203a3dd0ddf75eb
David Britch <davidbritch@users.noreply.github.com>
2019-08-22 12:15:07 +0100

    Build fixes.

--- Behaviors/AttachedNumericValidationBehavior/WorkingWithBehaviors.Android/MainActivity.cs
+++ Behaviors/AttachedNumericValidationBehavior/WorkingWithBehaviors.Android/MainActivity.cs
@@ -4,22 +4,22 @@ using Xamarin.Forms.Platform.Android;
 
 namespace AttachedNumericValidationBehavior.Droid
 {
-    [Activity(Label = "Behaviors", 
-		Theme = "@style/MainTheme", MainLauncher = true,
-		ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize
-	)]
-	public class MainActivity : FormsAppCompatActivity
+    [Activity(Label = "Behaviors",
+        Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize
+    )]
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
 
             Xamarin.Forms.Forms.Init(this, bundle);
 
-			LoadApplication (new App ());
+            LoadApplication(new App());
         }
     }
 }
