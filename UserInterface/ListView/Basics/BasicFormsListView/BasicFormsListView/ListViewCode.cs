47db82dd298309e0f36715220c971214c3ac7719
Justin Johnson <justin.d.johnson@microsoft.com>
2019-09-06 08:16:00 -0600

    Updated BasicFormsListView sample

--- UserInterface/ListView/Basics/BasicFormsListView/BasicFormsListView/ListViewCode.cs
+++ UserInterface/ListView/Basics/BasicFormsListView/BasicFormsListView/ListViewCode.cs
@@ -8,9 +8,12 @@ namespace BasicFormsListView
 	{
 		public ListViewCode ()
 		{
-			var lstView = new ListView ();
-			lstView.ItemsSource = new List<String> (){ "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
-			Content = lstView;
+            Title = "ListView Code Demo";
+            Padding = 10;
+
+			var listView = new ListView ();
+            listView.ItemsSource = Constants.Items;
+			Content = listView;
 		}
 	}
 }
