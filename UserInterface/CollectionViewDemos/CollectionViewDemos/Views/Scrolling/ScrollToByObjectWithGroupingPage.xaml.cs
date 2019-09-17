47eb81893eb851932896bb0a70c7848992e0835b
David Britch <davidbritch@users.noreply.github.com>
2019-09-17 12:13:47 +0100

    ScrollTo for grouped data.

---
+++ UserInterface/CollectionViewDemos/CollectionViewDemos/Views/Scrolling/ScrollToByObjectWithGroupingPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using CollectionViewDemos.Models;
+using CollectionViewDemos.ViewModels;
+using Xamarin.Forms;
+
+namespace CollectionViewDemos.Views
+{
+    public partial class ScrollToByObjectWithGroupingPage : ContentPage
+    {
+        public ScrollToByObjectWithGroupingPage()
+        {
+            InitializeComponent();
+            BindingContext = new GroupedAnimalsViewModel();
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            GroupedAnimalsViewModel viewModel = BindingContext as GroupedAnimalsViewModel;
+            AnimalGroup group = viewModel.Animals.FirstOrDefault(a => a.Name == "Monkeys");
+            Animal monkey = group.FirstOrDefault(m => m.Name == "Proboscis Monkey");
+            collectionView.ScrollTo(monkey, group, (ScrollToPosition)enumPicker.SelectedItem, animateSwitch.IsToggled);
+        }
+
+        void OnCollectionViewScrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            Debug.WriteLine("HorizontalDelta: " + e.HorizontalDelta);
+            Debug.WriteLine("VerticalDelta: " + e.VerticalDelta);
+            Debug.WriteLine("HorizontalOffset: " + e.HorizontalOffset);
+            Debug.WriteLine("VerticalOffset: " + e.VerticalOffset);
+            Debug.WriteLine("FirstVisibleItemIndex: " + e.FirstVisibleItemIndex);
+            Debug.WriteLine("CenterItemIndex: " + e.CenterItemIndex);
+            Debug.WriteLine("LastVisibleItemIndex: " + e.LastVisibleItemIndex);
+        }
+    }
+}
