cab2217ba8bd824e6d1cec3befdcfa41a7a0750c
David Britch <davidbritch@users.noreply.github.com>
2019-09-19 11:43:41 +0100

    RefreshView sample.

---
+++ UserInterface/RefreshViewDemo/RefreshViewDemo.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace RefreshViewDemo.Droid
+{
+    [Activity(Label = "RefreshViewDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+    }
+}
