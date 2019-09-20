8bcd748c7a940b14587ac23a416894b721108ffd
Justin Johnson <justin.d.johnson@microsoft.com>
2019-09-20 10:44:18 -0600

    Removed unused assets and modernized the launch assets

--- WorkingWithMaps/iOS/AppDelegate.cs
+++ WorkingWithMaps/iOS/AppDelegate.cs
@@ -1,24 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 using Foundation;
 using UIKit;
 
-using Xamarin.Forms;
-
 namespace WorkingWithMaps.iOS
 {
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : 
-	global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate // superclass new in 1.3
+    [Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init ();
 			global::Xamarin.FormsMaps.Init ();
 
-			LoadApplication (new App ());  // method is new in 1.3
+			LoadApplication (new App ());
 
 			return base.FinishedLaunching (app, options);
 		}
