73b61be8db14c8001bb7d1518119860d6efef4b9
Justin Johnson <justin.d.johnson@microsoft.com>
2019-09-23 16:02:02 -0600

    Added PinEvents page and tested on Android

---
+++ WorkingWithMaps/WorkingWithMaps/PinEvents.cs
@@ -0,0 +1,63 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace WorkingWithMaps
+{
+    public class PinEvents : ContentPage
+    {
+        public PinEvents()
+        {
+            Title = "Pin Events Demo";
+
+            var map = new Map
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            map.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    new Position(47.640663, -122.1376177), Distance.FromMiles(1)));
+
+            var pin1 = new Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(47.6368678, -122.137305),
+                Label = "Example Pin 1",
+                Address = "Example custom details..."
+            };
+            pin1.MarkerClicked += OnMarkerClicked;
+            pin1.InfoWindowClicked += OnInfoWindowClicked;
+            map.Pins.Add(pin1);
+
+            var pin2 = new Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(47.6406414, -122.1344833),
+                Label = "Example Pin 2",
+                Address = "Example custom details..."
+            };
+            pin2.MarkerClicked += OnMarkerClicked;
+            pin2.InfoWindowClicked += OnInfoWindowClicked;
+            map.Pins.Add(pin2);
+
+            Content = new StackLayout
+            {
+                Children = {
+                    map
+                }
+            };
+        }
+
+        private void OnInfoWindowClicked(object sender, PinClickedEventArgs e)
+        {
+            var pinName = ((Pin)sender).Label;
+            DisplayAlert("Info Window Clicked", $"The info window was clicked for {pinName}.", "Ok");
+        }
+
+        private void OnMarkerClicked(object sender, PinClickedEventArgs e)
+        {
+            var pinName = ((Pin)sender).Label;
+            DisplayAlert("Pin Clicked", $"{pinName} was clicked.", "Ok");
+        }
+    }
+}
