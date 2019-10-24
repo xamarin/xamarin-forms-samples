b189dd0eb55008d0107c0d3f0aba368ef2b6ce32
Justin Johnson <justin.d.johnson@microsoft.com>
2019-10-24 15:29:36 -0600

    Effects mostly working. Some bugs with BoxView

---
+++ Effects/RoundEffectDemo/RoundEffectDemo/RoundEffectDemo/RoundEffect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RoundEffectDemo
+{
+    public class RoundEffect : RoutingEffect
+    {
+        public RoundEffect() : base($"Xamarin.{nameof(RoundEffect)}")
+        {
+
+        }
+
+    }
+}
