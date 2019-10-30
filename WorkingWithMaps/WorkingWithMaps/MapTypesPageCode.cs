27d9b3abc0997c9803898ab43ac5d11979aee868
David Britch <davidbritch@users.noreply.github.com>
2019-10-30 14:17:53 +0000

    Sample edits.

--- WorkingWithMaps/WorkingWithMaps/MapTypesPageCode.cs
+++ WorkingWithMaps/WorkingWithMaps/MapTypesPageCode.cs
@@ -4,11 +4,11 @@ using Xamarin.Forms.Maps;
 
 namespace WorkingWithMaps
 {
-	public class MapTypesPageCode : ContentPage
-	{
-		Map map;
-		public MapTypesPageCode()
-		{
+    public class MapTypesPageCode : ContentPage
+    {
+        Map map;
+        public MapTypesPageCode()
+        {
             Title = "Map types demo";
 
             map = new Map();
@@ -16,20 +16,20 @@ namespace WorkingWithMaps
             // Create a Slider for zoom
             var slider = new Slider(1, 18, 12)
             {
-                Margin = new Thickness(20,0,20,0)
+                Margin = new Thickness(20, 0, 20, 0)
             };
-			slider.ValueChanged += (sender, e) =>
+            slider.ValueChanged += (sender, e) =>
             {
-				double zoomLevel = e.NewValue; // between 1 and 18
-				double latlongDegrees = 360 / (Math.Pow(2, zoomLevel));
-				if (map.VisibleRegion != null)
+                double zoomLevel = e.NewValue; // between 1 and 18
+                double latlongDegrees = 360 / (Math.Pow(2, zoomLevel));
+                if (map.VisibleRegion != null)
                 {
                     map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongDegrees, latlongDegrees));
                 }
             };
 
             // Create buttons
-		    Button streetButton = new Button { Text = "Street" };
+            Button streetButton = new Button { Text = "Street" };
             Button satelliteButton = new Button { Text = "Satellite" };
             Button hybridButton = new Button { Text = "Hybrid" };
 
@@ -37,21 +37,21 @@ namespace WorkingWithMaps
             satelliteButton.Clicked += OnButtonClicked;
             hybridButton.Clicked += OnButtonClicked;
 
-			StackLayout buttons = new StackLayout
+            StackLayout buttons = new StackLayout
             {
                 Spacing = 30,
-				HorizontalOptions = LayoutOptions.Center,
-				Orientation = StackOrientation.Horizontal, 
-				Children = {streetButton, satelliteButton, hybridButton }
-			};
+                HorizontalOptions = LayoutOptions.Center,
+                Orientation = StackOrientation.Horizontal,
+                Children = { streetButton, satelliteButton, hybridButton }
+            };
 
             // Build the page
             StackLayout stackLayout = new StackLayout();
-			stackLayout.Children.Add(map);
-			stackLayout.Children.Add (slider);
-			stackLayout.Children.Add (buttons);
-			Content = stackLayout;
-		}
+            stackLayout.Children.Add(map);
+            stackLayout.Children.Add(slider);
+            stackLayout.Children.Add(buttons);
+            Content = stackLayout;
+        }
 
         void OnButtonClicked(object sender, EventArgs e)
         {
