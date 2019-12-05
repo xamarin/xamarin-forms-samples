1b83dc788676a067ceb8d7d38d74fba9f1ea047a
Justin Johnson <justin.d.johnson@gmail.com>
2019-12-05 11:22:15 -0700

    Removed resume at id concept

--- Todo/Todo/App.cs
+++ Todo/Todo/App.cs
@@ -36,8 +36,6 @@ namespace Todo
 			}
 		}
 
-		public int ResumeAtTodoId { get; set; }
-
 		protected override void OnStart()
 		{
 
--- Todo/Todo/Views/TodoListPage.xaml.cs
+++ Todo/Todo/Views/TodoListPage.xaml.cs
@@ -15,8 +15,6 @@ namespace Todo
 		{
 			base.OnAppearing();
 
-			// Reset the 'resume' id, since we just want to re-start here
-			((App)App.Current).ResumeAtTodoId = -1;
 			listView.ItemsSource = await App.Database.GetItemsAsync();
 		}
 
@@ -30,8 +28,6 @@ namespace Todo
 
 		async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
 		{
-            //((App)App.Current).ResumeAtTodoId = (e.SelectedItem as TodoItem).ID;
-            //Debug.WriteLine("setting ResumeAtTodoId = " + (e.SelectedItem as TodoItem).ID);
             if (e.SelectedItem != null)
             {
                 await Navigation.PushAsync(new TodoItemPage
