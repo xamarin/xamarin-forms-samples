f8114d75c9f7500b384baab8ed812e41a8ca8554
Justin Johnson <justin.d.johnson@gmail.com>
2019-12-05 13:26:03 -0700

    Moved common values into constants file

---
+++ Todo/Todo/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Todo
+{
+    public static class Constants
+    {
+        public const string DatabaseFilename = "TodoSQLite.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            // open the database in read/write mode
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            // create the database if it doesn't exist
+            SQLite.SQLiteOpenFlags.Create |
+            // enable multi-threaded database access
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
+    }
+}
