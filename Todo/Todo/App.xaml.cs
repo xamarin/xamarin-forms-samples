ba0238ee063aa46072b36cfcf0b4afa5ae2b2864
Justin Johnson <justin.d.johnson@gmail.com>
2019-12-09 11:20:30 -0700

    Moved app resources into xaml

---
+++ Todo/Todo/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Todo
+{
+    public partial class App : Application
+    {
+        static TodoItemDatabase database;
+
+        public App()
+        {
+            InitializeComponent();
+
+            var nav = new NavigationPage(new TodoListPage());
+            nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+            nav.BarTextColor = Color.White;
+
+            MainPage = nav;
+        }
+
+        public static TodoItemDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new TodoItemDatabase();
+                }
+                return database;
+            }
+        }
+
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnSleep()
+        {
+
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+}
+
