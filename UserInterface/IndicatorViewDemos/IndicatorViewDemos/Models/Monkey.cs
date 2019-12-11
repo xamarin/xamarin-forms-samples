2db5881b43ca4cfe2cd212e8663322123f9cece4
David Britch <davidbritch@users.noreply.github.com>
2019-12-11 16:31:37 +0000

    New samples.

---
+++ UserInterface/IndicatorViewDemos/IndicatorViewDemos/Models/Monkey.cs
@@ -0,0 +1,10 @@
+﻿namespace IndicatorViewDemos.Models
+{
+    public class Monkey
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ UserInterface/SwipeViewDemos/SwipeViewDemos/Views/SwipeViewEventsPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SwipeViewDemos
+{
+    public partial class SwipeViewEventsPage : ContentPage
+    {
+        public SwipeViewEventsPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSwipeStarted(object sender, SwipeStartedEventArgs e)
+        {
+            Console.WriteLine($"\tSwipeStarted: Direction - {e.SwipeDirection}");
+        }
+
+        void OnSwipeChanging(object sender, SwipeChangingEventArgs e)
+        {
+            Console.WriteLine($"\tSwipeChanging: Direction - {e.SwipeDirection}, Offset - {e.Offset}");
+        }
+
+        void OnSwipeEnded(object sender, SwipeEndedEventArgs e)
+        {
+            Console.WriteLine($"\tSwipEnded: Direction - {e.SwipeDirection}");
+        }
+        
+        void OnCloseRequested(object sender, EventArgs e)
+        {
+            Console.WriteLine("\tCloseRequested.");
+        }
+
+        void OnCloseButtonClicked(object sender, EventArgs e)
+        {
+            swipeView.Close();   
+        }
+
+        async void OnFavoriteSwipeItemInvoked(object sender, EventArgs e)
+        {
+            await DisplayAlert("SwipeView", "Favorite invoked.", "OK");
+        }
+
+        async void OnDeleteSwipeItemInvoked(object sender, EventArgs e)
+        {
+            await DisplayAlert("SwipeView", "Delete invoked.", "OK");
+        }
+    }
+}
