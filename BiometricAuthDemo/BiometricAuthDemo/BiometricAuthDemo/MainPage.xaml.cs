bb8e4b987c022815c31417d4cae54ba8c8d777b3
Justin Johnson <justin.d.johnson@gmail.com>
2019-12-12 15:18:11 -0700

    Base project commit

---
+++ BiometricAuthDemo/BiometricAuthDemo/BiometricAuthDemo/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BiometricAuthDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        IBiometricAuthProvider authProvider;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            authProvider = DependencyService.Get<IBiometricAuthProvider>();
+        }
+
+        void OnAuthButtonClicked(object sender, EventArgs e)
+        {
+            if(authProvider.IsBiometricAuthEnabled)
+            {
+                authProvider.RequestAuthentication((result) =>
+                {
+                    UpdateResultText(result.Success, result.Message);
+                });
+            }
+            else
+            {
+                UpdateResultText(false, "Biometric authentication is not enabled on this device.");
+            }
+        }
+
+        void UpdateResultText(bool success, string message)
+        {
+            authResultMessage.TextColor = success ? Color.Green : Color.Red;
+            authResultMessage.Text = message;
+        }
+    }
+}
