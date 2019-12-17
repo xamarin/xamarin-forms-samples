e205a280687f68d0c5adb5b0d0587a186f8682d1
David Britch <davidbritch@users.noreply.github.com>
2019-12-17 16:10:32 +0000

    Updated FadeTriggerAction.

--- WorkingWithTriggers/WorkingWithTriggers/TriggerActions/FadeTriggerAction.cs
+++ WorkingWithTriggers/WorkingWithTriggers/TriggerActions/FadeTriggerAction.cs
@@ -1,28 +1,21 @@
-﻿using System;
-
-using Xamarin.Forms;
-using System.Diagnostics;
+﻿using Xamarin.Forms;
 
 namespace WorkingWithTriggers
 {
-	public class FadeTriggerAction : TriggerAction<VisualElement>
-	{
-		public FadeTriggerAction()
-		{
-		}
-		
-		public int StartsFrom { set; get; }
+    public class FadeTriggerAction : TriggerAction<VisualElement>
+    {
+        public int StartsFrom { set; get; }
 
-		protected override void Invoke (VisualElement visual)
-		{
-			
-			visual.Animate("0", new Animation( (d)=>{
-				var val = StartsFrom==1 ? d : 1-d;
-				// so i was aiming for a different color, but then i liked the pink :)
-				visual.BackgroundColor = Color.FromRgb(1, val, 1);
-			}),
-			length:1000, // milliseconds
-			easing: Easing.Linear);
-		}
-	}
+        protected override void Invoke(VisualElement sender)
+        {
+            sender.Animate("FadeTriggerAction", new Animation((d) =>
+            {
+                var val = StartsFrom == 1 ? d : 1 - d;
+                // so i was aiming for a different color, but then i liked the pink :)
+                sender.BackgroundColor = Color.FromRgb(1, val, 1);
+            }),
+            length: 1000, // milliseconds
+            easing: Easing.Linear);
+        }
+    }
 }
