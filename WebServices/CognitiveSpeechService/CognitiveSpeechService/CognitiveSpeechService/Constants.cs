576df0ad27a45dee128de061df2b25fd63024b94
Justin Johnson <justin.d.johnson@gmail.com>
2020-01-14 10:10:40 -0700

    Base checkin with placeholder constants

---
+++ WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CognitiveSpeechService
+{
+    public static class Constants
+    {
+        // API key can be a shared, multi-resource key or an individual service key
+        // and can be found and regenerated in the Azure portal
+        public string CognitiveServicesApiKey = "< Enter API Key Here >";
+
+        // Endpoint is based on your configured region and should be similar to: https://westus.api.cognitive.microsoft.com/
+        public string CognitiveServicesEndpoint = "< Enter Endpoint Here >";
+    }
+}
