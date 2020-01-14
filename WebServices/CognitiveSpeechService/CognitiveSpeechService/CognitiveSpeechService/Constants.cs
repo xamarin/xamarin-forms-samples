702ed003da075d8b35de167ef850c7c29a796282
Justin Johnson <justin.d.johnson@gmail.com>
2020-01-14 10:10:58 -0700

    Forgot static

--- WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService/Constants.cs
+++ WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService/Constants.cs
@@ -8,9 +8,9 @@ namespace CognitiveSpeechService
     {
         // API key can be a shared, multi-resource key or an individual service key
         // and can be found and regenerated in the Azure portal
-        public string CognitiveServicesApiKey = "< Enter API Key Here >";
+        public static string CognitiveServicesApiKey = "< Enter API Key Here >";
 
         // Endpoint is based on your configured region and should be similar to: https://westus.api.cognitive.microsoft.com/
-        public string CognitiveServicesEndpoint = "< Enter Endpoint Here >";
+        public static string CognitiveServicesEndpoint = "< Enter Endpoint Here >";
     }
 }
