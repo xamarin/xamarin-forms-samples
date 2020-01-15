a13dfdec3e4fc56bd2f8d6a3dda199393df1914d
Justin Johnson <justin.d.johnson@gmail.com>
2020-01-15 08:58:39 -0700

    Reworked to be cleaner UX

--- WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService/App.xaml.cs
+++ WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService/App.xaml.cs
@@ -10,7 +10,7 @@ namespace CognitiveSpeechService
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService/MainPage.xaml.cs
+++ WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService/MainPage.xaml.cs
@@ -13,52 +13,116 @@ namespace CognitiveSpeechService
     public partial class MainPage : ContentPage
     {
         SpeechRecognizer recognizer;
-        IMicrophoneService microphoneService;
+        IMicrophoneService micService;
+        bool isTranscribing = false;
 
         public MainPage()
         {
             InitializeComponent();
 
-            microphoneService = DependencyService.Resolve<IMicrophoneService>();
+            micService = DependencyService.Resolve<IMicrophoneService>();
         }
 
-        async void EnableMicClicked(object sender, EventArgs e)
+        async void TranscribeClicked(object sender, EventArgs e)
         {
-            bool isMicEnabled = await microphoneService.GetPermissionAsync();
-            if(!isMicEnabled)
+            bool isMicEnabled = await micService.GetPermissionAsync();
+
+            // Speech recognizer 
+            if(recognizer == null)
             {
-                UpdateTranscription("Please grant access to the microphone!", true);
+                var config = SpeechConfig.FromSubscription(Constants.CognitiveServicesApiKey, Constants.CognitiveServicesRegion);
+                recognizer = new SpeechRecognizer(config);
+                recognizer.Recognized += (obj, args) =>
+                {
+                    UpdateTranscription(args.Result.Text);
+                };
             }
-        }
 
-        void StartTranscribingClicked(object sender, EventArgs e)
-        {
-            var config = SpeechConfig.FromSubscription(Constants.CognitiveServicesApiKey, Constants.CognitiveServicesRegion);
-
-            UpdateTranscription("", true);
-
-            recognizer = new SpeechRecognizer(config);
-            recognizer.StartContinuousRecognitionAsync();
-            recognizer.Recognized += (obj, args) =>
+            // EARLY OUT: make sure mic is accessible
+            if (!isMicEnabled)
             {
-                UpdateTranscription(args.Result.Text);
-            };
+                UpdateTranscription("Please grant access to the microphone!");
+                return;
+            }
+
+            // if already transcribing, stop speech recognizer
+            if(isTranscribing)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    transcribingIndicator.IsRunning = false;
+                });
+
+                try
+                {
+                    await recognizer.StopContinuousRecognitionAsync();
+                }
+                catch(Exception ex)
+                {
+                    UpdateTranscription(ex.Message);
+                }
+                
+                isTranscribing = false;
+            }
+
+            // if not transcribing, clear existing text and start speech recognizer
+            else
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    transcribingIndicator.IsRunning = true;
+                    InsertDateTimeRecord();
+                });
+
+                try
+                {
+                    await recognizer.StartContinuousRecognitionAsync();
+                }
+                catch(Exception ex)
+                {
+                    UpdateTranscription(ex.Message);
+                }
+                
+                isTranscribing = true;
+            }
+
+            UpdateButton();
         }
 
-        void UpdateTranscription(string newText, bool clearText = false)
+        void UpdateTranscription(string newText)
         {
-            // Force on main thread since this is likely called
-            // from a background event
             Device.BeginInvokeOnMainThread(() =>
             {
-                if(clearText)
+                if(!string.IsNullOrWhiteSpace(newText))
                 {
-                    transcribedText.Text = "";
+                    transcribedText.Text += $"{newText}\n";
+                    scroll.ScrollToAsync(0, scroll.Height, true);
                 }
-
-                transcribedText.Text += newText;
             });
             
+        }
+
+        void InsertDateTimeRecord()
+        {
+            var msg = $"=================\n{DateTime.Now.ToString()}\n=================";
+            UpdateTranscription(msg);
+        }
+
+        void UpdateButton()
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if(isTranscribing)
+                {
+                    transcribeButton.Text = "Stop";
+                    transcribeButton.BackgroundColor = Color.Red;
+                }
+                else
+                {
+                    transcribeButton.Text = "Transcribe";
+                    transcribeButton.BackgroundColor = Color.Green;
+                }
+            });
         }
     }
 }
