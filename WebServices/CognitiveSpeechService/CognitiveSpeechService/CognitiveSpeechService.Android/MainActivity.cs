fe41c7f7cbc64acf74f3479bb18c9ebedbfde806
Justin Johnson <justin.d.johnson@gmail.com>
2020-01-15 08:58:59 -0700

    Android working on device but not emulator

--- WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService.Android/MainActivity.cs
+++ WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService.Android/MainActivity.cs
@@ -6,14 +6,23 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using CognitiveSpeechService.Services;
+using Xamarin.Forms;
+using Android;
+using CognitiveSpeechService.Droid.Services;
 
 namespace CognitiveSpeechService.Droid
 {
     [Activity(Label = "CognitiveSpeechService", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        IMicrophoneService micService;
+        internal static MainActivity Instance { get; private set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Instance = this;
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -22,12 +31,28 @@ namespace CognitiveSpeechService.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            micService = DependencyService.Resolve<IMicrophoneService>();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            switch(requestCode)
+            {
+                case AndroidMicService.RecordAudioPermissionCode:
+                    if(grantResults[0] == Permission.Granted)
+                    {
+                        micService.OnRequestPermissionResult(true);
+                    }
+                    else
+                    {
+                        micService.OnRequestPermissionResult(false);
+                    }
+                    break;
+            }
         }
     }
 }
---
+++ WebServices/CognitiveSpeechService/CognitiveSpeechService/CognitiveSpeechService.Android/Services/AndroidMicService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using CognitiveSpeechService.Droid.Services;
+using CognitiveSpeechService.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AndroidMicService))]
+namespace CognitiveSpeechService.Droid.Services
+{
+    public class AndroidMicService : IMicrophoneService
+    {
+        public const int RecordAudioPermissionCode = 1;
+        private TaskCompletionSource<bool> tcsPermissions;
+        string[] permissions = new string[] { Manifest.Permission.RecordAudio };
+
+        public Task<bool> GetPermissionAsync()
+        {
+            tcsPermissions = new TaskCompletionSource<bool>();
+
+            if((int)Build.VERSION.SdkInt < 23)
+            {
+                tcsPermissions.TrySetResult(true);
+            }
+            else
+            {
+                var currentActivity = MainActivity.Instance;
+                if(ActivityCompat.CheckSelfPermission(currentActivity, Manifest.Permission.RecordAudio) != (int)Permission.Granted)
+                {
+                    RequestMicPermissions();
+                }
+                else
+                {
+                    tcsPermissions.TrySetResult(true);
+                }
+                
+            }
+
+            return tcsPermissions.Task;
+        }
+
+        public void OnRequestPermissionResult(bool isGranted)
+        {
+            tcsPermissions.TrySetResult(isGranted);
+        }
+
+        void RequestMicPermissions()
+        {
+            if(ActivityCompat.ShouldShowRequestPermissionRationale(MainActivity.Instance, Manifest.Permission.RecordAudio))
+            {
+                Snackbar.Make(MainActivity.Instance.FindViewById(Android.Resource.Id.Content),
+                        "Microphone permissions are required for speech transcription!",
+                        Snackbar.LengthIndefinite)
+                        .SetAction("Ok", v =>
+                        {
+                            ((Activity)MainActivity.Instance).RequestPermissions(permissions, RecordAudioPermissionCode);
+                        })
+                        .Show();
+            }
+            else
+            {
+                ActivityCompat.RequestPermissions((Activity)MainActivity.Instance, permissions, RecordAudioPermissionCode);
+            }
+        }
+    }
+}
