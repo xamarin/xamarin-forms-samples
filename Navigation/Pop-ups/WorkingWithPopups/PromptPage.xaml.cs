02a0c8691db3063f1afbeb3e2be570d8d0297e1b
David Britch <davidbritch@users.noreply.github.com>
2020-01-17 10:57:46 +0000

    Now includes predefining a response.

--- Navigation/Pop-ups/WorkingWithPopups/PromptPage.xaml.cs
+++ Navigation/Pop-ups/WorkingWithPopups/PromptPage.xaml.cs
@@ -12,7 +12,7 @@ namespace WorkingWithPopups
 
         async void OnQuestion1ButtonClicked(object sender, EventArgs e)
         {
-            string result = await DisplayPromptAsync("Question 1", "What's your name?");
+            string result = await DisplayPromptAsync("Question 1", "What's your name?", initialValue:string.Empty);
             if (!string.IsNullOrWhiteSpace(result))
             {
                 question1ResultLabel.Text = $"Hello {result}.";
@@ -21,7 +21,7 @@ namespace WorkingWithPopups
 
         async void OnQuestion2ButtonClicked(object sender, EventArgs e)
         {
-            string result = await DisplayPromptAsync("Question 2", "What's 5 + 5?", maxLength: 2, keyboard: Keyboard.Numeric);
+            string result = await DisplayPromptAsync("Question 2", "What's 5 + 5?", initialValue:"10", maxLength: 2, keyboard: Keyboard.Numeric);
             if (!string.IsNullOrWhiteSpace(result))
             {
                 int number = Convert.ToInt32(result);
