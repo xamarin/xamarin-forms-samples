8dfad79a8ff3f33fdba00f6b23404ccf988f385a
David Britch <davidbritch@users.noreply.github.com>
2020-01-20 16:46:52 +0000

    Added TargetName support.

--- UserInterface/VsmDemos/VsmDemos/VsmDemos/VsmOnViewPage.xaml.cs
+++ UserInterface/VsmDemos/VsmDemos/VsmDemos/VsmOnViewPage.xaml.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace VsmDemos
 {
-//	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class VsmOnViewPage : ContentPage
 	{
 		public VsmOnViewPage ()
--- UserInterface/VsmDemos/VsmDemos/VsmDemos/VsmValidationPage.xaml.cs
+++ UserInterface/VsmDemos/VsmDemos/VsmDemos/VsmValidationPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Xamarin.Forms;
 
 namespace VsmDemos
