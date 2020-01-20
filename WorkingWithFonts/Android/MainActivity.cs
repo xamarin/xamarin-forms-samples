17f73586bcc83a666709514ccc350fefd4ad8965
David Britch <davidbritch@users.noreply.github.com>
2020-01-20 12:33:42 +0000

    Custom font embedded in shared project.

--- WorkingWithFonts/Android/MainActivity.cs
+++ WorkingWithFonts/Android/MainActivity.cs
@@ -1,31 +1,19 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
-
-using Xamarin.Forms.Platform.Android;
-using Android.Graphics;
 using Android.Content.PM;
-
 
 namespace WorkingWithFonts.Android
 {
 	[Activity(Label = "Fonts", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
 		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity :
-	global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity // superclass new in 1.3
+	global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
-
 			global::Xamarin.Forms.Forms.Init(this, bundle);
-
-			LoadApplication(new App()); // method is new in 1.3
+			LoadApplication(new App());
 		}
 	}
 
--- WorkingWithFonts/WorkingWithFonts/FontPageXaml.xaml.cs
+++ WorkingWithFonts/WorkingWithFonts/FontPageXaml.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace WorkingWithFonts
 {	
-	/// <summary>
-	/// Using fonts in Xamarin.Forms 1.3 is slightly different to earlier versions.
-	/// The 'Font' property has been replaced with three separate properties:
-	/// - FontFamily
-	/// - FontSize
-	/// - FontAttributes
-	/// Use these three properties directly and avoid using the Font class.
-	/// </summary>
 	public partial class FontPageXaml : ContentPage
 	{	
 		public FontPageXaml ()
