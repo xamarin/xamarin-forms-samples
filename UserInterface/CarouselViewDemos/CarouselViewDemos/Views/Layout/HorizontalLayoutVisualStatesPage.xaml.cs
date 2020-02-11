3bd03e341c4472f5f88fb965271adb176b905f15
David Britch <davidbritch@users.noreply.github.com>
2020-02-11 13:17:43 +0000

    Added visual states example.

---
+++ UserInterface/CarouselViewDemos/CarouselViewDemos/Views/Layout/HorizontalLayoutVisualStatesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CarouselViewDemos.Views
+{
+    public partial class HorizontalLayoutVisualStatesPage : ContentPage
+    {
+        public HorizontalLayoutVisualStatesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
