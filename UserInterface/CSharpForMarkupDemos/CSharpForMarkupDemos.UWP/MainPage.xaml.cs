87e1c882358dc8f35b7b893d5632cf9514f97738
David Britch <davidbritch@users.noreply.github.com>
2020-03-17 15:25:57 +0000

    UWP support added.

---
+++ UserInterface/CSharpForMarkupDemos/CSharpForMarkupDemos.UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CSharpForMarkupDemos.Helpers;
+
+namespace CSharpForMarkupDemos.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            TaskHelper.InitializeFromUIThread();
+            this.InitializeComponent();
+            this.LoadApplication(new CSharpForMarkupDemos.App());
+        }
+    }
+}
