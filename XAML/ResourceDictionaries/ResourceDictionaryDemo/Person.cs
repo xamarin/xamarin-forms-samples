60beea859e340ae0baa2117462f9a63bf9784028
David Britch <davidbritch@users.noreply.github.com>
2020-03-31 14:57:36 +0100

    Restyled app.

--- XAML/ResourceDictionaries/ResourceDictionaryDemo/Person.cs
+++ XAML/ResourceDictionaries/ResourceDictionaryDemo/Person.cs
@@ -3,9 +3,7 @@
 	public class Person
 	{
 		public string Name { get; private set; }
-
 		public int Age { get; private set; }
-
 		public string Location { get; private set; }
 
 		public Person (string name, int age, string location)
