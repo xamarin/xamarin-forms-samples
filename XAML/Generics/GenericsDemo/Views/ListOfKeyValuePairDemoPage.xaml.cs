070931e1456f8af6726e743ea57f332f9ea1bac0
David Britch <davidbritch@users.noreply.github.com>
2020-04-28 12:09:39 +0100

    New sample.

---
+++ XAML/Generics/GenericsDemo/Views/ListOfKeyValuePairDemoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GenericsDemo.Views
+{
+    public partial class ListOfKeyValuePairDemoPage : ContentPage
+    {
+        public ListOfKeyValuePairDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
