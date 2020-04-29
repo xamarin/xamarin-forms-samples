bfe476d37686b3bda645f537b8806715944b11b7
David Britch <davidbritch@users.noreply.github.com>
2020-04-29 16:23:53 +0100

    Formatted document.

--- WebServices/TodoREST/TodoREST/Constants.cs
+++ WebServices/TodoREST/TodoREST/Constants.cs
@@ -2,9 +2,9 @@
 
 namespace TodoREST
 {
-	public static class Constants
-	{
-		// URL of REST service
+    public static class Constants
+    {
+        // URL of REST service
         public static string RestUrl = Device.RuntimePlatform == Device.Android ? "https://10.0.2.2:5001/api/todoitems/{0}" : "https://localhost:5001/api/todoitems/{0}";
     }
 }
