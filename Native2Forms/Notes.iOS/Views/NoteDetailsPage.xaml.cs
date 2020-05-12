100cdbe53af98f2b63e7042922f2b9feba4c551c
Lukasz Lawicki <contact@lukaszlawicki.pl>
2020-05-12 18:39:00 +0200

    Added sample how to show forms page within UIViewController

---
+++ Native2Forms/Notes.iOS/Views/NoteDetailsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class NoteDetailsPage : ContentPage
+    {
+        public NoteDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ViewHolder.TranslateTo(0, 0, 600, Easing.BounceIn);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            
+            ViewHolder.TranslateTo(0, -2000, 300, Easing.BounceOut);
+        }
+    }
+}
