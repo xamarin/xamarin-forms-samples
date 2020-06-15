297e074fd9ec6944525c485eccc9cecbaa170048
David Britch <davidbritch@users.noreply.github.com>
2020-06-15 11:32:31 +0100

    Page rename for 4.6-SR4.

---
+++ XAML/MarkupExtensions/MarkupExtensions/MarkupExtensions/AppThemeBindingDemoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MarkupExtensions
+{
+    public partial class AppThemeBindingDemoPage : ContentPage
+    {
+        public AppThemeBindingDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
