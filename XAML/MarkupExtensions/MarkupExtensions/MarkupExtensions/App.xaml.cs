6cd9cddfae498ccf871a3f29e90ab2baf110e7a4
David Britch <davidbritch@users.noreply.github.com>
2020-06-19 14:30:25 +0100

    Re-enabled XAMLC.

--- XAML/MarkupExtensions/MarkupExtensions/MarkupExtensions/App.xaml.cs
+++ XAML/MarkupExtensions/MarkupExtensions/MarkupExtensions/App.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-//[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace MarkupExtensions
 {
     public partial class App : Application
