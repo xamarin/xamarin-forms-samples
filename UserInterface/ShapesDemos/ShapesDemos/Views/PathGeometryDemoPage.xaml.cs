6922c28d08eb662fb80d2e0f971f5533aebdb312
David Britch <davidbritch@users.noreply.github.com>
2020-06-23 17:21:21 +0100

    Path geometry demos.

---
+++ UserInterface/ShapesDemos/ShapesDemos/Views/PathGeometryDemoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShapesDemos
+{
+    public partial class PathGeometryDemoPage : ContentPage
+    {
+        public PathGeometryDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
