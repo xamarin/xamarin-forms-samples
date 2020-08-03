1683700f0a56a335be74d7cdae63e9d3e132c837
David Britch <davidbritch@users.noreply.github.com>
2020-08-03 16:53:32 +0100

    New sample.

---
+++ WorkingWithGestures/DragAndDropGesture/DragAndDropGesture/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace DragAndDropGesture
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new string[] { "DragAndDrop_Experimental", "Shapes_Experimental" });
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ WorkingWithGestures/DragAndDropGesture/DragAndDropGesture/ImageDragDemoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace DragAndDropGesture
+{
+    public partial class ImageDragDemoPage : ContentPage
+    {
+        public ImageDragDemoPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCorrectDrop(object sender, DropEventArgs e)
+        {
+            await DisplayAlert("Correct", "Congratulations!", "OK");
+        }
+
+        void OnIncorrectDragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.None;
+        }
+    }
+}
