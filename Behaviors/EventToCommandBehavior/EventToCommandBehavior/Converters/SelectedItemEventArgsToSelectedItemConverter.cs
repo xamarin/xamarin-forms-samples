817221c0fa7ba8a4f874b842d352217885d6ae5d
David Britch <davidbritch@users.noreply.github.com>
2020-08-24 14:55:24 +0100

    Code layout fixed.

--- Behaviors/EventToCommandBehavior/EventToCommandBehavior/Converters/SelectedItemEventArgsToSelectedItemConverter.cs
+++ Behaviors/EventToCommandBehavior/EventToCommandBehavior/Converters/SelectedItemEventArgsToSelectedItemConverter.cs
@@ -4,18 +4,18 @@ using Xamarin.Forms;
 
 namespace EventToCommandBehavior
 {
-	public class SelectedItemEventArgsToSelectedItemConverter : IValueConverter
-	{
-		public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			var eventArgs = value as SelectedItemChangedEventArgs;
-			return eventArgs.SelectedItem;
-		}
+    public class SelectedItemEventArgsToSelectedItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var eventArgs = value as SelectedItemChangedEventArgs;
+            return eventArgs.SelectedItem;
+        }
 
-		public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			throw new NotImplementedException ();
-		}
-	}
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
 
--- Behaviors/EventToCommandBehavior/EventToCommandBehavior/HomePageViewModel.cs
+++ Behaviors/EventToCommandBehavior/EventToCommandBehavior/HomePageViewModel.cs
@@ -5,43 +5,43 @@ using Xamarin.Forms;
 
 namespace EventToCommandBehavior
 {
-	public class HomePageViewModel : INotifyPropertyChanged
-	{
-		public ObservableCollection<Person> People { get; private set; }
+    public class HomePageViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Person> People { get; private set; }
 
-		public ICommand OutputAgeCommand { get; private set; }
+        public ICommand OutputAgeCommand { get; private set; }
 
-		public string SelectedItemText { get; private set; }
+        public string SelectedItemText { get; private set; }
 
-		public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-		public HomePageViewModel ()
-		{
-			People = new ObservableCollection<Person> 
+        public HomePageViewModel()
+        {
+            People = new ObservableCollection<Person>
             {
-				new Person ("Steve", 21),
-				new Person ("John", 37),
-				new Person ("Tom", 42),
-				new Person ("Lucas", 29),
-				new Person ("Tariq", 39),
-				new Person ("Jane", 30)
-			};
-			OutputAgeCommand = new Command<Person> (OutputAge);
-		}
+                new Person ("Steve", 21),
+                new Person ("John", 37),
+                new Person ("Tom", 42),
+                new Person ("Lucas", 29),
+                new Person ("Tariq", 39),
+                new Person ("Jane", 30)
+            };
+            OutputAgeCommand = new Command<Person>(OutputAge);
+        }
 
-		void OutputAge (Person person)
-		{
-			SelectedItemText = string.Format ("{0} is {1} years old.", person.Name, person.Age);
-			OnPropertyChanged ("SelectedItemText");
-		}
+        void OutputAge(Person person)
+        {
+            SelectedItemText = string.Format("{0} is {1} years old.", person.Name, person.Age);
+            OnPropertyChanged("SelectedItemText");
+        }
 
-		protected virtual void OnPropertyChanged (string propertyName)
-		{
-			var changed = PropertyChanged;
-			if (changed != null)
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
             {
-				PropertyChanged (this, new PropertyChangedEventArgs (propertyName));
-			}
-		}
-	}
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
 }
