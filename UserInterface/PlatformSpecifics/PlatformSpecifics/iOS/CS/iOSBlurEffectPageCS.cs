2997402fbecc4219f8751f12812a13b3259499f4
David Britch <davidbritch@users.noreply.github.com>
2020-09-28 15:00:06 +0100

    Blur applied on the specified VisualElement.

--- UserInterface/PlatformSpecifics/PlatformSpecifics/iOS/CS/iOSBlurEffectPageCS.cs
+++ UserInterface/PlatformSpecifics/PlatformSpecifics/iOS/CS/iOSBlurEffectPageCS.cs
@@ -4,35 +4,32 @@ using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace PlatformSpecifics
 {
-	public class iOSBlurEffectPageCS : ContentPage
-	{
-		public iOSBlurEffectPageCS()
-		{
-			var boxView = new BoxView { HeightRequest = 300, WidthRequest = 300 };
-			var noBlurButton = new Button { Text = "No Blur" };
-			noBlurButton.Clicked += (sender, e) => boxView.On<iOS>().UseBlurEffect(BlurEffectStyle.None);
-			var extraLightBlurButton = new Button { Text = "Extra Light Blur" };
-			extraLightBlurButton.Clicked += (sender, e) => boxView.On<iOS>().UseBlurEffect(BlurEffectStyle.ExtraLight);
-			var lightBlurButton = new Button { Text = "Light Blur" };
-			lightBlurButton.Clicked += (sender, e) => boxView.On<iOS>().UseBlurEffect(BlurEffectStyle.Light);
-			var darkBlurButton = new Button { Text = "Dark Blur" };
-			darkBlurButton.Clicked += (sender, e) => boxView.On<iOS>().UseBlurEffect(BlurEffectStyle.Dark);
+    public class iOSBlurEffectPageCS : ContentPage
+    {
+        public iOSBlurEffectPageCS()
+        {
+            Image image = new Image { Source = "monkeyface.png" };
+            image.On<iOS>().UseBlurEffect(BlurEffectStyle.ExtraLight);
 
-			Title = "Blur Effect";
-			Content = new StackLayout
-			{
-				Margin = new Thickness(20),
-				Children = {
-					new AbsoluteLayout {
-						HorizontalOptions = LayoutOptions.Center,
-						Children = {
-							new Image { Source = "monkeyface.png" },
-							boxView
-						}
-					},
-					noBlurButton, extraLightBlurButton, lightBlurButton, darkBlurButton
-				}
-			};
-		}
-	}
+            var noBlurButton = new Button { Text = "No Blur" };
+            noBlurButton.Clicked += (sender, e) => image.On<iOS>().UseBlurEffect(BlurEffectStyle.None);
+            var extraLightBlurButton = new Button { Text = "Extra Light Blur" };
+            extraLightBlurButton.Clicked += (sender, e) => image.On<iOS>().UseBlurEffect(BlurEffectStyle.ExtraLight);
+            var lightBlurButton = new Button { Text = "Light Blur" };
+            lightBlurButton.Clicked += (sender, e) => image.On<iOS>().UseBlurEffect(BlurEffectStyle.Light);
+            var darkBlurButton = new Button { Text = "Dark Blur" };
+            darkBlurButton.Clicked += (sender, e) => image.On<iOS>().UseBlurEffect(BlurEffectStyle.Dark);
+
+            Title = "Blur Effect";
+            Content = new StackLayout
+            {
+                Margin = new Thickness(20),
+                Children =
+                {
+                    image,
+                    noBlurButton, extraLightBlurButton, lightBlurButton, darkBlurButton
+                }
+            };
+        }
+    }
 }
