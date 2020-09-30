071e1c23385081f85686881960406cb2c021ca0e
David Britch <davidbritch@users.noreply.github.com>
2020-09-30 16:44:47 +0100

    Use the 5.0 RadioButton API.

---
+++ UserInterface/RadioButtonDemos/RadioButtonDemos.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RadioButtonDemos.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.LoadApplication(new RadioButtonDemos.App());
+        }
+    }
+}
