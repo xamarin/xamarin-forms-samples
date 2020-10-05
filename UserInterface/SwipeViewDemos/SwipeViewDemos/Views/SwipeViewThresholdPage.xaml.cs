7a94c5a7e71d2f6e2c6831255c02785e1be74020
David Britch <davidbritch@users.noreply.github.com>
2020-10-05 15:08:23 +0100

    Threshold example added.

---
+++ UserInterface/SwipeViewDemos/SwipeViewDemos/Views/SwipeViewThresholdPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace SwipeViewDemos
+{
+    public partial class SwipeViewThresholdPage : ContentPage
+    {
+        public SwipeViewThresholdPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            swipeView.Close();
+        }
+    }
+}
