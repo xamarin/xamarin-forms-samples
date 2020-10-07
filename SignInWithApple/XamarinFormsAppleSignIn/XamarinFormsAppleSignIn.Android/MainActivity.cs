7737151c2e8d6d5644571c6fe35ff347ced7dac0
David Britch <davidbritch@users.noreply.github.com>
2020-10-07 13:44:47 +0100

    AndroidX updates.

--- SignInWithApple/XamarinFormsAppleSignIn/XamarinFormsAppleSignIn.Android/MainActivity.cs
+++ SignInWithApple/XamarinFormsAppleSignIn/XamarinFormsAppleSignIn.Android/MainActivity.cs
@@ -1,10 +1,6 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace XamarinFormsAppleSignIn.Droid
@@ -20,6 +16,7 @@ namespace XamarinFormsAppleSignIn.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
