8847c8fba8eda82a9ca6c3920929d3778ae9a57c
David Britch <davidbritch@users.noreply.github.com>
2020-10-07 13:49:39 +0100

    AndroidX support.

--- LocalNotifications/LocalNotifications/LocalNotifications.Android/AndroidNotificationManager.cs
+++ LocalNotifications/LocalNotifications/LocalNotifications.Android/AndroidNotificationManager.cs
@@ -3,7 +3,7 @@ using Android.App;
 using Android.Content;
 using Android.Graphics;
 using Android.OS;
-using Android.Support.V4.App;
+using AndroidX.Core.App;
 using Xamarin.Forms;
 using AndroidApp = Android.App.Application;
 
