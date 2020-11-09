242c8da4b27449a1b18a050e6bc98e7c57c29784
David Britch <davidbritch@users.noreply.github.com>
2020-11-09 17:17:22 +0000

    Linker safe.

---
+++ XAML/ResourceDictionaries/ResourceDictionaryDemo/PreserveAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ResourceDictionaryDemo
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class PreserveAttribute : Attribute
+    {
+        public bool AllMembers;
+        public bool Conditional;
+
+        public PreserveAttribute()
+        {
+        }
+
+        public PreserveAttribute(bool allMembers, bool conditional)
+        {
+            AllMembers = allMembers;
+            Conditional = conditional;
+        }
+    }
+}
