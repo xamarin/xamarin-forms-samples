95972340827cc238d772d1ff7e74eab64ab6d526
David Britch <davidbritch@users.noreply.github.com>
2021-01-15 16:26:40 +0000

    Code updates.

--- GetStarted/Tutorials/CollectionViewTutorial/CollectionViewTutorial/Monkey.cs
+++ GetStarted/Tutorials/CollectionViewTutorial/CollectionViewTutorial/Monkey.cs
@@ -6,9 +6,9 @@
         public string Location { get; set; }
         public string ImageUrl { get; set; }
 
-        //public override string ToString()
-        //{
-        //    return Name;
-        //}
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
