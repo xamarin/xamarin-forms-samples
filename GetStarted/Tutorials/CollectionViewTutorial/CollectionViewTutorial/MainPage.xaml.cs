d31a7001dea8c7598a41096bafd64133dbe0a1a6
David Britch <davidbritch@users.noreply.github.com>
2021-01-15 14:33:25 +0000

    CollectionView tutorial.

---
+++ GetStarted/Tutorials/CollectionViewTutorial/CollectionViewTutorial/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CollectionViewTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public IList<Monkey> Monkeys { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Monkeys = new List<Monkey>();
+            Monkeys.Add(new Monkey
+            {
+                Name = "Baboon",
+                Location = "Africa & Asia",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Papio_anubis_%28Serengeti%2C_2009%29.jpg/200px-Papio_anubis_%28Serengeti%2C_2009%29.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Capuchin Monkey",
+                Location = "Central & South America",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/200px-Capuchin_Costa_Rica.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Blue Monkey",
+                Location = "Central and East Africa",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/BlueMonkey.jpg/220px-BlueMonkey.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Squirrel Monkey",
+                Location = "Central & South America",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg/220px-Saimiri_sciureus-1_Luc_Viatour.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Golden Lion Tamarin",
+                Location = "Brazil",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Golden_lion_tamarin_portrait3.jpg/220px-Golden_lion_tamarin_portrait3.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Howler Monkey",
+                Location = "South America",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Alouatta_guariba.jpg/200px-Alouatta_guariba.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Japanese Macaque",
+                Location = "Japan",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Macaca_fuscata_fuscata1.jpg/220px-Macaca_fuscata_fuscata1.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Mandrill",
+                Location = "Southern Cameroon, Gabon, Equatorial Guinea, and Congo",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Mandrill_at_san_francisco_zoo.jpg/220px-Mandrill_at_san_francisco_zoo.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Proboscis Monkey",
+                Location = "Borneo",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Proboscis_Monkey_in_Borneo.jpg/250px-Proboscis_Monkey_in_Borneo.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Red-shanked Douc",
+                Location = "Vietnam, Laos",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Portrait_of_a_Douc.jpg/159px-Portrait_of_a_Douc.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Gray-shanked Douc",
+                Location = "Vietnam",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Cuc.Phuong.Primate.Rehab.center.jpg/320px-Cuc.Phuong.Primate.Rehab.center.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Golden Snub-nosed Monkey",
+                Location = "China",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Golden_Snub-nosed_Monkeys%2C_Qinling_Mountains_-_China.jpg/165px-Golden_Snub-nosed_Monkeys%2C_Qinling_Mountains_-_China.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Black Snub-nosed Monkey",
+                Location = "China",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/RhinopitecusBieti.jpg/320px-RhinopitecusBieti.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Tonkin Snub-nosed Monkey",
+                Location = "Vietnam",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Tonkin_snub-nosed_monkeys_%28Rhinopithecus_avunculus%29.jpg/320px-Tonkin_snub-nosed_monkeys_%28Rhinopithecus_avunculus%29.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Thomas's Langur",
+                Location = "Indonesia",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Thomas%27s_langur_Presbytis_thomasi.jpg/142px-Thomas%27s_langur_Presbytis_thomasi.jpg"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Purple-faced Langur",
+                Location = "Sri Lanka",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Semnopithèque_blanchâtre_mâle.JPG/192px-Semnopithèque_blanchâtre_mâle.JPG"
+            });
+
+            Monkeys.Add(new Monkey
+            {
+                Name = "Gelada",
+                Location = "Ethiopia",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Gelada-Pavian.jpg/320px-Gelada-Pavian.jpg"
+            });
+
+            BindingContext = this;
+        }
+
+        void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Monkey selectedItem = e.CurrentSelection[0] as Monkey;
+        }
+    }
+}
---
+++ GetStarted/Tutorials/CollectionViewTutorial/README.md
@@ -0,0 +1,20 @@
+---
+name: Xamarin.Forms - CollectionView Tutorial
+description: "This sample demonstrates how to present lists of data, using the Xamarin.Forms CollectionView (get started)"
+page_type: sample
+languages:
+- csharp
+products:
+- xamarin
+extensions:
+    tags:
+    - getstarted
+urlFragment: getstarted-tutorials-collectionviewtutorial
+---
+# CollectionView Tutorial
+
+This sample demonstrates how to present lists of data, using the Xamarin.Forms `CollectionView`.
+
+For more information about this sample, see [CollectionView Tutorial](https://docs.microsoft.com/xamarin/get-started/tutorials/collectionview/).
+
+![CollectionView Tutorial application screenshot](Screenshots/01All.png "CollectionView Tutorial application screenshot")
