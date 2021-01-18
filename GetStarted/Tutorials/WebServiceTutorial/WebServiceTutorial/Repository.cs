4c918fedcbcf5e8d08d7ce33c53b99263ab4554e
David Britch <davidbritch@users.noreply.github.com>
2021-01-18 15:48:42 +0000

    Update sample to query GitHub.

---
+++ GetStarted/Tutorials/WebServiceTutorial/WebServiceTutorial/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace WebServiceTutorial
+{
+    public class Repository
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonProperty("homepage")]
+        public Uri Homepage { get; set; }
+
+        [JsonProperty("watchers")]
+        public int Watchers { get; set; }
+    }
+}
