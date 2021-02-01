6838719d892d4becbe66e551c167ea9c989368ae
David Britch <davidbritch@users.noreply.github.com>
2021-02-01 10:51:08 +0000

    Compile the resource dictionaries.

--- Native2Forms/Notes.iOS/MyDictionary.xaml.cs
+++ Native2Forms/Notes.iOS/MyDictionary.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Notes
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MyDictionary : ResourceDictionary
     {
         public MyDictionary()
