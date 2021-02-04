28ddfb708c1638c1f357ea0cea2693d693fe5b42
James Montemagno <james.montemagno@gmail.com>
2021-02-03 18:05:13 -0800

    Update more code

--- WebServices/TodoREST/TodoAPI/TodoAPI/Startup.cs
+++ WebServices/TodoREST/TodoAPI/TodoAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using TodoAPI.Interfaces;
 using TodoAPI.Services;
 
@@ -25,7 +26,7 @@ namespace TodoAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
--- WebServices/TodoREST/TodoREST/Constants.cs
+++ WebServices/TodoREST/TodoREST/Constants.cs
@@ -5,6 +5,9 @@ namespace TodoREST
 {
     public static class Constants
     {
+        // URL of REST service
+        //public static string RestUrl = "https://YOURPROJECT.azurewebsites.net:8081/api/todoitems/{0}";
+
         // URL of REST service (Android does not use localhost)
         // Use http cleartext for local deployment. Change to https for production
         public static string RestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000/api/todoitems/{0}" : "http://localhost:5000/api/todoitems/{0}";
