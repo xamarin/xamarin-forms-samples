6b5b069416293cf42a5a70d0b3fb4cedb2b77ec4
David Britch <davidbritch@users.noreply.github.com>
2021-02-09 15:21:17 +0000

    Test

---
+++ XAML/ResourceDictionaries/MyLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ XAML/ResourceDictionaries/Xamarin.Forms.Controls/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Xamarin.Forms.Controls
+{
+    public class Class1
+    {
+    }
+}
