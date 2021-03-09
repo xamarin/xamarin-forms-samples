6b970c98de2c2df73a9a5659cf2cc0739ac66820
David Britch <davidbritch@users.noreply.github.com>
2021-03-09 13:22:20 +0000

    Xaminals: Use IQueryAttributable (#717)
    * Use IQueryAttributable for view model navigation.
    
    * Better syntax.

---
+++ UserInterface/Xaminals/Xaminals/ViewModels/MonkeyDetailViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using Xamarin.Forms;
+using Xaminals.Data;
+using Xaminals.Models;
+
+namespace Xaminals.ViewModels
+{
+    public class MonkeyDetailViewModel : IQueryAttributable, INotifyPropertyChanged
+    {
+        public Animal Monkey { get; private set; }
+
+        public void ApplyQueryAttributes(IDictionary<string, string> query)
+        {
+            // Only a single query parameter is passed, which needs URL decoding.
+            string name = HttpUtility.UrlDecode(query["name"]);
+            LoadAnimal(name);
+        }
+
+        void LoadAnimal(string name)
+        {
+            try
+            {
+                Monkey = MonkeyData.Monkeys.FirstOrDefault(a => a.Name == name);
+                OnPropertyChanged("Monkey");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to load animal.");
+            }
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
--- UserInterface/Xaminals/Xaminals/Views/MonkeyDetailPage.xaml.cs
+++ UserInterface/Xaminals/Xaminals/Views/MonkeyDetailPage.xaml.cs
@@ -1,38 +1,14 @@
-﻿using System;
-using System.Linq;
-using Xamarin.Forms;
-using Xaminals.Data;
-using Xaminals.Models;
+﻿using Xamarin.Forms;
+using Xaminals.ViewModels;
 
 namespace Xaminals.Views
 {
-    [QueryProperty(nameof(Name), "name")]
     public partial class MonkeyDetailPage : ContentPage
     {
-        public string Name
-        {
-            set
-            {
-                LoadAnimal(value);
-            }
-        }
-
         public MonkeyDetailPage()
         {
             InitializeComponent();
-        }
-
-        void LoadAnimal(string name)
-        {
-            try
-            {
-                Animal animal = MonkeyData.Monkeys.FirstOrDefault(a => a.Name == name);
-                BindingContext = animal;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Failed to load animal.");
-            }
+            BindingContext = new MonkeyDetailViewModel();
         }
     }
 }
