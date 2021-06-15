e520c0465dc0a50088951d5d36ad648c4d9c0522
David Britch <davidbritch@users.noreply.github.com>
2021-06-15 10:23:04 +0100

    Regenerated Todo sample. (#735)

---
+++ Todo/Todo/Todo/Data/TodoItemDatabase.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Todo.Models;
+
+namespace Todo.Data
+{
+    public class TodoItemDatabase
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<TodoItemDatabase> Instance = new AsyncLazy<TodoItemDatabase>(async () =>
+        {
+            var instance = new TodoItemDatabase();
+            CreateTableResult result = await Database.CreateTableAsync<TodoItem>();
+            return instance;
+        });
+
+        public TodoItemDatabase()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+
+        public Task<List<TodoItem>> GetItemsAsync()
+        {
+            return Database.Table<TodoItem>().ToListAsync();
+        }
+
+        public Task<List<TodoItem>> GetItemsNotDoneAsync()
+        {
+            return Database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        }
+
+        public Task<TodoItem> GetItemAsync(int id)
+        {
+            return Database.Table<TodoItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(TodoItem item)
+        {
+            if (item.ID != 0)
+            {
+                return Database.UpdateAsync(item);
+            }
+            else
+            {
+                return Database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(TodoItem item)
+        {
+            return Database.DeleteAsync(item);
+        }
+    }
+}
---
+++ Todo/Todo/Todo/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+    }
+}
