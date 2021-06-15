9e5f2066a9ea82d648b69ac6c8b48a1875609f1c
David Britch <davidbritch@users.noreply.github.com>
2021-06-15 10:21:43 +0100

    Regenerated Todo sample.

---
+++ Todo/Todo/Todo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using Todo.Views;
+
+namespace Todo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new TodoListPage())
+            {
+                BarTextColor = Color.White,
+                BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"]
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Todo/Todo/Todo/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Todo.Data;
+using Todo.Models;
+using Xamarin.Forms;
+
+namespace Todo.Views
+{
+    public partial class TodoItemPage : ContentPage
+    {
+        public TodoItemPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.SaveItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.DeleteItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
